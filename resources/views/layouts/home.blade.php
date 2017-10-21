3d4df39fca38dc32c772fff1b0074770cf30fb4b
“ApplePieBnB” <sbboos123@yahoo.com.hk>
2017-10-21 21:32:43 +0800

    css

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -2,33 +2,8 @@
 
 @section('content')
 
-<div id="myCarousel" class="carousel slide" data-ride="carousel" style="margin-top: -25px; margin-bottom: 25px; width: 100%">
-	<!-- Indicators -->
-	<ol class="carousel-indicators">
-		<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-		<li data-target="#myCarousel" data-slide-to="1"></li>
-	</ol>
-
-	<!-- Wrapper for slides -->
-	<div class="carousel-inner">
-		<div class="item active">
-			<img style="width:100%" src="https://s3-us-west-2.amazonaws.com/freerider/system/intro/2.jpg" alt="Los Angeles">
-		</div>
-
-		<div class="item">
-			<img style="width:100%" src="https://s3-us-west-2.amazonaws.com/freerider/system/intro/1.jpg" alt="Chicago">
-		</div>
-	</div>
-
-	<!-- Left and right controls -->
-	<a class="left carousel-control" href="#myCarousel" data-slide="prev">
-		<span class="glyphicon glyphicon-chevron-left"></span>
-		<span class="sr-only">Previous</span>
-	</a>
-	<a class="right carousel-control" href="#myCarousel" data-slide="next">
-		<span class="glyphicon glyphicon-chevron-right"></span>
-		<span class="sr-only">Next</span>
-	</a>
+<div style="margin-top: -25px; margin-bottom: 25px; width: 100%">
+	<img style="width:100%" src="https://s3-us-west-2.amazonaws.com/freerider/system/intro/2.jpg" alt="Los Angeles">
 </div>
 
 <div class="container" style="width: 90%">
