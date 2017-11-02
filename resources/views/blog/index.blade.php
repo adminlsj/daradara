cda5459e83594ba48487fa11081c8e8f38aefc9c
“ApplePieBnB” <sbboos123@yahoo.com.hk>
2017-11-02 18:33:54 +0800

    css

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -7,7 +7,7 @@
 			<div class="blog-carousel owl-carousel owl-theme">
 				@foreach ($caro_blogs as $blog)
 					<a href="{{ route('blog.show', ['blog' => $blog->id]) }}">
-						<img class="img-responsive border-radius-2" src="https://s3-us-west-2.amazonaws.com/freerider/blogImgs/thumbnails/{{ $blog->id }}/{{ $blog->blogImgs->first()->filename }}" alt="First slide">
+						<img class="img-responsive border-radius-2" src="https://s3-us-west-2.amazonaws.com/freerider/blogImgs/originals/{{ $blog->id }}/{{ $blog->blogImgs->first()->filename }}" alt="First slide">
 						<div class="center-align white-text" style="position:absolute;bottom:0;left:0;right:0;font-size:17px;color:white;background-color: rgba(0, 0, 0, 0.5);height:70px;padding-left:15px;padding-right:15px; border-radius: 0 0 2px 2px;">
 							<span style="line-height: 70px; font-weight: 300; letter-spacing: 1px">{{ $blog->title }}</span>
 						</div>
