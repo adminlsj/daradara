2819c29406b40ea060424f80539dab236a8b5693
“ApplePieBnB” <sbboos123@yahoo.com.hk>
2017-11-03 22:16:24 +0800

    css

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -11,11 +11,6 @@
 					{{ $blog->title }}
 					<span class="pull-right" style="font-size:15px;font-weight:300">
 						<div style="margin-bottom:-25px">{{ Carbon\Carbon::parse($blog->created_at)->format('Y年m月d日') }}</div>
-						<div style="display: inline !important; margin:2px">
-							<div class="fb-like" data-href="https://www.facebook.com/freeriderhk/" data-layout="button" data-action="like" data-show-faces="false" data-share="false"></div>
-
-							<div class="fb-share-button right" style="margin-bottom:-50px;" data-href="https://www.facebook.com/freeriderhk/posts/1591857584440962:0" data-layout="button" data-size="small" data-mobile-iframe="true"><a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwww.facebook.com%2Ffreeriderhk%2Fposts%1593758424250878%3A0&amp;src=sdkpreparse">分享</a>
-						</div>
 					</span>
 				</h4>
 				<br>
