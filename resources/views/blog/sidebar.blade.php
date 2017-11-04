9c0dfe492ce5ec98afbfb1763b05bb1798becef1
“ApplePieBnB” <sbboos123@yahoo.com.hk>
2017-11-04 11:41:36 +0800

    css

--- resources/views/blog/sidebar.blade.php
+++ resources/views/blog/sidebar.blade.php
@@ -19,19 +19,6 @@
 
 <h3 style="color: grey; font-weight: 300; margin-top: 35px">為您推薦的商品</h3>
 <hr>
-@foreach($relatedOrders as $order)
-    <div class="row" style="margin-bottom: 15px;">
-        <div class="col-md-5">
-            <a href="{{ route('order.show', ['order' => $order->id]) }}">
-                    <img style="border: solid 1px #f2f2f2" src="https://s3-us-west-2.amazonaws.com/freerider/orderImgs/thumbnails/{{ $order->id }}/{{ $order->orderImgs->first()->filename }}.jpg" class="img-responsive img-circle">
-            </a>
-        </div>
-        <div class="col-md-7">
-            <div><a href="{{ route('order.show', ['order' => $order->id]) }}"><h3 style="color: black; font-weight: 400; font-size: 15px">{{ str_limit($order->name, 50) }}</h3></a></div>
-            <div style="font-size: 12.5px">${{ $order->price }} + $0 服務費</div>
-            <div style="font-size: 12.5px">{{ $order->end_date }} 前</div>
-        </div>
-    </div>
-@endforeach
+@include('order.related-orders')
 <br>
 <a href="/"><img style="width:100%; border: solid 1px #f2f2f2" src="https://s3-us-west-2.amazonaws.com/freerider/system/intro/7.jpg" alt="Los Angeles"></a>
