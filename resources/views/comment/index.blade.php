11e4e07fd58c02b0fe7695f917599ee3be5b17b9
“ApplePieBnB” <sbboos123@yahoo.com.hk>
2017-11-04 17:51:59 +0800

    css

--- resources/views/comment/index.blade.php
+++ resources/views/comment/index.blade.php
@@ -1,10 +1,10 @@
 <div id="comment-start">
 	@foreach($comments as $comment)
 		<div id="comment{{ $comment->id }}" class="row">
-			<div class="col-md-1">
+			<div class="col-md-1 col-xs-2">
 				<a href="/orders/search?name="><img src="https://s3-us-west-2.amazonaws.com/freerider/avatars/thumbnails/{{ $comment->user->avatar->filename }}.jpg" class="img-responsive img-circle"></a>
 			</div>
-			<div class="col-md-9">
+			<div class="col-md-9 col-xs-7">
 				<div class="row">
 					<a style="color: black; font-weight: 600" href="{{ route('user.show', ['user' => $comment->user->id]) }}">{{$comment->user->name}}</a>
 				</div>
@@ -12,15 +12,8 @@
 					<p>{{ $comment->text }}</p>
 				</div>
 			</div>
-			<div class="col-md-2 text-right">
+			<div class="col-md-2 text-right col-xs-3">
 				<small>{{ $comment->created_at->diffForHumans() }}</small>
-				@if(Auth::check() && auth()->user()->id == $comment->user->id)
-					<form id="deleteComment{{ $comment->id }}" action="{{ route('order.comment.destroy', ['order'=>$order->id, 'comment'=>$comment->id]) }}" method="POST" name="delete_item">
-						{{ csrf_field() }}
-						{{ method_field('DELETE') }}
-						<button type="submit" class="btn btn-danger" style="margin-top: 5px; line-height: 15px"><small>Delete</small></button>
-					</form>
-				@endif
 			</div>
 		</div>
 		<br>
