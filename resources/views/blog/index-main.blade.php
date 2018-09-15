1857b32ea6ac6e6220e44a632594edd4161ce400
AppleOPie <u3514481@connect.hku.hk>
2018-09-15 12:03:28 +0800

    Procfile for Heroku

--- resources/views/blog/index-main.blade.php
+++ resources/views/blog/index-main.blade.php
@@ -8,7 +8,7 @@
 			<div class="blog-carousel owl-carousel owl-theme">
 				@foreach ($caro_blogs as $blog)
 					<a href="{{ route('blog.show', ['blog' => $blog->id]) }}">
-						<img class="img-responsive border-radius-2" src="https://s3.amazonaws.com/twobayjobs/blogImgs/originals/{{ $blog->id }}/{{ $blog->blogImgs->first()->filename }}" alt="First slide">
+						<img class="img-responsive border-radius-2" src="https://s3.amazonaws.com/twobayjobs/blogImgs/originals/{{ $blog->id }}/{{ $blog->blogImgs->first()->filename }}.jpg" alt="First slide">
 						<div class="center-align white-text" style="position:absolute;bottom:0;left:0;right:0;font-size:17px;color:white;background-color: rgba(0, 0, 0, 0.5);height:70px;padding-left:15px;padding-right:15px; border-radius: 0 0 2px 2px;">
 							<span style="line-height: 70px; font-weight: 300; letter-spacing: 1px">{{ $blog->title }}</span>
 						</div>
@@ -22,7 +22,7 @@
 		@foreach ($blogs as $blog)
 			<div class="col-md-6 col-ms-12">
 				<div class="card">
-	                <a href="{{ route('blog.show', ['blog' => $blog->id]) }}"><img class="img-responsive" src="https://s3.amazonaws.com/twobayjobs/blogImgs/thumbnails/{{ $blog->id }}/{{ $blog->blogImgs->sortby('created_at')->first()->filename }}" alt="First slide"></a>
+	                <a href="{{ route('blog.show', ['blog' => $blog->id]) }}"><img class="img-responsive" src="https://s3.amazonaws.com/twobayjobs/blogImgs/thumbnails/{{ $blog->id }}/{{ $blog->blogImgs->sortby('created_at')->first()->filename }}.jpg" alt="First slide"></a>
 
 				    <div class="card-content">
 				        <a style="line-height: 25px; padding: 13px; font-weight: 300; color: grey; display:block; white-space: nowrap;overflow: hidden;text-overflow: ellipsis;" href="{{ route('blog.show', ['blog' => $blog->id]) }}">
