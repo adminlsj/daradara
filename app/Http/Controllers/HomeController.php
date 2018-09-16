2df8771bed1938e69e70aaa437b069bd5353668c
AppleOPie <u3514481@connect.hku.hk>
2018-09-16 13:11:52 +0800

    Procfile for Heroku

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -19,16 +19,14 @@ class HomeController extends Controller
     public function index()
     {
         //auth()->loginUsingId(1);
-
-        $blogs = Blog::all()->sortByDesc('created_at');
+        $relatedBlogs = Blog::inRandomOrder()->limit(15)->get();
         $caro_blogs = Blog::inRandomOrder()->limit(4)->get();
-        $relatedBlogs = Blog::inRandomOrder()->limit(3)->get();
-        $similar_blogs = Blog::inRandomOrder()->limit(5)->get();
+        $blogs = Blog::all()->sortByDesc('created_at');
 
         $featuredCompanies = ['騰訊', '阿里巴巴', '小米', '百度', '鏈家', '滴滴'];
         $featuredAgencies = ['exmil', 'ern', 'freerider'];
 
-        return view('layouts.home', compact('blogs', 'caro_blogs', 'relatedBlogs', 'similar_blogs', 'featuredCompanies', 'featuredAgencies'));
+        return view('layouts.home', compact('blogs', 'caro_blogs', 'relatedBlogs', 'featuredCompanies', 'featuredAgencies'));
     }
 
     public function contact()
