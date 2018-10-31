342f74cf8ff3c7b243934de4e7d0bc32576fa44b
AppleOPie <u3514481@connect.hku.hk>
2018-10-31 10:57:39 +0800

    Google Adsense

--- resources/views/blog/sidebar.blade.php
+++ resources/views/blog/sidebar.blade.php
@@ -11,7 +11,23 @@
     <br>
 </div>
 
-<div class="row">
+<div class="hidden-xs hidden-sm">
+    <div class="col-sm-12 col-md-12">
+        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <!-- Home Page Ads -->
+        <ins class="adsbygoogle"
+             style="display:block"
+             data-ad-client="ca-pub-4485968980278243"
+             data-ad-slot="9914751067"
+             data-ad-format="auto"
+             data-full-width-responsive="true"></ins>
+        <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
+    </div>
+</div>
+
+<div class="visible-xs-block visible-sm-block">
     <div class="col-sm-12 col-md-12">
         <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
         <!-- Home Page Ads -->
