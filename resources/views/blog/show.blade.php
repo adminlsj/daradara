9353496ee257c7e856be5cc4e3384745619d2fe3
AppleOPie <u3514481@connect.hku.hk>
2018-10-31 11:54:30 +0800

    Google Adsense

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -77,7 +77,7 @@
 
 					<br>
 
-					<div class="container visible-xs-block visible-sm-block" style="width:91% !important">
+					<div class="container visible-xs-block visible-sm-block" style="width:93% !important">
 						<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 				        <!-- Home Page Ads -->
 				        <ins class="adsbygoogle"
