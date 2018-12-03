f7968bb33f7118283c6c4f3fa5d2ac3225829c38
AppleOPie <u3514481@connect.hku.hk>
2018-12-03 11:48:49 +0800

    Google Adsense

--- database/migrations/2017_10_04_125728_create_comments_table.php
+++ database/migrations/2017_10_04_125728_create_comments_table.php
@@ -17,8 +17,6 @@ class CreateCommentsTable extends Migration
             $table->increments('id');
             $table->integer('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->integer('company_id')->unsigned();
-            $table->foreign('company_id')->references('id')->on('companies')->onDelete('cascade');
             $table->string('text');
             $table->timestamps();
         });
