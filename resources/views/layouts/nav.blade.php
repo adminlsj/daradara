1c203422c27b79908ad9ce6e51534526c62cd0b3
AppleOPie <u3514481@connect.hku.hk>
2018-12-03 15:41:45 +0800

    Google Adsense

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -17,7 +17,7 @@
                 <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
                 <!-- Navbar -->
                 <ins class="adsbygoogle"
-                     style="display:inline-block;width:50vw;height:90px"
+                     style="display:inline-block;width:100%;height:90px"
                      data-ad-client="ca-pub-4485968980278243"
                      data-ad-slot="1745786231"></ins>
                 <script>
