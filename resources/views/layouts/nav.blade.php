f041fc842b256711aaad5676bf3e97f06817bc34
AppleOPie <u3514481@connect.hku.hk>
2018-12-03 15:11:17 +0800

    Google Adsense

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,27 +1,29 @@
 <nav class="navbar navbar-default nav-main">
     <div class="container mobile-container">
             <!-- Left Side Of Navbar -->
-            <ul style="margin-left: -15px" class="nav navbar-nav vertical-align" >
+            <ul style="margin-left: -15px" class="nav navbar-nav vertical-align hidden-xs hidden-sm" >
                 <li>
                     <a href="{{ url('/') }}" style="margin-right: -10px;">
                         <img src="https://s3-us-west-2.amazonaws.com/freerider/avatars/originals/default_freerider_profile_pic.jpg" style="border: 2px solid white; border-radius: 2px; margin-top: -10px; margin-bottom: -8px" width="100px" height="100px">
                     </a>
                 </li>
-                <li>
+                <li class="hidden-xs hidden-sm">
                     <a style="font-size: 50px; font-weight: 400;" href="{{ url('/') }}">{{ config('app.name', 'FreeRider') }}</a>
                 </li>
             </ul>
 
             <!-- Right Side Of Navbar -->
-            <ul style="z-index: 100; background-color: gray" class="nav navbar-nav navbar-right">
+            <ul style="background-color: gray; margin-left:0px; margin-right: 0px" class="nav navbar-nav navbar-right">
                 <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-                <!-- Navbar Ads -->
-                <ins class="adsbygoogle"
-                     style="display:block"
+                <!-- Navbar Desktop -->
+                <ins class="adsbygoogle hidden-xs hidden-sm"
+                     style="display:inline-block;width:50vw;height:100px"
                      data-ad-client="ca-pub-4485968980278243"
-                     data-ad-slot="9416392692"
-                     data-ad-format="link"
-                     data-full-width-responsive="true"></ins>
+                     data-ad-slot="1745786231"></ins>
+                <ins class="adsbygoogle visible-xs-block visible-sm-block"
+                     style="display:inline-block;width:100%;height:100px;"
+                     data-ad-client="ca-pub-4485968980278243"
+                     data-ad-slot="1745786231"></ins>
                 <script>
                 (adsbygoogle = window.adsbygoogle || []).push({});
                 </script>
