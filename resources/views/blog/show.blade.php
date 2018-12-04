b41e115bd954679b7120833fcf865d46f3f29e19
AppleOPie <u3514481@connect.hku.hk>
2018-12-04 19:56:31 +0800

    Google Adsense

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -8,7 +8,7 @@
 				<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 				<!-- Content -->
 				<ins class="adsbygoogle"
-				     style="display:inline-block;width:auto;height:100px"
+				     style="display:inline-block;height:100px"
 				     data-ad-client="ca-pub-4485968980278243"
 				     data-ad-slot="9914751067"></ins>
 				<script>
