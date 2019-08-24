8cacf8240bc9aa5c3686ee8b4426ccb9dffbcfdf
AppleOPie <u3514481@connect.hku.hk>
2019-08-24 20:43:18 +0800

    Google Adsense

--- resources/views/video/genreIndex.blade.php
+++ resources/views/video/genreIndex.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-<div style="width:78%; margin: 0 auto;" class="mobile-container">
-	<div class="row no-gutter">
+<div style="width:80%;" class="container">
+	<div class="row video-no-gutter">
 		<div class="col-md-8" style="margin-top: 15px;">
-			<div class="video-sidebar-wrapper">
+			<div style="overflow: hidden;" class="video-sidebar-wrapper">
 			    <div id="sidebar-results"><!-- results appear here --></div>
 			    <div style="text-align: center" class="ajax-loading"><img src="https://s3.amazonaws.com/twobayjobs/system/loading.gif"/></div>
 			</div>
