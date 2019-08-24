ab3ac1135a620d35722ad3c7c5dee2403c2e6add
AppleOPie <u3514481@connect.hku.hk>
2019-08-24 17:20:18 +0800

    Google Adsense

--- resources/views/video/genreIndex.blade.php
+++ resources/views/video/genreIndex.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div style="width:78%; margin: auto; overflow-x:hidden !important" class="mobile-container">
+<div style="width:78%; margin: auto;" class="mobile-container">
 	<div class="row">
 		<div class="col-md-8" style="margin-top: 15px;">
 			<div class="sidebar-wrapper">
