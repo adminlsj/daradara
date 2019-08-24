e81849b45f6c118c39834ee301bf9c82077bc57b
AppleOPie <u3514481@connect.hku.hk>
2019-08-24 15:52:32 +0800

    Google Adsense

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -322,7 +322,7 @@ class BlogController extends Controller
     {
         $html = '';
         foreach ($videos as $video) {
-            $html .='<div class="fb-video"
+            $html .='<div style="overflow:hidden" class="fb-video"
                       data-href="'.$video->content.'"
                       data-width="auto"
                       data-allowfullscreen="true"
