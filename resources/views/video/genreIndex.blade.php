52bc7e1ddc3040349a4721042136960fa28b983b
AppleOPie <u3514481@connect.hku.hk>
2019-08-24 20:12:32 +0800

    Google Adsense

--- resources/views/video/genreIndex.blade.php
+++ resources/views/video/genreIndex.blade.php
@@ -4,7 +4,7 @@
 <div>
 	<div class="row">
 		<div class="col-md-8" style="margin-top: 15px;">
-			<div class="sidebar-wrapper">
+			<div class="video-sidebar-wrapper">
 			    <div id="sidebar-results"><!-- results appear here --></div>
 			    <div style="text-align: center" class="ajax-loading"><img src="https://s3.amazonaws.com/twobayjobs/system/loading.gif"/></div>
 			</div>
