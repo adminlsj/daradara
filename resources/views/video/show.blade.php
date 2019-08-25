96d9d9bf74413fefa317e0e197f156428b6e9589
AppleOPie <u3514481@connect.hku.hk>
2019-08-25 09:40:38 +0800

    Google Adsense

--- resources/views/video/show.blade.php
+++ resources/views/video/show.blade.php
@@ -11,21 +11,6 @@
 			      data-autoplay="false"
 			      data-show-captions="false"></div>
 
-			<div class="video-title-container">
-			    <div>
-			        <a href="https://www.facebook.com/laughseejapan/" target="_blank">
-			            <img src="https://twobayjobs.s3.amazonaws.com/avatars/originals/default_laughseejapan_profile_pic.jpg" class="video-profile-pic" width="40px" height="40px">
-			        </a>
-			    </div>
-			    <div>
-			        <a href="{{ route('blog.show', ['blog' => $video, 'genre' => $video->genre, 'category' => $video->category ]) }}">
-			            <h4 class="video-title">{{ $video->title }}</h4>
-			        </a>
-			        <p class="video-caption">{{ $video->caption }}</p>
-			        <p class="video-tags">娛見日本@laughseejapan | <a href="{{ route('blog.category.index', ['genre' => $video->genre, 'category' => $video->category]) }}">{{ array_search($video->category, App\Blog::$genres[$video->genre]['categories']) }}</a> | {{ Carbon\Carbon::parse($video->created_at)->format('Y-m-d') }}</span></p>
-			    </div>
-			</div>​
-			<br>
 
 			<div class="video-sidebar-wrapper">
 			    <div id="sidebar-results">
