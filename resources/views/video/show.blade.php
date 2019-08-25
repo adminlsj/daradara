822d861e47d26019a68b4b9f1368c87a0d13c90a
AppleOPie <u3514481@connect.hku.hk>
2019-08-25 11:22:18 +0800

    Google Adsense

--- resources/views/video/show.blade.php
+++ resources/views/video/show.blade.php
@@ -6,7 +6,7 @@
 		<div class="col-md-8" style="margin-top: 15px;">
 			<div class="video-sidebar-wrapper">
 		    	@include('video.singleVideoPost')
-		    	<div style="background-color: #fafafa" class="fb-comments" data-href="{{ $video->content }}" data-width="100%" data-numposts="5" data-colorscheme="light" data-order-by="social"></div>
+		    	<div class="fb-comments" data-href="{{ $video->content }}" data-width="100%" data-numposts="5" data-colorscheme="light" data-order-by="social"></div>
 			    <div id="sidebar-results">
 			    	<!-- results appear here -->
 			    </div>
