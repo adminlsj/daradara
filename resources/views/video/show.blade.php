663ebc3841353457676273ddd4a822a1418cc64f
AppleOPie <u3514481@connect.hku.hk>
2019-08-25 09:50:07 +0800

    Google Adsense

--- resources/views/video/show.blade.php
+++ resources/views/video/show.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div style="width:78%; margin: 0 auto;" class="mobile-container">
+<div style="width:78%; margin: 0 auto;" class="container mobile-container">
 	<div class="row">
 		<div class="col-md-8" style="margin-top: 15px;">
 			<div class="fb-video"
