41c7f0c5189d48bb4dc40b56e17951fce19ec3af
AppleOPie <u3514481@connect.hku.hk>
2019-08-25 09:35:51 +0800

    Google Adsense

--- resources/views/video/show.blade.php
+++ resources/views/video/show.blade.php
@@ -4,28 +4,13 @@
 <div style="width:78%; margin: 0 auto;" class="mobile-container">
 	<div class="row video-no-gutter">
 		<div class="col-md-8" style="margin-top: 15px;">
-			<div class="fb-video"
-      data-href="{{ $video->content }}"
-      data-width="auto"
-      data-allowfullscreen="false"
-      data-autoplay="false"
-      data-show-captions="false"></div>
+			<div style="overflow-x: hidden !important;" class="fb-video"
+		      data-href="{{ $video->content }}"
+		      data-width="auto"
+		      data-allowfullscreen="false"
+		      data-autoplay="false"
+		      data-show-captions="false"></div>
 
-<div class="video-title-container">
-    <div>
-        <a href="https://www.facebook.com/laughseejapan/" target="_blank">
-            <img src="https://twobayjobs.s3.amazonaws.com/avatars/originals/default_laughseejapan_profile_pic.jpg" class="video-profile-pic" width="40px" height="40px">
-        </a>
-    </div>
-    <div>
-        <a href="{{ route('blog.show', ['blog' => $video, 'genre' => $video->genre, 'category' => $video->category ]) }}">
-            <h4 class="video-title">{{ $video->title }}</h4>
-        </a>
-        <p class="video-caption">{{ $video->caption }}</p>
-        <p class="video-tags">娛見日本@laughseejapan | <a href="{{ route('blog.category.index', ['genre' => $video->genre, 'category' => $video->category]) }}">{{ array_search($video->category, App\Blog::$genres[$video->genre]['categories']) }}</a> | {{ Carbon\Carbon::parse($video->created_at)->format('Y-m-d') }}</span></p>
-    </div>
-</div>​
-<br>
 			<div class="video-sidebar-wrapper">
 			    <div id="sidebar-results">
 			    	<!-- results appear here -->
