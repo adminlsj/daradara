ee75770427295a3e865beafa76474182fa909015
AppleOPie <u3514481@connect.hku.hk>
2019-08-27 22:10:21 +0800

    Google Adsense

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,14 +1,14 @@
-<nav id="scroll-hide-nav" class="responsive-frame">
-  <div style="width: 80%; background-color: white !important;" class="container-fluid">
-    <div style="background-color: white !important;">
+<nav style="background-color: #414143 !important" id="scroll-hide-nav" class="responsive-frame">
+  <div style="width: 80%; background-color: #414143 !important;" class="container-fluid">
+    <div style="background-color: #414143 !important;">
       <a href="{{ route('blog.genre.index', ['genre' => $genre]) }}">
-	        <img src="https://s3-us-west-2.amazonaws.com/freerider/avatars/originals/default_freerider_profile_pic.jpg" style="border-radius: 20px; margin-top: -15px;" width="40px" height="40px">
+	        <img src="https://twobayjobs.s3.amazonaws.com/avatars/originals/square_freerider_profile_pic.jpg" style="border-radius: 20px; margin-top: -15px;" width="45px" height="45px">
 	    </a>
 
-	    <a style="font-size: 30px; color: black !important; font-weight: 600; line-height: 50px; text-decoration: none;" href="{{ route('blog.genre.index', ['genre' => $genre]) }}">{{ App\Blog::$genres[$genre]['navTitle'] }}</a>
+	    <a style="font-size: 30px; color: white !important; font-weight: 300; line-height: 50px; text-decoration: none; margin-left: -5px;" href="{{ route('blog.genre.index', ['genre' => $genre]) }}">{{ App\Blog::$genres[$genre]['navTitle'] }}</a>
 
       @foreach (App\Blog::$genres[$genre]['categories'] as $text => $value)
-        <a class="pull-right" style="color: black !important; padding: 0px 0px {{ $loop->index == 0 ? '0px' : '15px'}} 15px; line-height: 40px;" href="{{ route('blog.category.index', ['genre' => $genre, 'category' => $value]) }}"><i style="font-size: 30px; vertical-align:top; margin-top: 10px" class="material-icons">{{ App\Blog::$navIcons[$loop->index] }}</i></a>
+        <a class="pull-right" style="color: #f2f2f2 !important; padding: 0px 0px {{ $loop->index == 0 ? '0px' : '15px'}} 15px;" href="{{ route('blog.category.index', ['genre' => $genre, 'category' => $value]) }}"><i style="font-size: 30px; vertical-align:middle; margin-bottom: -22px" class="material-icons">{{ App\Blog::$navIcons[$loop->index] }}</i></a>
       @endforeach
     </div>
   </div>
