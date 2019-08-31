de5c9b40ff00d0ec61fbcd6c719ab13a11939811
AppleOPie <u3514481@connect.hku.hk>
2019-08-31 09:59:46 +0800

    Google Adsense

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -114,6 +114,9 @@ function load_more(page){
     })
 
     .done(function(data){
+        if (page == 1) {
+            (adsbygoogle = window.adsbygoogle || []).push({});
+        }
         if (data.length == 0){
 	        console.log(data.length);
             $('.ajax-loading').html(" ");
@@ -125,9 +128,6 @@ function load_more(page){
         var $newhtml = $("<div id='" + newDivName + "'>" + data + "</div>");
         $('#sidebar-results').append($newhtml);
         FB.XFBML.parse($newhtml[0]);
-        if (page == 1) {
-            (adsbygoogle = window.adsbygoogle || []).push({});
-        }
     })
 
     .fail(function(jqXHR, ajaxOptions, thrownError){
