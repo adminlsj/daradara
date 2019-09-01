c1c4445bb097eee835b26c36e700d8ecfad71ede
AppleOPie <u3514481@connect.hku.hk>
2019-09-01 16:58:14 +0800

    Google Adsense

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -130,32 +130,17 @@ class BlogController extends Controller
             $video = $blog;
 
             $loop = 0;
-            $preVideos = [];
+            $videos = [];
             foreach ($video->tags() as $tag) {
                 if ($loop == 0) {
-                    $preVideos = Blog::where('genre', $genre)->where('tags', 'like', '%'.$tag.'%')->where('created_at', '<', $video->created_at);
+                    $videos = Blog::where('genre', $genre)->where('tags', 'like', '%'.$tag.'%')->where('id', '!=', $video->id);
                 } else {
-                    $preVideos = $preVideos->orWhere('genre', $genre)->where('tags', 'like', '%'.$tag.'%')->where('created_at', '<', $video->created_at);
+                    $videos = $videos->orWhere('genre', $genre)->where('tags', 'like', '%'.$tag.'%')->where('id', '!=', $video->id);
                 }
                 $loop++;
             }
-            $preVideos = $preVideos->distinct()->orderBy('created_at', 'desc')->get();
-            $preHTML = $this->videoLoadHTML($preVideos);
-
-            $loop = 0;
-            $postVideos = [];
-            foreach ($video->tags() as $tag) {
-                if ($loop == 0) {
-                    $postVideos = Blog::where('genre', $genre)->where('tags', 'like', '%'.$tag.'%')->where('created_at', '>', $video->created_at);
-                } else {
-                    $postVideos = $postVideos->orWhere('genre', $genre)->where('tags', 'like', '%'.$tag.'%')->where('created_at', '>', $video->created_at);
-                }
-                $loop++;
-            }
-            $postVideos = $postVideos->distinct()->orderBy('created_at', 'desc')->get();
-            $postHTML = $this->videoLoadHTML($postVideos);
-
-            $html = $preHTML.''.$postHTML;
+            $videos = $videos->distinct()->orderBy('created_at', 'desc')->paginate(5);
+            $html = $this->videoLoadHTML($videos);
             if ($request->ajax()) {
                 return $html;
             }
