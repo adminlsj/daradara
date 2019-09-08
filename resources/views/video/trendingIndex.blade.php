37d8e07cc9a0a18e46023653136efd9f1e448e47
AppleOPie <u3514481@connect.hku.hk>
2019-09-08 19:12:18 +0800

    Google Adsense

--- resources/views/video/trendingIndex.blade.php
+++ resources/views/video/trendingIndex.blade.php
@@ -3,10 +3,10 @@
 @section('content')
 <div style="width:78%; margin: 0 auto;" class="mobile-container">
 	<div class="trending-circles">
-		<a href="{{ route('blog.category.index', ['genre' => 'laughseejapan', 'category' => '搞笑']) }}"><div style="display: inline-block; margin-right: 7px;" class="circle">#搞笑</div></a>
-		<a href="{{ route('blog.category.index', ['genre' => 'laughseejapan', 'category' => '感動']) }}"><div style="display: inline-block;margin-right: 7px;" class="circle">#感動</div></a>
-		<a href="{{ route('blog.category.index', ['genre' => 'laughseejapan', 'category' => '月曜']) }}"><div style="display: inline-block;margin-right: 7px;" class="circle">#月曜</div></a>
-		<a href="{{ route('blog.category.index', ['genre' => 'laughseejapan', 'category' => '街訪']) }}"><div style="display: inline-block;margin-right: 7px;" class="circle">#街訪</div></a>
+		<a href="{{ route('blog.category.index', ['genre' => 'laughseejapan', 'category' => '搞笑']) }}"><div style="display: inline-block; margin-right: 8px;" class="circle">#搞笑</div></a>
+		<a href="{{ route('blog.category.index', ['genre' => 'laughseejapan', 'category' => '感動']) }}"><div style="display: inline-block;margin-right: 8px;" class="circle">#感動</div></a>
+		<a href="{{ route('blog.category.index', ['genre' => 'laughseejapan', 'category' => '月曜']) }}"><div style="display: inline-block;margin-right: 8px;" class="circle">#月曜</div></a>
+		<a href="{{ route('blog.category.index', ['genre' => 'laughseejapan', 'category' => '街訪']) }}"><div style="display: inline-block;margin-right: 8px;" class="circle">#街訪</div></a>
     </div>
 	<div class="row video-no-gutter">
 		<div class="col-md-8" style="margin-top: 10px;">
