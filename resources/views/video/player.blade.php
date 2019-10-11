83b4f54aeed6fbee7f471b05ceb36c2189d077d6
AppleOPie <u3514481@connect.hku.hk>
2019-10-11 23:43:08 +0800

    Google Adsense

--- resources/views/video/player.blade.php
+++ resources/views/video/player.blade.php
@@ -2,7 +2,6 @@
   <video id="video-js-player" style="min-width: 100%; min-height: 100%;" class="video-js vjs-waiting"
       poster='https://i.imgur.com/{{ $video->blogImgs[0]->imgur }}h.png'
       data-setup='{ "aspectRatio":"16:9", "controls": true, "playsinline" : true, "autoplay": true, "muted": true, "preload": "auto" }'>
-    <source src="https://archive.org/download/{{ $video->source }}.mp4" type='video/mp4' />
-    <source src="https://archive.org/download/{{ $video->source }}.ogv" type='video/ogg' />
+    <source src="{{ $video->source }}" type='video/mp4' />
   </video>
 </div>
