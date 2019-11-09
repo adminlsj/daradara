553fbc4850501810cd97b8536472762bd99b9cae
AppleOPie <u3514481@connect.hku.hk>
2019-11-09 19:58:18 +0800

    Google Adsense

--- resources/views/video/singleHomeVideo.blade.php
+++ resources/views/video/singleHomeVideo.blade.php
@@ -4,13 +4,16 @@
 	</a>
 
 	<div style="position: relative; height: 50px">
-	    <span style="position: absolute; top:-26px; right: 7px; background-color: rgba(0,0,0,0.8); color: white; padding: 1px 5px 1px 5px; font-size: 0.85em; border-radius: 2px; opacity: 0.9">{{ $video->duration() }}</span>
+	    <span style="position: absolute; top:-28px; right: 6px; background-color: rgba(0,0,0,0.8); color: white; padding: 1px 5px 1px 5px; font-size: 0.85em; border-radius: 2px; opacity: 0.9">{{ $video->duration() }}</span>
 	    <div>
 	        <a href="{{ route('video.trending') }}?v={{ $video->id }}">
 	          <h4 style="margin-top: 5px; font-weight: 500; line-height: 19px; font-size: 1em;overflow: hidden;text-overflow: ellipsis; display: -webkit-box; -webkit-line-clamp: 2; -webkit-box-orient: vertical;">{{ $video->title }}</h4>
 	        </a>
 
-	        <p style="color: gray; margin-top: -7px; margin-bottom: 0px; font-size: 0.85em;">觀看次數：{{ $video->views() }}次 • {{ Carbon\Carbon::parse($video->created_at)->format('Y-m-d') }}</p>
+	        <p style="color: gray; margin-top: -7px; margin-bottom: 0px; font-size: 0.85em;">
+	        	<i style="vertical-align:middle; font-size: 1.2em; margin-top: -2.5px;" class="material-icons">remove_red_eye</i>
+	        	{{ $video->views() }}次 • {{ Carbon\Carbon::parse($video->created_at)->format('Y-m-d') }}
+	        </p>
 	    </div>
 	</div>​
 </div>
@@ -21,13 +24,16 @@
 	</a>
 
 	<div style="position: relative; height: 50px;">
-	    <span style="position: absolute; top:-26px; right: 7px; background-color: rgba(0,0,0,0.8); color: white; padding: 1px 5px 1px 5px; font-size: 0.85em; border-radius: 2px; opacity: 0.9">{{ $video->duration() }}</span>
+	    <span style="position: absolute; top:-28px; right: 6px; background-color: rgba(0,0,0,0.8); color: white; padding: 1px 5px 1px 5px; font-size: 0.85em; border-radius: 2px; opacity: 0.9">{{ $video->duration() }}</span>
 	    <div>
 	        <a href="{{ route('video.trending') }}?v={{ $video->id }}">
 	          <h4 style="margin-top: 5px; font-weight: 500; line-height: 19px; font-size: 1em;overflow: hidden;text-overflow: ellipsis; display: -webkit-box; -webkit-line-clamp: 2; -webkit-box-orient: vertical;">{{ $video->title }}</h4>
 	        </a>
 
-	        <p style="color: #a8a8a8; margin-top: -7px; margin-bottom: 0px; font-size: 0.85em;">觀看次數：{{ $video->views() }}次 • {{ Carbon\Carbon::parse($video->created_at)->format('Y-m-d') }}</p>
+	        <p style="color: gray; margin-top: -7px; margin-bottom: 0px; font-size: 0.85em;">
+	        	<i style="vertical-align:middle; font-size: 1.2em; margin-top: -2.5px;" class="material-icons">remove_red_eye</i>
+	        	{{ $video->views() }}次 • {{ Carbon\Carbon::parse($video->created_at)->format('Y-m-d') }}
+	        </p>
 	    </div>
 	</div>​
 </div>
