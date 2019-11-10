9abec8c35f7057f06fb80fd7db6d811a2b24cee1
AppleOPie <u3514481@connect.hku.hk>
2019-11-10 17:54:03 +0800

    Google Adsense

--- resources/views/video/singleShowPost.blade.php
+++ resources/views/video/singleShowPost.blade.php
@@ -15,11 +15,11 @@
       <h4 id="shareBtn-title" style="line-height: 23px; font-weight: 500; margin-top:-10px; margin-bottom: 0px; color: #323232; font-size: 1.25em">{{ $video->title }}</h4>
     </a>
 
-    <p style="color: #a9a9a9; margin-top: 4px; margin-bottom: 0px; font-size: 0.8em;"><i style="vertical-align:middle; font-size: 1.3em; margin-top: -2px; margin-right: 4px;" class="material-icons">remove_red_eye</i>{{ $video->views() }}次 • {{ Carbon\Carbon::parse($video->created_at)->format('Y-m-d') }}</p>
+    <p style="color: #a9a9a9; margin-top: 4px; margin-bottom: 0px; font-size: 0.85em;"><i style="vertical-align:middle; font-size: 1.2em; margin-top: -2px; margin-right: 4px;" class="material-icons">remove_red_eye</i>{{ $video->views() }}次 • {{ Carbon\Carbon::parse($video->created_at)->format('Y-m-d') }}</p>
 
-    <div id="toggleVideoDescription" style="margin-top: -31px; padding-top:10px; padding-right:2px;cursor: pointer; color: gray;" class="pull-right"><i id="toggleVideoDescriptionIcon" class="material-icons noselect">expand_more</i></div>
+    <div id="toggleVideoDescription" style="margin-top: -32px; padding-top:10px; padding-right:2px;cursor: pointer; color: gray;" class="pull-right"><i id="toggleVideoDescriptionIcon" class="material-icons noselect">expand_more</i></div>
 
-    <a id="shareBtn" style="margin-top: -26px; padding-right:10px; cursor: pointer; text-decoration: none;" class="pull-right">
+    <a id="shareBtn" style="margin-top: -27px; padding-right:10px; cursor: pointer; text-decoration: none;" class="pull-right">
       <i style="color: gray;-moz-transform: scale(-1, 1);-webkit-transform: scale(-1, 1);-o-transform: scale(-1, 1);-ms-transform: scale(-1, 1);transform: scale(-1, 1);" class="material-icons pull-right noselect">reply</i>
     </a>
 
