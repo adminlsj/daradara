d0d70df15f4dd2aba30b4019c6deda0772fa8546
AppleOPie <u3514481@connect.hku.hk>
2019-11-10 23:28:55 +0800

    Google Adsense

--- resources/views/video/singleShowPost.blade.php
+++ resources/views/video/singleShowPost.blade.php
@@ -15,15 +15,17 @@
       <h4 id="shareBtn-title" style="line-height: 23px; font-weight: 500; margin-top:-10px; margin-bottom: 0px; color: #323232; font-size: 1.25em">{{ $video->title }}</h4>
     </a>
 
-    <p style="color: #a9a9a9; margin-top: 4px; margin-bottom: 0px; font-size: 0.85em;"><i style="vertical-align:middle; font-size: 1.2em; margin-top: -2px; margin-right: 3px;" class="material-icons">remove_red_eye</i>{{ $video->views() }}次 • {{ Carbon\Carbon::parse($video->created_at)->format('Y-m-d') }}</p>
+    <p style="color: #a9a9a9; margin-top: 4px; margin-bottom: 0px; font-size: 0.85em;">
+      <i style="vertical-align:middle; font-size: 1.2em; margin-top: -2px; margin-right: 3px;" class="material-icons">remove_red_eye</i>{{ $video->views() }}次 • {{ Carbon\Carbon::parse($video->created_at)->format('Y-m-d') }}
+    </p>
 
-    <div id="toggleVideoDescription" style="margin-top: -32px; padding-top:10px; padding-right:2px;cursor: pointer; color: gray;" class="pull-right"><i id="toggleVideoDescriptionIcon" class="material-icons noselect">expand_more</i></div>
+    <div id="toggleVideoDescription" style="margin-top: -31px; padding-top:10px; padding-right:2px;cursor: pointer; color: gray;" class="pull-right"><i id="toggleVideoDescriptionIcon" class="material-icons noselect">expand_more</i></div>
 
-    <a id="shareBtn" style="margin-top: -27px; padding-right:10px; cursor: pointer; text-decoration: none;" class="pull-right">
+    <a id="shareBtn" style="margin-top: -26px; padding-right:10px; cursor: pointer; text-decoration: none;" class="pull-right">
       <i style="color: gray;-moz-transform: scale(-1, 1);-webkit-transform: scale(-1, 1);-o-transform: scale(-1, 1);-ms-transform: scale(-1, 1);transform: scale(-1, 1);" class="material-icons pull-right noselect">reply</i>
     </a>
 
     <div id="videoDescription" style="display: none; margin-top: 5px;">
-      <p style="white-space: pre-wrap; color: gray; margin-bottom: 5px;">{{ $video->caption }}</p>
+      <p style="white-space: pre-wrap; color: gray; margin-bottom: 4px;">{{ $video->caption }}</p>
     </div>
 </div>​
--- resources/views/video/trendingVideoPost.blade.php
+++ resources/views/video/trendingVideoPost.blade.php
@@ -4,9 +4,9 @@
             <img style="border-radius: 5px;" src="https://i.imgur.com/{{ $video->imgur }}m.png" width="100%" height="100%">
         </div>
         <div class="col-xs-5 col-sm-5 col-md-5" style="padding-left: 0px; padding-right: 0px;">
-            <h4 style="margin-top: 0px; font-weight: 500; line-height: 19px; font-size: 1em; overflow: hidden;text-overflow: ellipsis; display: -webkit-box; -webkit-line-clamp: 3; -webkit-box-orient: vertical;">{{ $video->title }}</h4>
-            <p style="color: gray; margin-top: -7px; margin-bottom: 0px; font-size: 0.85em;">
-                <i style="vertical-align:middle; font-size: 1.2em; margin-top: -2.5px;" class="material-icons">remove_red_eye</i>
+            <h4 style="margin-top: -1px; font-weight: 500; line-height: 19px; font-size: 1em; overflow: hidden;text-overflow: ellipsis; display: -webkit-box; -webkit-line-clamp: 3; -webkit-box-orient: vertical;">{{ $video->title }}</h4>
+            <p style="color: #a9a9a9; margin-top: -8px; margin-bottom: 0px; font-size: 0.8em;">
+                <i style="vertical-align:middle; font-size: 1.15em; margin-top: -2.5px;" class="material-icons">remove_red_eye</i>
                 {{ $video->views() }}次 • {{ Carbon\Carbon::parse($video->created_at)->format('Y-m-d') }}
             </p>
         </div>
