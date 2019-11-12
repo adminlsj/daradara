fc174cec78ba93f318878696d5985c52686be4ee
AppleOPie <u3514481@connect.hku.hk>
2019-11-12 21:01:26 +0800

    Google Adsense

--- resources/views/video/player.blade.php
+++ resources/views/video/player.blade.php
@@ -3,33 +3,6 @@
       poster='https://i.imgur.com/{{ $video->imgur }}l.png'
       oncontextmenu="return false;"
       data-setup='{ "aspectRatio":"16:9", "controls": true, "playsinline" : true, "autoplay": true, "muted": true, "preload": "auto" }'>
-    <source id="_video" src="{{ $video->hd }}" type='video/mp4' />
+    <source src="{{ $video->hd }}" type='video/mp4' />
   </video>
 </div>
-
-<script type="text/javascript">
-  var xhr = new XMLHttpRequest();
-  xhr.responseType = 'blob';
-
-  xhr.onload = function() {
-    var reader = new FileReader();
-    
-    reader.onloadend = function() {
-      var byteCharacters = atob(reader.result.slice(reader.result.indexOf(',') + 1));
-      var byteNumbers = new Array(byteCharacters.length);
-      for (var i = 0; i < byteCharacters.length; i++) {
-        byteNumbers[i] = byteCharacters.charCodeAt(i);
-      }
-      var byteArray = new Uint8Array(byteNumbers);
-      var blob = new Blob([byteArray], {type: 'video/mp4'});
-      var url = URL.createObjectURL(blob);
-      
-      document.getElementById('_video').src = url;
-    }
-    
-    reader.readAsDataURL(xhr.response);
-  };
-
-  xhr.open('GET', '{{ $video->hd }}');
-  xhr.send();
-</script>
