46350ea7ee1eea8c33e3a619e6e7bd10a43d56d9
AppleOPie <u3514481@connect.hku.hk>
2019-12-19 01:40:56 +0800

    Google Adsense

--- resources/views/video/singleHomeVideo.blade.php
+++ resources/views/video/singleHomeVideo.blade.php
@@ -6,7 +6,7 @@
 	<div style="position: relative; height: 50px;">
 	    <span style="position: absolute; top:-28px; right: 6px; background-color: rgba(0,0,0,0.8); color: white; padding: 1px 5px 1px 5px; font-size: 0.85em; border-radius: 2px; opacity: 0.9">{{ $video->duration() }}</span>
 	    <div>
-	        <a style="color: #191919; text-decoration: none;" href="{{ route('video.trending') }}?v={{ $video->id }}">
+	        <a style="color: #323232; text-decoration: none;" href="{{ route('video.trending') }}?v={{ $video->id }}">
 	          <h4 style="margin-top: 5px; font-weight: 500; line-height: 19px; font-size: 1em;overflow: hidden;text-overflow: ellipsis; display: -webkit-box; -webkit-line-clamp: 2; -webkit-box-orient: vertical;">{{ $video->title }}</h4>
 	        </a>
 
