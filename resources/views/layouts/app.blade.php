344a09cb109aaeb365593d99601c3ed42308beb7
AppleOPie <u3514481@connect.hku.hk>
2019-12-24 13:56:29 +0800

    Google Adsense

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,7 +35,7 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
 </head>
 <body>
-    <div style="background-color:{{ Request::is('*watch*') && $is_program ? '#414141' : 'white' }};">
+    <div style="background-color:{{ !Request::is('/') && !Request::is('*rank*') && !Request::is('*search*') && $is_program ? '#414141' : 'white' }};">
         <div>@include('layouts.nav')</div>
         <div style="margin-top: 50px;" class="responsive-frame">
             @yield('content')
