191eca4eabc61ad942d426d802e12e9f36ca9e6a
AppleOPie <u3514481@connect.hku.hk>
2019-12-28 23:55:32 +0800

    Google Adsense

--- app/Watch.php
+++ app/Watch.php
@@ -13,7 +13,7 @@ class Watch extends Model
 
     public function videos()
     {
-        return Blog::where('category', $this->category)->get();
+        return Blog::where('category', $this->category)->orderBy('created_at', 'asc')->get();
     }
 
     public function titleToURL()
