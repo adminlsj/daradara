32f8e27380d81ecb14048f6f8878e715b2190f8b
AppleOPie <u3514481@connect.hku.hk>
2020-02-16 05:55:00 +0800

    Google Adsense

--- app/Video.php
+++ app/Video.php
@@ -168,6 +168,8 @@ class Video extends Model
                 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:56.0) Gecko/20100101 Firefox/56.0',
                 'Host: api.bilibili.com',
                 'Cookie: SESSDATA=1feadc09%2C1582358038%2Ca8f2f511;',
+                'X_FORWARDED_FOR: 128.1.62.197:80',
+                'REMOTE_ADDR: 128.1.62.197:80'
             ]);
             $data = json_decode(curl_exec($curl_connection), true);
             curl_close($curl_connection);
