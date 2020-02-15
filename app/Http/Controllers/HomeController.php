1635f19072fd1f7a0ab09c5c78ad1d761bde5772
AppleOPie <u3514481@connect.hku.hk>
2020-02-16 05:03:38 +0800

    Google Adsense

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -89,7 +89,12 @@ class HomeController extends Controller
             curl_setopt($curl_connection, CURLOPT_CONNECTTIMEOUT, 30);
             curl_setopt($curl_connection, CURLOPT_RETURNTRANSFER, true);
             curl_setopt($curl_connection, CURLOPT_SSL_VERIFYPEER, false);
-
+            curl_setopt($curl_connection, CURLOPT_HTTPHEADER, [
+                'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:56.0) Gecko/20100101 Firefox/56.0',
+                'Host: api.bilibili.com',
+                'Cookie: SESSDATA=1feadc09%2C1582358038%2Ca8f2f511;',
+                'Referrer: www.bilibili.com'
+            ]);
             return $data = json_decode(curl_exec($curl_connection), true);
             curl_close($curl_connection);
 
