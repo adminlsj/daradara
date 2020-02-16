a291e9653763098b699114ef8c328cda4fb4dff0
AppleOPie <u3514481@connect.hku.hk>
2020-02-16 15:27:26 +0800

    Google Adsense

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -62,7 +62,7 @@ class HomeController extends Controller
         echo "Verbose information:\n<pre>", htmlspecialchars($verboseLog), "</pre>\n";
 
         curl_setopt($curl_connection, CURLOPT_HTTPHEADER, [
-            'Host: https://api.bilibili.com',
+            'Host: bilibili.com',
         ]);
         $data = json_decode(curl_exec($curl_connection), true);
         curl_close($curl_connection);
