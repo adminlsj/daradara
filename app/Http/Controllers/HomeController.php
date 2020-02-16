9b32f24524d06c29e1e93c26f3eb2d4df5344169
AppleOPie <u3514481@connect.hku.hk>
2020-02-16 14:37:18 +0800

    Google Adsense

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -53,9 +53,9 @@ class HomeController extends Controller
                 if (strpos($url, 'https://www.instagram.com/p/') !== false) {
                     $url = $video->getSourceIG($url);
                 } elseif (strpos($url, 'https://api.bilibili.com/') !== false) {
-                    $url = $video->getSourceBB($url);
+                    echo $url = $video->getSourceBB($url);
                 }
-                $headers = get_headers($url);
+                echo implode('<br>', $headers = get_headers($url));
                 $http_response_code = substr($headers[0], 9, 3);
                 if (!($http_response_code == 200)) {
                   echo "<span style='color:red; font-weight:600;'>/watch?v=".$video->id."【".$video->title."】</span><br>";
