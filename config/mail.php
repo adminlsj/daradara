2379866067a7eb01f317417008be98c4b3b4c4a7
AppleOPie <u3514481@connect.hku.hk>
2020-02-22 12:07:03 +0800

    Google Adsense

--- config/mail.php
+++ config/mail.php
@@ -57,7 +57,7 @@ return [
 
     'from' => [
         'address' => env('MAIL_FROM_ADDRESS', 'twobayjobs@gmail.com'),
-        'name' => env('MAIL_FROM_NAME', 'LaughSeeJapan'),
+        'name' => env('MAIL_FROM_NAME', 'Twobayjobs'),
     ],
 
     'reply_to' => [
