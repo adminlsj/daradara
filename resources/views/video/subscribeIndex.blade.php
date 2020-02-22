c884bb52ed929324bfeee875dcb305025618dad8
AppleOPie <u3514481@connect.hku.hk>
2020-02-22 20:22:08 +0800

    Google Adsense

--- resources/views/video/subscribeIndex.blade.php
+++ resources/views/video/subscribeIndex.blade.php
@@ -22,8 +22,8 @@
 		</div>
 		<hr style="margin: 0px 0px 20px 0px; border-color: #e5e5e5;">
 		<div class="subscribes-tab padding-setup">
-			<a style="margin-right: 5px;">最新内容</a>
-			<a>儲存的影片</a>
+			<a href="{{ route('video.subscribes') }}?m=newest" class="{{ Request::is('*subscribes*') ? 'active' : '' }}" style="margin-right: 5px;">最新内容</a>
+			<!-- <a href="#">儲存的影片</a>-->
 		</div>
 		<hr style="margin: 20px 0px 0px 0px; border-color: #e5e5e5;">
 		@foreach ($videos as $video)
