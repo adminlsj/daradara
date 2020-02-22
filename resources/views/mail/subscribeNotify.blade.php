5bf201409a0ec4444520796534a080b7776459fc
AppleOPie <u3514481@connect.hku.hk>
2020-02-23 00:21:44 +0800

    Google Adsense

--- resources/views/mail/subscribeNotify.blade.php
+++ resources/views/mail/subscribeNotify.blade.php
@@ -1,18 +1,18 @@
 @component('mail::message')
 
 <div>
-	<a href="https://www.laughseejapan.com"><img src="https://i.imgur.com/M8tqx5K.png" height="30" alt="{{ $video->watch()->title }} 更新了 "></a>
+	<a href="https://www.laughseejapan.com"><img src="https://i.imgur.com/M8tqx5K.png" height="30"></a>
 </div>
 
 <div style="margin-top: 15px">
 	<a href="https://www.laughseejapan.com/watch?v={{ $video->id }}" style="text-decoration: none;">
-		<img src="https://i.imgur.com/{{ $video->imgur }}h.png" alt="{{ $video->title }}">
+		<img src="https://i.imgur.com/{{ $video->imgur }}h.png">
 	</a>
 </div>
 
 <div style="margin-top: 3px;">
 	<a href="https://www.laughseejapan.com/{{ $video->genre }}/{{ $video->watch()->titleToUrl() }}" style="text-decoration: none; color: black;">
-		<img style="width: 30px; height: auto; float: left; border-radius: 50%;" src="https://i.imgur.com/{{ $video->watch()->imgur }}s.jpg" alt="{{ $video->watch()->title }}">
+		<img style="width: 30px; height: auto; float: left; border-radius: 50%;" src="https://i.imgur.com/{{ $video->watch()->imgur }}s.jpg" alt="{{ $video->watch()->title.' 更新了 ' }}">
 	</a>
 	<a href="https://www.laughseejapan.com/watch?v={{ $video->id }}" style="text-decoration: none; color: black;">
 		<div style="margin-left: 38px; font-size: 0.95em;">{{ $video->title }}</div>
