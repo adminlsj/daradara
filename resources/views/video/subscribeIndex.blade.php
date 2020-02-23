4333c6ef9252d06ffce770bcc201bada3cbc3ede
AppleOPie <u3514481@connect.hku.hk>
2020-02-23 13:57:05 +0800

    Google Adsense

--- resources/views/video/subscribeIndex.blade.php
+++ resources/views/video/subscribeIndex.blade.php
@@ -37,8 +37,8 @@
 						<img style="width: 45px; height: auto; float: left; border-radius: 50%;" src="https://i.imgur.com/{{ $video->watch()->imgur }}s.jpg" alt="{{ $video->watch()->title }}">
 					</a>
 					<a href="{{ route('video.watch') }}?v={{ $video->id }}" style="text-decoration: none; color: black;">
-						<div style="margin-left: 53px; font-size: 1.1em;">{{ $video->title }}</div>
-						<div style="margin-left: 53px; font-size: 0.85em; color: dimgray; margin-top: 2px;">{{ $video->watch()->title }} • 收看次數：{{ $video->views() }} • {{ Carbon\Carbon::parse($video->created_at)->diffForHumans() }}</div>
+						<div style="margin-left: 53px; font-size: 1.1em; line-height: 19px">{{ $video->title }}</div>
+						<div style="margin-left: 53px; font-size: 0.85em; color: dimgray; margin-top: 3px;">{{ $video->watch()->title }} • 收看次數：{{ $video->views() }} • {{ Carbon\Carbon::parse($video->created_at)->diffForHumans() }}</div>
 					</a>
 				</div>
 			</div>
