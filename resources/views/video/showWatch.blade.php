4dae4881d208bb0ac446355d56c09ab5f33c747e
AppleOPie <u3514481@connect.hku.hk>
2020-02-24 02:27:19 +0800

    Google Adsense

--- resources/views/video/showWatch.blade.php
+++ resources/views/video/showWatch.blade.php
@@ -45,12 +45,13 @@
 
 							<div style="margin: 7px 15px;">
 								<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+								<!-- Horizontal Banner Ads -->
 								<ins class="adsbygoogle"
-								     style="display:block"
-								     data-ad-format="fluid"
-								     data-ad-layout-key="-a0+ev-53-bk+14i"
+								     style="display:block;"
 								     data-ad-client="ca-pub-4485968980278243"
-								     data-ad-slot="9284406342"></ins>
+								     data-ad-slot="8455082664"
+								     data-ad-format="auto"
+								     data-full-width-responsive="true"></ins>
 								<script>
 								     (adsbygoogle = window.adsbygoogle || []).push({});
 								</script>
