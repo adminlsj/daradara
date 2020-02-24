b7ca9b325ee76e0083d5ff51eae6af08333d5438
AppleOPie <u3514481@connect.hku.hk>
2020-02-24 19:07:52 +0800

    Google Adsense

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -88,4 +88,22 @@ class RegisterController extends Controller
 
         return $user;
     }
+
+    /**
+     * Handle a registration request for the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function register(Request $request)
+    {
+        $this->validator($request->all())->validate();
+
+        event(new Registered($user = $this->create($request->all())));
+
+        $this->guard()->login($user, true);
+
+        return $this->registered($request, $user)
+                        ?: redirect($this->redirectPath());
+    }
 }
