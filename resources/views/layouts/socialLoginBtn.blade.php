6679f89f753e2fa9af11a432641084a3a899d217
AppleOPie <u3514481@connect.hku.hk>
2020-02-24 15:12:12 +0800

    Google Adsense

--- resources/views/layouts/socialLoginBtn.blade.php
+++ resources/views/layouts/socialLoginBtn.blade.php
@@ -2,17 +2,17 @@
 <div class="row" style="text-align: center">
   <div class="col-xs-6">
     <a href="{{ url('/auth/facebook') }}" style="text-decoration: none; color: #4267B2">
-      <div style="padding: 10px 15px; border: 1px solid #e9e9e9; margin-right: 4px;">
+      <div style="padding: 10px; border: 1px solid #e9e9e9; margin-right: 4px;">
         <i style="float: left; margin-top: -2px; font-size: 1.8em;" class="fab fa-facebook"></i>
-        <span style="font-size: 0.95em; font-weight: 500;">Facebook 登入</span>
+        <span style="font-size: 0.95em; font-weight: 500;"><span class="hidden-xs hidden-sm">Facebook </span>登入</span>
       </div>
     </a>
   </div>
   <div class="col-xs-6">
     <a href="{{ url('/auth/google') }}" style="text-decoration: none; color: red">
-      <div style="padding: 10px 15px; border: 1px solid #e9e9e9; margin-left: 4px;">
+      <div style="padding: 10px; border: 1px solid #e9e9e9; margin-left: 4px;">
         <i style="float: left; margin-top: 0px; font-size: 1.60em;" class="fab fa-google"></i>
-        <span style="font-size: 0.95em; font-weight: 500;">Google 登入</span>
+        <span style="font-size: 0.95em; font-weight: 500;"><span class="hidden-xs hidden-sm">Google </span>登入</span>
       </div>
     </a>
   </div>
