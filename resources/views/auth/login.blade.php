88fdf84ec619930bcb919398839ae6fa81fda4e1
AppleOPie <u3514481@connect.hku.hk>
2020-02-24 13:43:52 +0800

    Google Adsense

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -36,6 +36,7 @@
         </form>
 
         <a class="btn social-signin facebook" href="{{ url('/auth/facebook') }}">Log in with Facebook</a>
+        <a class="btn social-signin facebook" href="{{ url('/auth/google') }}">Log in with Google</a>
 
     </div>
 </div>
