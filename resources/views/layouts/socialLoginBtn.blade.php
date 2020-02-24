ca806ad460e7877d73a170a121e4ca157ab480d4
AppleOPie <u3514481@connect.hku.hk>
2020-02-24 15:19:51 +0800

    Google Adsense

--- resources/views/layouts/socialLoginBtn.blade.php
+++ resources/views/layouts/socialLoginBtn.blade.php
@@ -4,7 +4,7 @@
     <a href="{{ url('/auth/facebook') }}" style="text-decoration: none; color: #4267B2">
       <div style="padding: 10px; border: 1px solid #e9e9e9; margin-right: 4px;">
         <i style="float: left; margin-top: -2px; font-size: 1.8em;" class="fab fa-facebook"></i>
-        <span style="font-size: 0.95em; font-weight: 500;"><span class="hidden-xs hidden-sm">Facebook </span>登入</span>
+        <span style="font-size: 0.95em; font-weight: 500;">Facebook<span class="hidden-xs hidden-sm"> 登入</span></span>
       </div>
     </a>
   </div>
@@ -12,7 +12,7 @@
     <a href="{{ url('/auth/google') }}" style="text-decoration: none; color: red">
       <div style="padding: 10px; border: 1px solid #e9e9e9; margin-left: 4px;">
         <i style="float: left; margin-top: 0px; font-size: 1.60em;" class="fab fa-google"></i>
-        <span style="font-size: 0.95em; font-weight: 500;"><span class="hidden-xs hidden-sm">Google </span>登入</span>
+        <span style="font-size: 0.95em; font-weight: 500;">Google<span class="hidden-xs hidden-sm"> 登入</span></span>
       </div>
     </a>
   </div>
