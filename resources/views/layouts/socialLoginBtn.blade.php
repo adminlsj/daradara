819939381407c6803be1d8d569272bd50207df9c
AppleOPie <u3514481@connect.hku.hk>
2020-02-25 03:05:15 +0800

    Google Adsense

--- resources/views/layouts/socialLoginBtn.blade.php
+++ resources/views/layouts/socialLoginBtn.blade.php
@@ -2,7 +2,7 @@
 <div class="row" style="text-align: center">
   <div class="col-xs-12 col-md-6">
     <a href="{{ url('/auth/facebook') }}" style="text-decoration: none; color: #4267B2">
-      <div class="social-login-wrapper-left" style="padding: 10px 15px; border: 1px solid #e9e9e9;">
+      <div class="social-login-wrapper-left" style="padding: 10px; border: 1px solid #e9e9e9;">
         <i style="float: left; margin-top: -2px; font-size: 1.8em;" class="fab fa-facebook"></i>
         <span style="font-size: 0.95em; font-weight: 500;">Facebook 登入</span>
       </div>
@@ -10,7 +10,7 @@
   </div>
   <div class="col-xs-12 col-md-6">
     <a href="{{ url('/auth/google') }}" style="text-decoration: none; color: crimson">
-      <div class="social-login-wrapper-right" style="padding: 10px 15px; border: 1px solid #e9e9e9;">
+      <div class="social-login-wrapper-right" style="padding: 10px; border: 1px solid #e9e9e9;">
         <i style="float: left; margin-top: 0px; font-size: 1.60em;" class="fab fa-google"></i>
         <span style="font-size: 0.95em; font-weight: 500;">Google 登入</span>
       </div>
