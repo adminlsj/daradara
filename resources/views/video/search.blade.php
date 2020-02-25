6d8591aaf82ef448570046fc9e278897fbd5a568
AppleOPie <u3514481@connect.hku.hk>
2020-02-25 18:34:47 +0800

    Google Adsense

--- resources/views/video/search.blade.php
+++ resources/views/video/search.blade.php
@@ -18,7 +18,7 @@
 </div>
 
 <div class="main-content">
-	<div class="padding-setup" style="background-color: #e9e9e9; min-height: auto; padding-top: 7px; padding-bottom: 7px;">
+	<div class="padding-setup" style="background-color: #e9e9e9; min-height: auto; padding-top: 14px; padding-bottom: 14px;">
 		<div id="search-top-watch">
 		  <a href="{{ route('video.intro', [$watch->genre, $watch->titleToUrl()]) }}" class="row no-gutter">
 		    <div class="col-xs-6 col-sm-6 col-md-3">
@@ -28,7 +28,7 @@
 		    <div class="col-xs-6 col-sm-6 col-md-7">
 		      <h4>{{ $watch->title }}</h4>
 		      <p style="overflow: hidden;text-overflow: ellipsis; display: -webkit-box; -webkit-line-clamp: 1; -webkit-box-orient: vertical;">{{ $watch->cast }}</p>
-		      <p style="margin-top: 9px; overflow: hidden;text-overflow: ellipsis; display: -webkit-box; -webkit-line-clamp: 2; -webkit-box-orient: vertical;">{{ $watch->description }}</p>
+		      <p style="margin-top: 9px; overflow: hidden;text-overflow: ellipsis; display: -webkit-box; -webkit-line-clamp: 3; -webkit-box-orient: vertical;">{{ $watch->description }}</p>
 		    </div>
 		  </a>
 		</div>
