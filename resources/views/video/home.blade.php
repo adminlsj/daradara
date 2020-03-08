458f06af1d20c188877e1b0d7ea9d5b08af4ddbf
AppleOPie <u3514481@connect.hku.hk>
2020-03-08 13:44:40 +0800

    Google Adsense

--- resources/views/video/home.blade.php
+++ resources/views/video/home.blade.php
@@ -11,13 +11,25 @@
 <div class="main-content">
 	<div style="background-color: #F5F5F5;">
 		<div class="padding-setup" style="overflow-x: hidden; padding-top:10px;">
-			<div class="home-ads-wrapper">
+			<div class="hidden-xs hidden-sm">
 				<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-				<!-- Horizontal Banner Ads -->
 				<ins class="adsbygoogle"
-				     style="display:inline-block; width: calc(100%);"
+				     style="display:block;"
 				     data-ad-client="ca-pub-4485968980278243"
-				     data-ad-slot="8455082664"></ins>
+				     data-ad-slot="4478704168"
+				     data-ad-format="auto"
+				     data-full-width-responsive="true"></ins>
+				<script>
+				     (adsbygoogle = window.adsbygoogle || []).push({});
+				</script>
+			</div>
+
+			<div class="hidden-md hidden-lg">
+				<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+				<ins class="adsbygoogle"
+				     style="display:inline-block; width: calc(100%); height:150px;"
+				     data-ad-client="ca-pub-4485968980278243"
+				     data-ad-slot="2160211323"></ins>
 				<script>
 				     (adsbygoogle = window.adsbygoogle || []).push({});
 				</script>
