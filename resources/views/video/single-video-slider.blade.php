2ae081533ddc6cfe3f28343fd4535af1d46cfd7a
AppleOPie <u3514481@connect.hku.hk>
2020-03-09 11:44:50 +0800

    Google Adsense

--- resources/views/video/single-video-slider.blade.php
+++ resources/views/video/single-video-slider.blade.php
@@ -1,9 +1,33 @@
-<div class="swiper-slide multiple-link-wrapper" style="width: 250px !important;">
-    <a style="text-decoration: none; color: black" class="overlay" href="{{ route('video.watch') }}?v={{ $video->id }}"></a>
-    <img class="lazy" style="border-radius: 3px; width: 100%; height: 100%;" src="{{ $video->imgur16by9() }}" data-src="{{ $video->imgurL() }}" data-srcset="{{ $video->imgurL() }}" alt="{{ $video->title }}">
+@if ($is_mobile)
+	<div class="swiper-container">
+	  <div class="swiper-wrapper">
+	    @foreach ($videos as $video)
+	        <div class="swiper-slide multiple-link-wrapper" style="width: 250px !important;">
+			    <a style="text-decoration: none; color: black" class="overlay" href="{{ route('video.watch') }}?v={{ $video->id }}"></a>
+			    <img class="lazy" style="border-radius: 3px; width: 100%; height: 100%;" src="{{ $video->imgur16by9() }}" data-src="{{ $video->imgurL() }}" data-srcset="{{ $video->imgurL() }}" alt="{{ $video->title }}">
 
-    <div class="inner" style="height: 47px;">
-      <h4 style="line-height: 19px; font-size: 1em;overflow: hidden;text-overflow: ellipsis; display: -webkit-box; -webkit-line-clamp: 1; -webkit-box-orient: vertical; font-weight: 400; color: black">{{ $video->title }}</h4>
-      <p style="font-size: 0.85em; margin-top: -9px; color: #595959"><a href="{{ route('video.intro', [$video->genre, $video->watch()->titleToUrl()]) }}">{{ $video->watch()->title }}</a> • {{ Carbon\Carbon::parse($video->uploaded_at)->diffForHumans() }}</p>
+			    <div class="inner" style="height: 47px; margin-top: -4px;">
+			      <h4 style="line-height: 19px; font-size: 1em;overflow: hidden;text-overflow: ellipsis; display: -webkit-box; -webkit-line-clamp: 1; -webkit-box-orient: vertical; font-weight: 400; color: black">{{ $video->title }}</h4>
+			      <p style="font-size: 0.85em; margin-top: -9px; color: #595959"><a href="{{ route('video.intro', [$video->genre, $video->watch()->titleToUrl()]) }}">{{ $video->watch()->title }}</a> • {{ Carbon\Carbon::parse($video->uploaded_at)->diffForHumans() }}</p>
+			    </div>
+			</div>
+	    @endforeach
+	  </div>
+	</div>
+
+@else
+	<div class="row no-gutter" style="padding: 0px 15px">
+        @foreach ($videos as $video)
+          <div style="padding: 0px 5px; margin-bottom: 7px" class="col-md-3 multiple-link-wrapper">
+              <a style="text-decoration: none; color: black" class="overlay" href="{{ route('video.watch') }}?v={{ $video->id }}"></a>
+              <img class="lazy" style="border-radius: 3px; width: 100%; height: 100%;" src="{{ $video->imgur16by9() }}" data-src="{{ $video->imgurL() }}" data-srcset="{{ $video->imgurL() }}" alt="{{ $video->title }}">
+
+              <div class="inner" style="height: 47px; margin-top: -4px;">
+                <h4 style="line-height: 19px; font-size: 1em;overflow: hidden;text-overflow: ellipsis; display: -webkit-box; -webkit-line-clamp: 1; -webkit-box-orient: vertical; font-weight: 400; color: black">{{ $video->title }}</h4>
+                <p style="font-size: 0.85em; margin-top: -9px; color: #595959"><a href="{{ route('video.intro', [$video->genre, $video->watch()->titleToUrl()]) }}">{{ $video->watch()->title }}</a> • {{ Carbon\Carbon::parse($video->uploaded_at)->diffForHumans() }}</p>
+              </div>
+          </div>
+        @endforeach
     </div>
-</div>
+
+@endif
--- resources/views/video/varietyIndex.blade.php
+++ resources/views/video/varietyIndex.blade.php
@@ -18,7 +18,7 @@
     <script src="https://unpkg.com/swiper/js/swiper.min.js"></script>
 
     <div style="padding: 0px 20px; padding-bottom: 10px">
-      <h4>LaughSeeJapan熱門頻道</h4>
+      <h4>LaughSeeJapan熱門頻道<span style="float: right; font-size: 0.85em; margin-top: 1.5px;">更多<i style="margin-left: 1px; vertical-align:middle; font-size: 1em; margin-top: -3px;" class="material-icons">arrow_forward_ios</i></span></h4>
     </div>
     <div class="swiper-container">
       <div class="swiper-wrapper">
@@ -36,64 +36,62 @@
       </div>
     </div>
 
-    <div style="margin-top: 25px; padding: 0px 20px; padding-bottom: 10px">
+    <div style="margin-top: 25px; padding: 0px 20px; padding-bottom: 9px">
       <h4>最新精彩內容</h4>
     </div>
-    <div class="swiper-container">
-      <div class="swiper-wrapper">
-        @foreach ($newest as $video)
-          @include('video.single-video-slider')
-        @endforeach
-      </div>
-    </div>
-    <div style="padding: 0px 20px; margin-top: -4px; text-align: center;" class="col-xs-12 col-sm-12 col-md-2 col-lg-2">
-      <a style="text-decoration: none; color: black; font-size: 1em;" href="{{ route('video.newest') }}?g=variety">
-        <div style="border: 1px solid black; padding: 8px; border-radius: 7px; font-weight: 500">顯示更多</div>
+    @include('video.single-video-slider', ['videos' => $newest])
+    <div class="col-xs-12 col-sm-12 col-md-2 col-lg-2 show-more-btn">
+      <a href="{{ route('video.newest') }}?g=variety">
+        <div>顯示更多</div>
       </a>
     </div>
 
-    <div style="margin-top: 65px; padding: 0px 20px; padding-bottom: 10px">
+    <div style="margin-top: 65px; padding: 0px 20px; padding-bottom: 9px">
       <h4>#明星嘉賓</h4>
     </div>
-    <div class="swiper-container">
-      <div class="swiper-wrapper">
-        @foreach ($artist as $video)
-          @include('video.single-video-slider')
-        @endforeach
-      </div>
-    </div>
-    <div style="padding: 0px 20px; margin-top: -4px; text-align: center;" class="col-xs-12 col-sm-12 col-md-2 col-lg-2">
-      <a style="text-decoration: none; color: black; font-size: 1em;" href="{{ route('video.search') }}?query=明星">
-        <div style="border: 1px solid black; padding: 8px; border-radius: 7px; font-weight: 500">顯示更多</div>
+    @include('video.single-video-slider', ['videos' => $artist])
+    <div class="col-xs-12 col-sm-12 col-md-2 col-lg-2 show-more-btn">
+      <a href="{{ route('video.search') }}?query=明星">
+        <div>顯示更多</div>
       </a>
     </div>
 
-    <div style="margin-top: 65px; padding: 0px 20px; padding-bottom: 10px">
+    <div style="margin-top: 65px; padding: 0px 20px; padding-bottom: 9px">
       <h4>#整人企劃</h4>
     </div>
-    <div class="swiper-container">
-      <div class="swiper-wrapper">
-        @foreach ($trick as $video)
-          @include('video.single-video-slider')
-        @endforeach
-      </div>
-    </div>
-    <div style="padding: 0px 20px; margin-top: -4px; text-align: center;" class="col-xs-12 col-sm-12 col-md-2 col-lg-2">
-      <a style="text-decoration: none; color: black; font-size: 1em;" href="{{ route('video.search') }}?query=整人">
-        <div style="border: 1px solid black; padding: 8px; border-radius: 7px; font-weight: 500">顯示更多</div>
+    @include('video.single-video-slider', ['videos' => $trick])
+    <div class="col-xs-12 col-sm-12 col-md-2 col-lg-2 show-more-btn">
+      <a href="{{ route('video.search') }}?query=整人">
+        <div>顯示更多</div>
       </a>
     </div>
+
+    <div style="margin-top: 65px; padding: 0px 20px; padding-bottom: 9px">
+      <h4>更多發燒影片</h4>
+    </div>
+    <div class="row no-gutter" style="padding: 0px 15px">
+      <div class="video-sidebar-wrapper">
+          <div id="sidebar-results"><!-- results appear here --></div>
+          <div style="text-align: center;" class="ajax-loading"><img style="width: 40px; height: auto; padding-top: 25px; padding-bottom: 45px;" src="https://i.imgur.com/TcZjkZa.gif"/></div>
+      </div>
+    </div>
+  </div>
 
     <!-- Initialize Swiper -->
     <script>
       var swiper = new Swiper('.swiper-container', {
         slidesPerView: 'auto',
         freeMode: true,
+        mousewheel: true,
         spaceBetween: 10,
       });
     </script>
 
-    <br><br><br>
   </div>
 </div>
+@endsection
+
+@section('script')
+  @parent
+  <script src="{{ mix('js/loadMore.js') }}"></script>
 @endsection
