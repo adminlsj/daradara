33f04d5072f2a391e8e1a9aa9d7a3dd879dba89a
AppleOPie <u3514481@connect.hku.hk>
2020-03-09 11:54:46 +0800

    Google Adsense

--- resources/views/video/single-video-slider.blade.php
+++ resources/views/video/single-video-slider.blade.php
@@ -7,8 +7,8 @@
 			    <img class="lazy" style="border-radius: 3px; width: 100%; height: 100%;" src="{{ $video->imgur16by9() }}" data-src="{{ $video->imgurL() }}" data-srcset="{{ $video->imgurL() }}" alt="{{ $video->title }}">
 
 			    <div class="inner" style="height: 47px; margin-top: -4px;">
-			      <h4 style="line-height: 19px; font-size: 1em;overflow: hidden;text-overflow: ellipsis; display: -webkit-box; -webkit-line-clamp: 1; -webkit-box-orient: vertical; font-weight: 400; color: black">{{ $video->title }}</h4>
-			      <p style="font-size: 0.85em; margin-top: -9px; color: #595959"><a href="{{ route('video.intro', [$video->genre, $video->watch()->titleToUrl()]) }}">{{ $video->watch()->title }}</a> • {{ Carbon\Carbon::parse($video->uploaded_at)->diffForHumans() }}</p>
+			      <h4 style="line-height: 19px; font-size: 1.05em;overflow: hidden;text-overflow: ellipsis; display: -webkit-box; -webkit-line-clamp: 1; -webkit-box-orient: vertical; font-weight: 400; color: black">{{ $video->title }}</h4>
+			      <p style="font-size: 0.9em; margin-top: -9px; color: #595959"><a href="{{ route('video.intro', [$video->genre, $video->watch()->titleToUrl()]) }}">{{ $video->watch()->title }}</a> • {{ Carbon\Carbon::parse($video->uploaded_at)->diffForHumans() }}</p>
 			    </div>
 			</div>
 	    @endforeach
@@ -23,8 +23,8 @@
               <img class="lazy" style="border-radius: 3px; width: 100%; height: 100%;" src="{{ $video->imgur16by9() }}" data-src="{{ $video->imgurL() }}" data-srcset="{{ $video->imgurL() }}" alt="{{ $video->title }}">
 
               <div class="inner" style="height: 47px; margin-top: -4px;">
-                <h4 style="line-height: 19px; font-size: 1em;overflow: hidden;text-overflow: ellipsis; display: -webkit-box; -webkit-line-clamp: 1; -webkit-box-orient: vertical; font-weight: 400; color: black">{{ $video->title }}</h4>
-                <p style="font-size: 0.85em; margin-top: -9px; color: #595959"><a href="{{ route('video.intro', [$video->genre, $video->watch()->titleToUrl()]) }}">{{ $video->watch()->title }}</a> • {{ Carbon\Carbon::parse($video->uploaded_at)->diffForHumans() }}</p>
+                <h4 style="line-height: 19px; font-size: 1.05em;overflow: hidden;text-overflow: ellipsis; display: -webkit-box; -webkit-line-clamp: 1; -webkit-box-orient: vertical; font-weight: 400; color: black">{{ $video->title }}</h4>
+                <p style="font-size: 0.9em; margin-top: -9px; color: #595959"><a href="{{ route('video.intro', [$video->genre, $video->watch()->titleToUrl()]) }}">{{ $video->watch()->title }}</a> • {{ Carbon\Carbon::parse($video->uploaded_at)->diffForHumans() }}</p>
               </div>
           </div>
         @endforeach
