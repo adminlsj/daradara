067b65faceebedfce7317018844470bd86373781
AppleOPie <u3514481@connect.hku.hk>
2020-03-12 15:57:47 +0800

    Google Adsense

--- resources/views/video/singleVideoComment.blade.php
+++ resources/views/video/singleVideoComment.blade.php
@@ -1,6 +1,6 @@
 <a href="{{ route('user.show', $comment->user()) }}">
   <img class="img-circle" style="width: 35px; height: auto; float:left;" src="{{ $comment->user()->avatar == null ? $comment->user()->avatarDefault() : $comment->user()->avatar->filename }}">
 </a>
-<div style="padding-left: 45px; font-size: 0.9em;"><a style="text-decoration: none; color: gray;" href="{{ route('user.show', $comment->user()) }}">{{ $comment->user()->name }} • <span style="color: gray;">{{ Carbon\Carbon::parse($comment->created_at)->diffForHumans() }}</span></a></div>
-<div id="comment-index-text" style="color: white; font-size: 1em; margin-top: 1px;">{{ $comment->text }}</div>
+<div class="comment-index-text" style="font-size: 0.9em;"><a style="text-decoration: none; color: gray;" href="{{ route('user.show', $comment->user()) }}">{{ $comment->user()->name }} • <span style="color: gray;">{{ Carbon\Carbon::parse($comment->created_at)->diffForHumans() }}</span></a></div>
+<div class="comment-index-text" style="color: white; font-size: 1em; margin-top: 1px;">{{ $comment->text }}</div>
 <br>
