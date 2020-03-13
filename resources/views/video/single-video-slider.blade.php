4b9a6b676865ceebf698f7aba417a13794b7e95a
AppleOPie <u3514481@connect.hku.hk>
2020-03-13 22:01:56 +0800

    Google Adsense

--- resources/views/video/single-video-slider.blade.php
+++ resources/views/video/single-video-slider.blade.php
@@ -7,7 +7,7 @@
 
 			    <div class="inner" style="height: 66px; margin-top: -4px;">
 			      <h4 style="line-height: 19px; font-size: 1.05em;overflow: hidden;text-overflow: ellipsis; display: -webkit-box; -webkit-line-clamp: 2; -webkit-box-orient: vertical; font-weight: 400; color: black; white-space: initial;">{{ $video->title }}</h4>
-			      <p style="font-size: 0.9em; margin-top: -9px; color: #595959"><a href="{{ route('video.intro', [$video->genre, $video->watch()->titleToUrl()]) }}">{{ $video->watch()->title }}</a> • {{ Carbon\Carbon::parse($video->uploaded_at)->diffForHumans() }}</p>
+			      <p style="font-size: 0.9em; margin-top: -9px; color: #595959"><a href="{{ route('video.intro', [$video->genre, $video->watch()->titleToUrl()]) }}">{{ str_limit($video->watch()->title, 30) }}</a> • {{ Carbon\Carbon::parse($video->uploaded_at)->diffForHumans() }}</p>
 			    </div>
 			</div>
 	    @endforeach
