4582debda04f1fd0a147b7cf0b7270f253bff749
AppleOPie <u3514481@connect.hku.hk>
2020-03-13 14:01:28 +0800

    Google Adsense

--- app/Video.php
+++ app/Video.php
@@ -196,7 +196,7 @@ class Video extends Model
         }*/
 
         $url = 'https://www.bilibili.com/video/av84400542';
-        $cmd = ' youtube-dl -g ' . escapeshellarg($url);
+        $cmd = ' youtube-dl --geo-bypass-country HK -g ' . escapeshellarg($url);
         return exec($cmd, $outputsd);
         return $results = print_r($outputsd[0], true);
     }
