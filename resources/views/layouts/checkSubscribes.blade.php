88f70fe69794c5a99000eedb577176c41db87eea
AppleOPie <u3514481@connect.hku.hk>
2020-03-19 21:40:02 +0800

    Google Adsense

--- resources/views/layouts/checkSubscribes.blade.php
+++ resources/views/layouts/checkSubscribes.blade.php
@@ -2,6 +2,7 @@
   <thead>
     <tr>
       <th scope="col">Rank</th>
+      <th scope="col">Genre</th>
       <th scope="col">Tag</th>
       <th scope="col">Subscribes</th>
     </tr>
@@ -10,6 +11,7 @@
   	@foreach ($rankings as $rank)
 	    <tr style="background-color: {{ $loop->iteration % 2 == 0 ? '#e5e5e5' : 'white' }}">
 	      <th scope="row">{{ $loop->iteration }}</th>
+        <td>{{ App\Watch::where('title', $rank['tag'])->first() ? App\Watch::where('title', $rank['tag'])->first()->genre() : 'TAG' }}</td>
 	      <td>{{ $rank['tag'] }}</td>
 	      <td style="text-align: center">{{ $rank['count'] }}</td>
 	    </tr>
