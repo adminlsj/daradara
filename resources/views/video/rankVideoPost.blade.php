107dd26294a149d8d1d0ec1159150a2d133b3b80
AppleOPie <u3514481@connect.hku.hk>
2020-03-25 21:29:16 +0800

    Google Adsense

--- resources/views/video/rankVideoPost.blade.php
+++ resources/views/video/rankVideoPost.blade.php
@@ -5,14 +5,14 @@
             <img class="lazy" style="border-radius: 5px; width: 100%; height: 100%;" src="{{ $video->imgur16by9() }}" data-src="{{ $video->imgurL() }}" data-srcset="{{ $video->imgurL() }}" alt="{{ $video->title }}">
         </div>
         <div class="col-xs-6 col-sm-6 col-md-8 col-lg-9" style="padding-top: 0px; padding-left: 5px;">
-            <h4>{{ $video->title }}</h4>
+            <h4 style="overflow: hidden; text-overflow: ellipsis; display: -webkit-box; -webkit-line-clamp: 3; -webkit-box-orient: vertical;">{{ $video->title }}</h4>
             <p>
             @if ($video->category == 'video')
               {{ $video->genre() }} • 
             @else
               <a class="text-ellipsis-mobile" href="{{ route('video.intro', [$video->genre, $video->watch()->titleToUrl()]) }}">{{ $video->watch()->title }} • </a>
             @endif
-            觀看次數：{{ $video->views() }}次 • {{ Carbon\Carbon::parse($video->uploaded_at)->diffForHumans() }}</p>
+            <span class="text-ellipsis-mobile">觀看次數：{{ $video->views() }}次 • {{ Carbon\Carbon::parse($video->uploaded_at)->diffForHumans() }}</p></span>
             <p style="margin-top: 9px; overflow: hidden;text-overflow: ellipsis; display: -webkit-box; -webkit-line-clamp: 2; -webkit-box-orient: vertical;" class="hidden-xs hidden-sm">{{ $video->caption }}</p>
         </div>
     </div>
