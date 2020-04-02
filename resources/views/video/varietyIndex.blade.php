4afe16a940de8bb734806edd5a6a97a150bf814e
AppleOPie <u3514481@connect.hku.hk>
2020-04-02 15:38:09 +0800

    Google Adsense

--- resources/views/video/varietyIndex.blade.php
+++ resources/views/video/varietyIndex.blade.php
@@ -13,21 +13,10 @@
   @include('layouts.nav-index')
   <div style="background-color: #F5F5F5; padding-top: 20px; {{ Request::is('drama') || Request::is('anime') ? 'margin-top: 27px;' : '' }}">
     <div style="padding: 0px 20px; padding-bottom: 8px">
-      <h4>LaughSeeJapan熱門頻道<a href="{{ route('video.varietyList') }}" style="float: right; text-decoration: none; color: black"><i style="vertical-align:middle; font-size: 1em; margin-top: -3.5px;" class="material-icons">arrow_forward_ios</i></a></h4>
+      <h4>LaughSeeJapan熱門頻道</h4>
     </div>
-    <div id="custom-scroll-slider">
-      @foreach ($selected as $watch)
-        <div style="border-radius: 10px; box-shadow: 1px 4px 6px rgba(0,0,0,0.1); width: 150px !important; background: #fff; display: inline-block; vertical-align: text-top;">
-          <a style="text-decoration: none; color: black" href="{{ route('video.intro', [$watch->genre, $watch->titleToUrl()]) }}">
-            <img class="lazy" style="width: 100%; height: 100%; border-top-left-radius: 10px; border-top-right-radius: 10px;" src="{{ $watch->imgurDefault() }}" data-src="{{ $watch->imgurL() }}" data-srcset="{{ $watch->imgurL() }}" alt="{{ $watch->title }}">
-
-            <div style="height: 47px; padding: 2px 15px;">
-              <h4 style="line-height: 19px; font-size: 1em;overflow: hidden;text-overflow: ellipsis; display: -webkit-box; -webkit-line-clamp: 1; -webkit-box-orient: vertical; font-weight: 450; white-space: initial">{{ $watch->title }}</h4>
-            </div>
-          </a>
-        </div>
-      @endforeach
-    </div>
+    @include('video.single-watch-slider')
+    
 
     <div style="margin-top: 25px; padding: 0px 20px; padding-bottom: 9px">
       <h4>最夯發燒影片</h4>
