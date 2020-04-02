435f2f70f0f5d87a2433d9ee969d2f488f492086
AppleOPie <u3514481@connect.hku.hk>
2020-04-02 16:06:32 +0800

    Google Adsense

--- resources/views/video/single-watch-slider.blade.php
+++ resources/views/video/single-watch-slider.blade.php
@@ -16,8 +16,8 @@
 @else
   <div id="custom-scroll-slider" style="position: relative;">
     @foreach ($selected as $watch)
-      @if ($loop->iteration <= 6)
-        <div style="border-radius: 10px; box-shadow: 1px 4px 6px rgba(0,0,0,0.1); width: calc((100% - 190px) / 6) !important; background: #fff; display: inline-block; vertical-align: text-top;">
+      @if ($loop->iteration <= 7)
+        <div style="border-radius: 10px; box-shadow: 1px 4px 6px rgba(0,0,0,0.1); width: calc((100% - 140px) / 7) !important; background: #fff; display: inline-block; vertical-align: text-top;">
           <a style="text-decoration: none; color: black" href="{{ route('video.intro', [$watch->genre, $watch->titleToUrl()]) }}">
             <img class="lazy" style="width: 100%; height: 100%; border-top-left-radius: 10px; border-top-right-radius: 10px;" src="{{ $watch->imgurDefault() }}" data-src="{{ $watch->imgurL() }}" data-srcset="{{ $watch->imgurL() }}" alt="{{ $watch->title }}">
 
@@ -27,12 +27,12 @@
           </a>
         </div>
       @endif
-      @if ($loop->iteration == 7)
-        <a href="{{ route('video.varietyList') }}" style="color:black; width: 64px; height:64px; background-color: white; box-shadow: 1px 4px 6px rgba(0,0,0,0.1); border-radius: 50%; text-align: center; position: absolute; top:calc(50% - 35px); right: 30px">
-          <i style="vertical-align:middle; font-size: 2em; margin-top: 19px; margin-left: 3px;" class="material-icons">arrow_forward_ios</i>
+      @if ($loop->iteration == 8)
+        <a class="variety-list-btn" href="{{ route('video.varietyList') }}" style="color:black; width: 60px; height:60px; background-color: white; box-shadow: 1px 4px 6px rgba(0,0,0,0.1); border-radius: 50%; text-align: center; position: absolute; top:calc(50% - 35px); right: 20px">
+          <i style="vertical-align:middle; font-size: 1.9em; margin-top: 18px; margin-left: 3px;" class="material-icons variety-list-arrow">arrow_forward_ios</i>
         </a>
       @endif
     @endforeach
   </div>
-  
+
 @endif
