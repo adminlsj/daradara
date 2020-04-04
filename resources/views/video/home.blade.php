8402a4c1961db4cb4ad8c767748603949985c05f
AppleOPie <u3514481@connect.hku.hk>
2020-04-04 22:56:23 +0800

    Google Adsense

--- resources/views/video/home.blade.php
+++ resources/views/video/home.blade.php
@@ -34,17 +34,19 @@
 	</div>
 
 	<div class="paravi-padding-setup" style="padding: 0px 6%; padding-bottom: 12px">
-		<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-		<!-- Home Desktop Ads -->
-		<ins class="adsbygoogle"
-		     style="display:block; border: 1px solid black !important"
-		     data-ad-client="ca-pub-4485968980278243"
-		     data-ad-slot="4478704168"
-		     data-ad-format="auto"
-		     data-full-width-responsive="true"></ins>
-		<script>
-		     (adsbygoogle = window.adsbygoogle || []).push({});
-		</script>
+		<div style="border: 1px solid black">
+			<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+			<!-- Home Desktop Ads -->
+			<ins class="adsbygoogle"
+			     style="display:block;"
+			     data-ad-client="ca-pub-4485968980278243"
+			     data-ad-slot="4478704168"
+			     data-ad-format="auto"
+			     data-full-width-responsive="true"></ins>
+			<script>
+			     (adsbygoogle = window.adsbygoogle || []).push({});
+			</script>
+		</div>
 	</div>
 
 	<div class="paravi-padding-setup home-featured-wrapper">
