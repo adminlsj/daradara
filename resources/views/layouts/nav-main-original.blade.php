dbfa0ae5d24fff9fef8fba10127f884828d5d719
AppleOPie <u3514481@connect.hku.hk>
2020-04-06 01:22:01 +0800

    Google Adsense

--- resources/views/layouts/nav-main-original.blade.php
+++ resources/views/layouts/nav-main-original.blade.php
@@ -1,10 +1,10 @@
 <nav class="nav-main-original {{ $theme == 'dark' ? 'dark-theme-nav-main' : 'white-theme-nav-main' }}">
   <div class="container-fluid">
     <div style="height: 50px;">
-      <i style="vertical-align:middle; margin-left: 10px; margin-right:25px; font-size: 1.8em; margin-top: 10px" class="material-icons hidden-xs hidden-sm">menu</i>
+      <i style="vertical-align:middle; margin-left: 10px; margin-right:25px; font-size: 1.8em; margin-top: 8px" class="material-icons hidden-xs hidden-sm">menu</i>
 
       <a href="/">
-          <img src="https://i.imgur.com/{{ $theme == 'dark' ? 'xSMGFWh' : 'M8tqx5K.png'}}.png" style="margin-top: 11px; margin-left: -5px;" height="26" alt="娛見日本 LaughSeeJapan">
+          <img src="https://i.imgur.com/{{ $theme == 'dark' ? 'xSMGFWh' : 'M8tqx5K.png'}}.png" style="margin-top: 13px; margin-left: -5px;" height="26" alt="娛見日本 LaughSeeJapan">
       </a>
 
       <a id="nav-account-icon" class="pull-right" style="padding: 0px 0px 0px 15px;" href="{{ Auth::check() ? route('user.show', Auth::user()) : route('login')}}"><i style="font-size: 25px; vertical-align:middle; margin-bottom: -22.5px" class="material-icons">account_circle</i></a>
