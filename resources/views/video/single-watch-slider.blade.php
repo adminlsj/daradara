524f3beca9c6a2d8390777dcaadc4a6510254ddd
AppleOPie <u3514481@connect.hku.hk>
2020-04-08 03:35:19 +0800

    Google Adsense

--- resources/views/video/single-watch-slider.blade.php
+++ resources/views/video/single-watch-slider.blade.php
@@ -5,7 +5,7 @@
         <a style="text-decoration: none; color: black" href="{{ route('video.intro', [$watch->genre, $watch->titleToUrl()]) }}">
           <img class="lazy" style="width: 100%; height: 100%; border-top-left-radius: 10px; border-top-right-radius: 10px;" src="{{ $watch->imgurDefault() }}" data-src="{{ $watch->imgurL() }}" data-srcset="{{ $watch->imgurL() }}" alt="{{ $watch->title }}">
 
-          <div style="height: 45px; padding: 2px 15px;">
+          <div style="height: 45px; padding: 2px 0px 2px 15px;">
             <h4 style="line-height: 19px; font-size: 1em;overflow: hidden;text-overflow: ellipsis; display: -webkit-box; -webkit-line-clamp: 1; -webkit-box-orient: vertical; font-weight: 450; white-space: initial">{{ $watch->title }}</h4>
           </div>
         </a>
