7c724b15d179d45cb58e958e2fd05c77106d3ffa
AppleOPie <u3514481@connect.hku.hk>
2020-04-12 12:58:09 +0800

    Google Adsense

--- resources/views/video/singleSubscribeVideo.blade.php
+++ resources/views/video/singleSubscribeVideo.blade.php
@@ -9,7 +9,7 @@
 		</a>
 		<a href="{{ route('video.watch') }}?v={{ $video->id }}" style="text-decoration: none; color: black;">
 			<div style="margin-left: 53px; font-size: 1.1em; line-height: 19px">{{ $video->title }}</div>
-			<div style="margin-left: 53px; font-size: 0.85em; color: #595959; margin-top: 3px;">{{ $video->user()->name }} • 收看次數：{{ $video->views() }} • {{ Carbon\Carbon::parse($video->uploaded_at)->diffForHumans() }}</div>
+			<div style="margin-left: 53px; font-size: 0.85em; color: #595959; margin-top: 3px;">{{ $video->user()->name }} • {{ Carbon\Carbon::parse($video->uploaded_at)->diffForHumans() }}</div>
 		</a>
 	</div>
 </div>
@@ -19,11 +19,10 @@
   <div class="row no-gutter inner">
     <div class="col-xs-6 col-sm-6 col-md-3">
       <img class="lazy" style="width: 100%; height: 100%; padding-right: 13px" src="{{ $video->imgur16by9() }}" data-src="{{ $video->imgurL() }}" data-srcset="{{ $video->imgurL() }}" alt="{{ $video->title }}">
-      <span>{{ $video->duration() }}</span>
     </div>
     <div style="padding-top: 1px; padding-right: 12px; padding-left: 4px;" class="col-xs-6 col-sm-6 col-md-7">
       <h4>{{ $video->title }}</h4>
-      <p><a href="{{ route('user.show', [$video->user()]) }}">{{ $video->user()->name }}</a> • <br class="hidden-md hidden-lg">觀看次數：{{ $video->views() }}次 • {{ Carbon\Carbon::parse($video->uploaded_at)->diffForHumans() }}</p>
+      <p><a href="{{ route('user.show', [$video->user()]) }}">{{ $video->user()->name }}</a> • {{ Carbon\Carbon::parse($video->uploaded_at)->diffForHumans() }}</p>
       <p style="margin-top: 9px; overflow: hidden;text-overflow: ellipsis; display: -webkit-box; -webkit-line-clamp: 2; -webkit-box-orient: vertical;" class="hidden-xs hidden-sm">{{ $video->caption }}</p>
     </div>
   </div>
