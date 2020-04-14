2fb8838c2876cdce7eb1469b39b37bc112f71f56
AppleOPie <u3514481@connect.hku.hk>
2020-04-14 23:42:32 +0800

    First commit

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,9 +1,7 @@
 <?php
 
-namespace App\Http\Controllers\Admin;
+namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
-use App\Http\Requests;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
