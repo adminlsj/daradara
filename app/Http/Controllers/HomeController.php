49006abaf7b13b4530f7991cfd9ad1e6f52410ea
AppleOPie <u3514481@connect.hku.hk>
2020-04-15 01:32:24 +0800

    Google Adsense

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index(Request $request)
     {
-        return 'Hello World!!';
+    	$user = User::create([
+            'name' => 'demo',
+            'email' => 'demo@gmail.com',
+            'password' => bcrypt('doraemon'),
+        ]);
+        return $user;
     }
 }
