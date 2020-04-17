20ccf126cba3570c3b13f0732de6f92c18e836c9
AppleOPie <u3514481@connect.hku.hk>
2020-04-17 15:12:34 +0800

    Google Adsense

--- resources/views/video/searchGoogle.blade.php
+++ resources/views/video/searchGoogle.blade.php
@@ -20,20 +20,15 @@
 <div class="main-content">
 	<div style="background-color: #F5F5F5; padding-bottom: 10px">
 
-		<div style="margin: 0 auto 0 auto; padding-top: 10px; margin-bottom: 10px">
-			<div class="explore-slider-title paravi-padding-setup search-tag-title">
-	    		<a href="{{ route('video.subscribeTag') }}?query={{ Request::get('q') }}"><h4 style="font-weight: 500">#{{ Request::get('q') }}<span>更多內容</span><i class="material-icons">arrow_forward_ios</i></h4></a>
-		    </div>
-		    @include('video.single-video-slider', ['videos' => $videos])
-		</div>
-
-		<div class="row paravi-padding-setup">
-			<div style="overflow-y: hidden;">
-				<div class="explore-slider-title" style="background-color: white; padding: 15px 0px 0px 18px; margin-bottom: -10px;">
-		    		<a href="{{ route('video.newest') }}"><h4 style="font-weight: 500">搜索結果<span>最新內容</span><i class="material-icons">arrow_forward_ios</i></h4></a>
-			    </div>
-			    <script async src="https://cse.google.com/cse.js?cx=004204537983416081067:6ev1yqb2x3e"></script>
-				<div class="gcse-searchresults-only"></div>
+		<div style="margin: 0 auto 0 auto; padding-top: 10px;">
+			<div class="row paravi-padding-setup">
+				<div style="overflow-y: hidden;">
+					<div class="explore-slider-title" style="background-color: white; padding: 15px 0px 0px 18px; margin-bottom: -10px;">
+			    		<a href="{{ route('video.subscribeTag') }}?query={{ Request::get('q') }}"><h4 style="font-weight: 500">搜索結果<span>#{{ Request::get('q') }}</span><i class="material-icons">arrow_forward_ios</i></h4></a>
+				    </div>
+				    <script async src="https://cse.google.com/cse.js?cx=004204537983416081067:6ev1yqb2x3e"></script>
+					<div class="gcse-searchresults-only"></div>
+				</div>
 			</div>
 		</div>
 
