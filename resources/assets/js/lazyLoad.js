ed18e3f79570e785d713cd0b80fb3e3563ebf98a
AppleOPie <u3514481@connect.hku.hk>
2020-04-17 12:17:37 +0800

    Google Adsense

--- resources/assets/js/lazyLoad.js
+++ resources/assets/js/lazyLoad.js
@@ -1,23 +1,23 @@
 document.addEventListener("DOMContentLoaded", function() {
   var lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
 
-  let lazyImageObserver = new IntersectionObserver(function(entries, observer) {
-    entries.forEach(function(entry) {
-      if (entry.isIntersecting) {
-        let lazyImage = entry.target;
-        lazyImage.src = lazyImage.dataset.src;
-        lazyImage.srcset = lazyImage.dataset.srcset;
-        lazyImage.classList.remove("lazy");
-        lazyImageObserver.unobserve(lazyImage);
-      }
+  if ("IntersectionObserver" in window) {
+    let lazyImageObserver = new IntersectionObserver(function(entries, observer) {
+      entries.forEach(function(entry) {
+        if (entry.isIntersecting) {
+          let lazyImage = entry.target;
+          lazyImage.src = lazyImage.dataset.src;
+          lazyImage.srcset = lazyImage.dataset.srcset;
+          lazyImage.classList.remove("lazy");
+          lazyImageObserver.unobserve(lazyImage);
+        }
+      });
+    }, {
+      rootMargin: "0px 0px 256px 0px"
     });
-  }, {
-    root: null, // avoiding 'root' or setting it to 'null' sets it to default value: viewport
-    rootMargin: "0px 0px 256px 0px",
-    threshold: 0
-  });
-  
-  lazyImages.forEach(function(lazyImage) {
-    lazyImageObserver.observe(lazyImage);
-  });
+    
+    lazyImages.forEach(function(lazyImage) {
+      lazyImageObserver.observe(lazyImage);
+    });
+  }
 });
