b8c6b772f4915ad272f54a51823cd499a5f9199d
AppleOPie <u3514481@connect.hku.hk>
2020-04-19 20:34:15 +0800

    First Commit

--- resources/views/layouts/nav-main-original.blade.php
+++ resources/views/layouts/nav-main-original.blade.php
@@ -10,16 +10,16 @@
           <span id="nav-logo-text">LaughSeeJapan</span>
       </a>
 
-      <form id="search-form" class="hidden-xs hidden-sm pull-left" style="width: 70%; margin-top: 5px; margin-left:152px; margin-right: -400px; position: relative;" action="{{ route('video.searchGoogle') }}" method="GET">
+      <form id="search-form" class="hidden-xs hidden-sm pull-left" style="width: 50%; margin-top: 5px; left:370px; position: absolute;" action="{{ route('video.searchGoogle') }}" method="GET">
           <i style="position: absolute; top: 8px; left: 17px; color: dimgray" class="material-icons">search</i>
-          <input name="q" style="box-shadow: none; border: 1px solid #e8eaed; background-color: #e8eaed; font-size: 1.1em; width: 62%; border-radius: 7px; height: 40px; padding-left: 53px; color: #222222; padding-bottom: 2.5px;" type="text" value="{{ request('q') }}" placeholder="搜索">
+          <input name="q" style="box-shadow: none; border: 1px solid #e8eaed; background-color: #e8eaed; font-size: 1.1em;border-radius: 7px; height: 40px; padding-left: 53px; color: #222222; padding-bottom: 2.5px;" type="text" value="{{ request('q') }}" placeholder="搜索">
       </form>
 
       @if (Auth::check())
         <a class="hidden-xs hidden-sm" style="position: absolute; top: 9px; right: 26px;" href="{{ route('user.show', Auth::user()) }}"><i style="font-size: 26px;" class="material-icons">account_circle</i></a>
         <a class="hidden-xs hidden-sm" style="position: absolute; top: 9px; right: 68px;" href="{{ route('user.userEditUpload', Auth::user()) }}"><i style="font-size: 26px;" class="material-icons">video_call</i></a>
       @else
-        <a class="no-select nav-item-text hidden-xs hidden-sm" style="color: #cf2d52; border: 1px solid #cf2d52; padding: 6px 10px; margin-top: 7px; margin-right: 15px; font-weight: 500" href="{{ route('login') }}">登入</a>
+        <a class="no-select nav-item-text hidden-xs hidden-sm" style="color: #cf2d52; border: 1px solid #cf2d52; padding: 6px 10px; margin-top: 7px; margin-right: 15px; font-weight: 500;" href="{{ route('login') }}">登入</a>
         <a class="no-select nav-item-text hidden-xs hidden-sm" style="color: white; background-color: #cf2d52; padding: 5px 10px; margin-top: 7px; font-weight: 500" href="{{ route('register') }}">註冊</a>
       @endif
 
