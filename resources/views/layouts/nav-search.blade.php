9a46b9394984b6503854f6140e7fb1e3d1e4c31f
AppleOPie <u3514481@connect.hku.hk>
2020-04-19 22:27:33 +0800

    First Commit

--- resources/views/layouts/nav-search.blade.php
+++ resources/views/layouts/nav-search.blade.php
@@ -2,11 +2,11 @@
   <div style="background-color: #e9e9e9;">
 
     <form id="search-form" style="background-color: #e9e9e9;" action="{{ route('video.searchGoogle') }}" method="GET">
-      <a id="toggleSearchBar" style="color: #646464 !important; padding: 0px 15px 0px 0px; cursor: pointer;"><i style="font-size: 25px; vertical-align:middle; margin-bottom: -22.5px" class="material-icons">arrow_back</i></a>
+      <a id="toggleSearchBar" style="color: #646464 !important; padding: 0px 15px 0px 0px; cursor: pointer;"><i style="font-size: 25px; vertical-align:middle; margin-bottom: -24px" class="material-icons">arrow_back</i></a>
 
-      <input id="q" name="q" style="vertical-align:middle; margin-bottom: -27px; background-color: #e9e9e9; box-shadow: none; border-bottom: none;" type="text" value="{{ request('q') }}" placeholder="搜索">
+      <input id="q" name="q" style="vertical-align:middle; margin-bottom: -28px; background-color: #e9e9e9; box-shadow: none; border-bottom: none;" type="text" value="{{ request('q') }}" placeholder="搜索">
 
-      <a class="search-submit-btn" type="submit" style="color: #646464 !important; padding: 0px 0px 15px 15px; cursor: pointer;"><i style="font-size: 25px; vertical-align:middle; margin-bottom: -22.5px" class="material-icons">search</i></a>
+      <a class="search-submit-btn" type="submit" style="color: #646464 !important; padding: 0px 0px 15px 15px; cursor: pointer;"><i style="font-size: 25px; vertical-align:middle; margin-bottom: -24px" class="material-icons">search</i></a>
     </form>
   </div>
 </nav>
