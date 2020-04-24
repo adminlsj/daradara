4683ee3c2894f6c832c78affa9392eb6b640ab8f
AppleOPie <u3514481@connect.hku.hk>
2020-04-24 11:32:42 +0800

    Google Adsense

--- resources/views/video/singleLoadMoreSliderWatches.blade.php
+++ resources/views/video/singleLoadMoreSliderWatches.blade.php
@@ -1,6 +1,6 @@
 <div class="col-xs-6 col-sm-3 col-md-3 hover-opacity-all load-more-wrapper" style="position: relative;">
     <a style="text-decoration: none; color: black" href="{{ route('video.playlist') }}?list={{ $watch->id }}">
-      <img class="lazy" style="width: 100%; height: 100%;" src="{{ $first->imgur16by9() }}" data-src="{{ $first ? $first->imgurH() : 'https://i.imgur.com/JMcgEkPl.jpg' }}" data-srcset="{{ $first ? $first->imgurH() : 'https://i.imgur.com/JMcgEkPl.jpg' }}" alt="{{ $watch->title }}">
+      <img class="lazy" style="width: 100%; height: 100%;" src="https://i.imgur.com/JMcgEkPl.jpg" data-src="{{ $first ? $first->imgurH() : 'https://i.imgur.com/JMcgEkPl.jpg' }}" data-srcset="{{ $first ? $first->imgurH() : 'https://i.imgur.com/JMcgEkPl.jpg' }}" alt="{{ $watch->title }}">
       <span>
         <div style="margin: 0;position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">
           <div>{{ $watch->videos()->count() }}</div>
