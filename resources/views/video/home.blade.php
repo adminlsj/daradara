a2b5b960e503ee1e992cea2ac9fd918a9a696b6d
AppleOPie <u3514481@connect.hku.hk>
2020-04-25 14:24:08 +0800

    Google Adsense

--- resources/views/video/home.blade.php
+++ resources/views/video/home.blade.php
@@ -38,10 +38,10 @@
 			<div class="subscribes-tab">
 		    	<a id="default-tag" class="load-tag-videos active" style="margin-right: 5px;">全部推薦內容</a>
 				<a class="load-tag-videos" style="margin-right: 5px;">#創意廣告</a>
-				<a class="load-tag-videos" style="margin-right: 5px;">#搞笑影片</a>
+				<a class="load-tag-videos" style="margin-right: 5px;">#日本網紅</a>
 				<a class="load-tag-videos" style="margin-right: 5px;">#動漫講評</a>
+				<a class="load-tag-videos" style="margin-right: 5px;">#MAD·AMV</a>
 				<a class="load-tag-videos" style="margin-right: 5px;">#日劇講評</a>
-				<a class="load-tag-videos" style="margin-right: 5px;">#電影講評</a>
 			</div>
 		@endif
 
