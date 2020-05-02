e3eed629d96dfbbf52c4add40845742b543047d9
AppleOPie <u3514481@connect.hku.hk>
2020-05-02 20:42:56 +0800

    Google Adsense

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -210,7 +210,7 @@ $('[id=toggleSearchBar]').click(function(e) {
     var x = document.getElementById("searchBar");
     if (x.style.display === "none") {
         x.style.display = "block";
-        document.getElementById("q").focus();
+        document.getElementById("query").focus();
     } else {
         x.style.display = "none";
     }
