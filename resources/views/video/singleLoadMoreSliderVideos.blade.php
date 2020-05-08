ae523d240f76015970f984884b0778e0e351794f
AppleOPie <u3514481@connect.hku.hk>
2020-05-08 13:51:57 +0800

    Google Adsense

--- resources/views/video/singleLoadMoreSliderVideos.blade.php
+++ resources/views/video/singleLoadMoreSliderVideos.blade.php
@@ -2,7 +2,7 @@
     <div style="position: relative;">
 	    <a href="{{ route('video.watch') }}?v={{ $video->id }}">
 	      <img class="lazy" style="width: 100%; height: 100%;" src="{{ $video->imgur16by9() }}" data-src="{{ $video->imgurL() }}" data-srcset="{{ $video->imgurL() }}" alt="{{ $video->title }}">
-	      <div style="position: absolute; bottom: 0px; width: 100%;padding: 8px 10px; font-size: 1.0em; color: white; background: linear-gradient(to bottom, transparent 0%, black 120%);">
+	      <div style="position: absolute; bottom: 0px; width: 100%;padding: 8px 10px; font-size: 0.95em; color: white; background: linear-gradient(to bottom, transparent 0%, black 120%);">
 	        <div style="font-size: 0.8em; font-weight: bold;">{{ $video->user()->name }}</div>
 	        <div style="margin-top: 1px; font-weight: bold;">{{ $video->title }}</div>
 	      </div>
