01dadeac385646ccbc1583de79522f56709c9096
AppleOPie <u3514481@connect.hku.hk>
2020-05-12 16:25:49 +0800

    Google Adsense

---
+++ resources/views/video/singleShowRelated.blade.php
@@ -0,0 +1,13 @@
+<a href="{{ route('video.watch') }}?v={{ $video->id }}{{ $source == 'playlist' ? '&list='.Request::get('list') : '' }}" class="row no-gutter">
+  <div style="padding-right: 4px; position: relative;" class="col-xs-6 col-sm-6 col-md-6">
+    <img class="lazy" style="width: 100%; height: 100%; border-top-left-radius: 3px; border-bottom-left-radius: 3px;" src="{{ $video->imgur16by9() }}" data-src="{{ $video->imgurL() }}" data-srcset="{{ $video->imgurL() }}" alt="{{ $video->title }}">
+  </div>
+  <div style="padding-left: 4px;" class="col-xs-6 col-sm-6 col-md-6 related-watch-title" style="position: relative;">
+    <h4 style="margin-top:0px; margin-bottom: 0px; line-height: 19px; font-size: 1.00em; color:#222222;overflow: hidden;text-overflow: ellipsis; display: -webkit-box; -webkit-line-clamp: 3; -webkit-box-orient: vertical;">{{ $video->title }}</h4>
+  </div>
+</a>
+
+<div style="position: absolute; bottom: 7px; left: calc(50% + 7px);">
+	<img class="lazy" style="float:left; width: 18px; height: 18px; border: 1px solid #f5f5f5; margin-top: 1px" src="{{ $video->user()->avatarCircleB() }}" data-src="{{ $video->user()->avatar == null ? $video->user()->avatarDefault() : $video->user()->avatar->filename }}" data-srcset="{{ $video->user()->avatar == null ? $video->user()->avatarDefault() : $video->user()->avatar->filename }}">
+	<a href="{{ route('user.show', [$video->user()]) }}" style="color: darkgray; font-size: 0.8em; margin-left: 5px;">{{ $video->user()->name }}</a>
+</div>
--- resources/views/video/video-playlist-wrapper.blade.php
+++ resources/views/video/video-playlist-wrapper.blade.php
@@ -1,14 +1,14 @@
 @if ($videos)
 	@foreach ($videos as $video)
 	    <div class="related-watch-wrap" style="{{ $video->id == $current->id ? 'background-color: #E5E5E5' : '' }};">
-	    	@include('video.singleRelatedWatch', ['source' => 'playlist'])
+	    	@include('video.singleShowRelated', ['source' => 'playlist'])
 		</div>
 	@endforeach
 	<hr style="margin: 7px 15px; border-color: #e5e5e5">
 @endif
 
 @foreach ($related as $video)
-    <div class="related-watch-wrap" style="{{ $video->id == $current->id ? 'background-color: #E5E5E5' : '' }};">
-    	@include('video.singleRelatedWatch', ['source' => 'video'])
+    <div class="related-watch-wrap hover-opacity-all" style="position: relative;">
+    	@include('video.singleShowRelated', ['source' => 'video'])
 	</div>
 @endforeach
