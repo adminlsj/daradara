2abbd2ea5800a717fdd3141353baa4bdd0dbf407
AppleOPie <u3514481@connect.hku.hk>
2020-05-12 20:41:30 +0800

    Google Adsense

--- resources/views/video/video-playlist-wrapper.blade.php
+++ resources/views/video/video-playlist-wrapper.blade.php
@@ -1,11 +1,11 @@
 @if ($videos)
-	<div style="margin: 15px 15px 0px 15px; box-shadow: 0 4px 8px 0 rgba(0,0,0,0.1); border-radius: 3px;">
+	<div style="margin: 15px; box-shadow: 0 4px 8px 0 rgba(0,0,0,0.1); border-radius: 3px;">
 		<div style="background-color: #e1e1e1; padding: 10px; border-top-left-radius: 3px; border-top-right-radius: 3px;">
 			<div><a style="color: #222222; font-weight: bold; font-size: 1.1em" href="{{ route('video.playlist') }}?list={{ $current->watch()->id }}">{{ $current->watch()->title }}</a></div>
 			<div><a style="color: dimgray; font-weight: bold; font-size: 0.85em" href="{{ route('user.show', [$current->user()]) }}">{{ $current->user()->name }}</a></div>
 		</div>
 
-		<div style="height: 300px; overflow-y: scroll; padding: 5px 0px;">
+		<div style="height: 325px; overflow-y: scroll; padding: 5px 0px;">
 			@foreach ($videos as $video)
 			    <div class="hover-opacity-all" style="{{ $video->id == $current->id ? 'background-color: #e9e9e9' : '' }}; padding: 5px 10px;">
 			    	<a href="{{ route('video.watch') }}?v={{ $video->id }}&list={{ Request::get('list') }}" class="row no-gutter">
