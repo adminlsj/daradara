7cd0b6c595815141f3ab0f7130c651c948476c65
AppleOPie <u3514481@connect.hku.hk>
2020-05-17 15:54:14 +0800

    Google Adsense

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('laughseejapan:update-videos')
-                 ->dailyAt('15:52');
+                 ->dailyAt('08:00');
     }
 
     /**
