b2a82c74ff2aec93c6480ebb90d2563b7b9491c8
AppleOPie <u3514481@connect.hku.hk>
2020-05-25 14:01:10 +0800

    Google Adsense

--- resources/views/video/player.blade.php
+++ resources/views/video/player.blade.php
@@ -1,6 +1,6 @@
 @if (!$video->outsource || strpos($video->sd, "instagram.com") !== FALSE || $is_mobile && strpos($video->sd, "player.bilibili.com") !== FALSE )
   <div class="aspect-ratio" style="background-color: black; background-image: url('https://i.imgur.com/TcZjkZa.gif'); background-position: center; background-repeat: no-repeat; background-size: 50px;">
-      <iframe src="{!! $video->source() !!}" style="border: 0; overflow: hidden;" allow="autoplay" allowfullscreen></iframe>
+      <iframe src="https://www.agefans.tv/player/dpx/?url={!! $video->source() !!}" style="border: 0; overflow: hidden;" allow="autoplay" allowfullscreen></iframe>
   </div>
 
 @else
