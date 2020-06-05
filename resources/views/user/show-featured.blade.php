8164c49b74be5e3de55269bcb1db650340cba6ec
AppleOPie <u3514481@connect.hku.hk>
2020-06-05 13:40:27 +0800

    Google Adsense

--- resources/views/user/show-featured.blade.php
+++ resources/views/user/show-featured.blade.php
@@ -16,38 +16,17 @@
 <div class="hidden-sm hidden-xs sidebar-menu">
 	@include('video.sidebarMenu', ['theme' => 'white'])
 </div>
+
 <div class="main-content home">
 	<div style="background-color: #F5F5F5;">
 
-    <div class="home-genre-banner-wrapper" style="background-color: #EFE3E3; margin-bottom: 25px;">
-      <img style="width: auto; height: 100%; float: right" src="https://i.imgur.com/Qlawqysh.png">
-      <div class="home-genre-panel">
-        <img class="lazy" style="float:left; width: 70px; height: 70px; border-top-left-radius: 3px; border-bottom-left-radius: 3px;" src="{{ $user->avatarCircleB() }}" data-src="{{ $user->avatar == null ? $user->avatarDefault() : $user->avatar->filename }}" data-srcset="{{ $user->avatar == null ? $user->avatarDefault() : $user->avatar->filename }}">
-        <div style="height: 70px; margin-left: 85px; padding-top: 9px; padding-right: 15px;">
-          <div style="font-size: 1.5em; font-weight: bold; color: #444444">{{ $user->name }}</div>
-          <div style="color: gray; font-weight: bold; color: #666666">{{ $subscribers }} 位訂閱者</div>
-        </div>
-      </div>
-      @if (Auth::check() && $user->id == Auth::user()->id)
-        <div class="paravi-padding-setup" style="position: absolute; right: 0px; top: 10px">
-          <form style="width:auto; height: auto; font-size: 1em; margin-top: 5px;" id="logout-form" action="{{ route('logout') }}" method="POST" class="pull-right">
-              {{ csrf_field() }}
-              <button style="background-color: inherit !important; color: #d84b6b;" class="btn btn-info" type="submit">登出</button>
-          </form>
-        </div>
-      @endif
-      <div class="subscribes-tab" style="border: none; position: absolute; bottom: -10px;">
-        <a href="{{ route('user.show', [$user, 'featured']) }}" class="active" style="margin-right: 5px;">首頁</a>
-        <a href="{{ route('user.show', [$user, 'videos']) }}" style="margin-right: 5px;">影片</a>
-        <a href="{{ route('user.show', [$user, 'playlists']) }}" style="margin-right: 5px;">播放清單</a>
-        <a href="{{ route('user.show', [$user, 'about']) }}" style="margin-right: 5px;">簡介</a>
-        @if (Auth::check() && $user->id == Auth::user()->id)
-          <a href="{{ route('user.userEditUpload', $user) }}" style="margin-right: 5px;">上傳影片</a>
-        @endif
-      </div>
+    <div class="row no-gutter load-more-container" style="padding-top: 19px;">
+      <img class="lazy user-show-title" style="float:left; width: 56px; height: 56px; border-top-left-radius: 3px; border-bottom-left-radius: 3px; margin-right: 15px" src="{{ $user->avatarCircleB() }}" data-src="{{ $user->avatar == null ? $user->avatarDefault() : $user->avatar->filename }}" data-srcset="{{ $user->avatar == null ? $user->avatarDefault() : $user->avatar->filename }}">
+      <h5 class="user-show-title" style="font-size: 1em; color: #555555; font-weight: normal; line-height: 0px">{{ $subscribers }} 位訂閱者</h5>
+      <h3 class="user-show-title no-select" style="font-size: 2em; margin-top: 4px; margin-bottom: 10px"><span style="font-size: 0.93em">{{ $user->name }}</span></h3>
     </div>
 
-    <div class="row no-gutter load-more-container" style="margin-top: -20px;">
+    <div class="row no-gutter load-more-container" style="margin-top: -6px;">
       <a href="{{ route('user.show', [$user, 'videos']) }}" style="color: inherit; text-decoration: none">
         <h3 class="user-show-title">近期動態<i style="font-size: 0.85em; vertical-align: middle; margin-top: -3px; margin-left: 5px" class="material-icons">arrow_forward_ios</i></h3>
       </a>
