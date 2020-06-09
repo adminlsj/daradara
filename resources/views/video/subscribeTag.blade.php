a08d8d8e12a9b2bcf0a055aec2f2df01e643bb88
AppleOPie <u3514481@connect.hku.hk>
2020-06-09 13:55:58 +0800

    Google Adsense

--- resources/views/video/subscribeTag.blade.php
+++ resources/views/video/subscribeTag.blade.php
@@ -14,7 +14,7 @@
 		    <div class="col-xs-12 col-sm-12 col-md-12">
 		    	<div style="display: inline-block;">
 					<img style="width: 50px; height: auto; float: left; border-radius: 50%; margin-top: 5px" src="https://i.imgur.com/{{ $videos->first()->imgur }}s.jpg" alt="{{ $tag }}">
-			    	<div style="margin-left: 58px;"><h4 style="margin-top:8px; line-height: 23px">#{{ $tag }}</h4></div>
+			    	<div style="margin-left: 58px;"><h4 style="margin-top:7px; line-height: 23px">#{{ $tag }}</h4></div>
 			    	<div style="margin-left: 58px; margin-top: -6px; margin-bottom: -5px;"><p style="color: #595959; font-size: 0.9em"><span id="subscribes-count">{{ App\Subscribe::where('tag', $tag)->count() }}</span> 位訂閱者</p></div>
 		    	</div>
 		    	<div id="tag-subscribe-wrapper-btn" style="display: inline-block;" class="pull-right">
