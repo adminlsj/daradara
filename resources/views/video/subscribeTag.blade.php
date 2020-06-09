81b4aec7adc60ac6100ba08e06ead2182df33661
AppleOPie <u3514481@connect.hku.hk>
2020-06-09 13:40:12 +0800

    Google Adsense

--- resources/views/video/subscribeTag.blade.php
+++ resources/views/video/subscribeTag.blade.php
@@ -10,14 +10,16 @@
 </div>
 <div class="main-content" style="background-color: #F5F5F5;">
 	<div>
-		<div class="row no-gutter padding-setup" style="padding-top: 10px; padding-bottom: 15px; background-color: #e9e9e9">
-		    <div class="col-xs-6 col-sm-6 col-md-3">
-				<img style="width: 50px; height: auto; float: left; border-radius: 50%; margin-top: 5px" src="https://i.imgur.com/{{ $videos->first()->imgur }}s.jpg" alt="{{ $tag }}">
-		    	<div style="margin-left: 58px"><h4>#{{ $tag }}</h4></div>
-		    	<div style="margin-left: 58px; margin-top: -5px; margin-bottom: -5px;"><p style="color: #595959; font-size: 0.9em"><span id="subscribes-count">{{ App\Subscribe::where('tag', $tag)->count() }}</span> 位訂閱者</p></div>
-		    </div>
-		    <div class="col-xs-6 col-sm-6 col-md-7">
-		    	@include('video.tag-subscribe-wrapper')
+		<div class="row no-gutter padding-setup" style="padding-top: 10px; padding-bottom: 15px; background-color: #e9e9e9; padding-left: 0px">
+		    <div class="col-xs-12 col-sm-12 col-md-12">
+		    	<div style="display: inline-block;">
+					<img style="width: 50px; height: auto; float: left; border-radius: 50%; margin-top: 5px" src="https://i.imgur.com/{{ $videos->first()->imgur }}s.jpg" alt="{{ $tag }}">
+			    	<div class="text-ellipsis" style="margin-left: 58px"><h4>#{{ $tag }}</h4></div>
+			    	<div style="margin-left: 58px; margin-top: -5px; margin-bottom: -5px;"><p style="color: #595959; font-size: 0.9em"><span id="subscribes-count">{{ App\Subscribe::where('tag', $tag)->count() }}</span> 位訂閱者</p></div>
+		    	</div>
+		    	<div id="tag-subscribe-wrapper-btn" style="display: inline-block;" class="pull-right">
+		    		@include('video.tag-subscribe-wrapper')
+		    	</div>
 		    </div>
 		</div>
 		<hr style="margin: 0px 0px 0px 0px; border-color: #e5e5e5;">
