43fee773a7b0a145be7feaba40d1e6e105129fe0
AppleOPie <u3514481@connect.hku.hk>
2020-06-16 23:43:29 +0800

    Google Adsense

--- resources/views/video/copyright.blade.php
+++ resources/views/video/copyright.blade.php
@@ -2,6 +2,6 @@
 <div style="width: 100%; height: 100%; background-color: black; text-align: center; vertical-align: middle; position: relative;">
 	<div style="width: 80%; margin: 0; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">
 	<span style="color: gray; font-size: 18vw;" class="material-icons">error_outline</span>
-	<h1 style="color:white; font-size: 3.72vw; font-weight: normal">由於該影片違反 LaughSeeJapan 的社群規範，因此已將影片下架，敬請見諒。</h1>
+	<h1 style="color:white; font-size: 3.72vw; font-weight: normal; line-height: 6vw">由於該影片違反 LaughSeeJapan 的社群規範，因此已將影片下架，敬請見諒。</h1>
 	</div>
 </div>
