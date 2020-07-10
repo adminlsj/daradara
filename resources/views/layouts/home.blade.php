838ea825bf07d57c11b2be3dc4713c414e3be3a5
AppleOPie <u3514481@connect.hku.hk>
2020-07-10 14:49:08 +0800

    Google Adsense

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -12,9 +12,18 @@
 	</span>
 
 	<div style="padding: 0 20px">
-		<a class="nav-item-icon" style="right: 16px; color: dimgray" href="{{ Auth::check() ? route('user.show', Auth::user()) : route('login')}}"><i class="material-icons">account_circle</i></a>
-		<a href="{{ route('video.subscribes') }}" class="nav-item-icon" style="top: 10px; right: 62px; cursor: pointer; color: dimgray"><i style="font-size: 1.65em" class="material-icons">subscriptions</i></a>
-		<a id="nav-account-icon" class="pull-right nav-item-icon" style="right: 105px; color: dimgray" href="{{ Auth::check() ? route('user.userEditUpload', Auth::user()) : route('login')}}"><i class="material-icons">video_call</i></a>
+		@if (Auth::check())
+	        <a class="hidden-xs hidden-sm nav-item-icon" style="right: 26px; color: #666666;" href="{{ route('user.show', Auth::user()) }}"><i class="material-icons">account_circle</i></a>
+	        <a class="hidden-xs hidden-sm nav-item-icon" style="right: 75px; color: #666666;" href="{{ route('user.userEditUpload', Auth::user()) }}"><i class="material-icons">video_call</i></a>
+	    @else
+	        <a class="no-select nav-item-text hidden-xs hidden-sm" style="color: #e8eaed; border: 1px solid #e8eaed; padding: 8px 14px; margin-top: 5px; margin-right: 7px; font-weight: bold; color: #666666" href="{{ route('login') }}">登入</a>
+	        <a class="no-select nav-item-text hidden-xs hidden-sm" style="color: white; background-color: #e8eaed; border-color: #e8eaed; padding: 7px 14px; margin-top: 5px; font-weight: bold; color: #666666;" href="{{ route('register') }}">註冊</a>
+	        <a class="no-select pull-right hidden-xs hidden-sm" style="margin-top: 8px; margin-right: 18px; color: #666666;" href="{{ route('login') }}"><i class="material-icons">video_call</i></a>
+	    @endif
+
+		<a class="hidden-md hidden-lg nav-item-icon" style="right: 16px; color: #666666;" href="{{ Auth::check() ? route('user.show', Auth::user()) : route('login')}}"><i class="material-icons">account_circle</i></a>
+		<a id="toggleSearchBar" class="hidden-md hidden-lg nav-item-icon" style="right: 60px; cursor: pointer; color: #666666;"><i class="material-icons">search</i></a>
+		<a id="nav-account-icon" class="pull-right hidden-md hidden-lg nav-item-icon" style="right: 104px; color: #666666;" href="{{ Auth::check() ? route('user.userEditUpload', Auth::user()) : route('login')}}"><i class="material-icons">video_call</i></a>
 	</div>
 
 	<div style="margin: 0;position: absolute;top: 46%;left: 50%;transform: translate(-50%, -50%); width: 100%;">
@@ -52,8 +61,8 @@
     </div>
 </div>
 
-<div class="hidden-xs hidden-sm" style="background-color: #333333; width: 100%; position: absolute; bottom: 0px; padding: 10px 10px; z-index: 100000;">
-	<span style="float: left;"><a href="/contact" style="padding: 0px 15px; color: dimgray">廣告</a><a href="/about" style="padding: 0px 15px; color: dimgray">娛見日本</a><a href="/about" style="padding: 0px 15px; color: dimgray">關於</a><a href="/contact" style="padding: 0px 15px; color: dimgray">聯絡</a></span><span style="float: right"><a href="/terms" style="padding: 0px 15px; color: dimgray">服務條款</a><a href="/policies" style="padding: 0px 15px; color: dimgray">社群規範</a><a href="/copyright" style="padding: 0px 15px; color: dimgray">版權申訴</a></span>
+<div class="hidden-xs hidden-sm" style="background-color: #e8eaed; width: 100%; position: absolute; bottom: 0px; padding: 10px 10px; z-index: 100000;">
+	<span style="float: left;"><a href="/contact" style="padding: 0px 15px; color: darkgray">廣告</a><a href="/about" style="padding: 0px 15px; color: darkgray">娛見日本</a><a href="/about" style="padding: 0px 15px; color: darkgray">關於</a><a href="/contact" style="padding: 0px 15px; color: darkgray">聯絡</a></span><span style="float: right"><a href="/terms" style="padding: 0px 15px; color: darkgray">服務條款</a><a href="/policies" style="padding: 0px 15px; color: darkgray">社群規範</a><a href="/copyright" style="padding: 0px 15px; color: darkgray">版權申訴</a></span>
 </div>
 
 @include('layouts.nav-bottom')
