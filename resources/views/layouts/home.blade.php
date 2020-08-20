2703abae007089fbd2184bfac465cbc795d86a70
laughseejapan <laughseejapan@gmail.com>
2020-08-20 17:30:01 +0800

    hanime1

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -33,7 +33,7 @@
 					<div style="font-size: 12px; margin-top: -1px">播放清單</div>
 				</div>
 				<div style="width: 33%; float:left; display: inline-block; margin-top: 10px;">
-					<a href="{{ route('video.watch') }}?v={{ $banner->id }}" target="_blank" class="hover-opacity-all home-banner-btn home-banner-play-btn" style="cursor: pointer; font-size: 14px; border-radius: 3px; text-decoration: none; background-color: white; padding: 7px 22px 7px 12px; color: black;"><span style="vertical-align: middle; font-size: 2em; margin-top: -3px; padding-right: 3px" class="material-icons">play_arrow</span>播放</a>
+					<a href="{{ route('video.watch') }}?v={{ $banner->id }}" target="_blank" class="hover-opacity-all home-banner-btn home-banner-play-btn" style="cursor: pointer; font-size: 14px; border-radius: 3px; text-decoration: none; background-color: white; padding: 8px 22px 8px 12px; color: black;"><span style="vertical-align: middle; font-size: 2em; margin-top: -3px; padding-right: 3px" class="material-icons">play_arrow</span>播放</a>
 				</div>
 				<a href="{{ route('video.info') }}?v={{ $banner->id }}" style="width: 33%; float:left; display: inline-block; color: white; text-decoration: none;">
 					<span class="material-icons">info</span>
