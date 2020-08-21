9ee399141ec6e494adf360c8aa7c7d4eb6fd65ff
laughseejapan <laughseejapan@gmail.com>
2020-08-21 20:22:00 +0800

    hanime1

--- resources/views/nav/main.blade.php
+++ resources/views/nav/main.blade.php
@@ -6,7 +6,7 @@
   <a class="nav-item" href="{{ route('home.search') }}">H動漫</a>
   <a href="https://www.laughseejapan.com/search?query=%E5%8B%95%E6%BC%AB" target="_blank" class="nav-item" href="javascript:void(0)">動畫卡通</a>
   <a class="nav-item" href="{{ route('home.search') }}">最新內容</a>
-  <a class="nav-item hidden-xs" href="{{ route('home.list') }}">我的清單</a>
+  <a class="nav-item hidden-xs" href="{{ Auth::check() ? route('home.list') : route('login') }}">我的清單</a>
 
   <a style="padding-right: 0px" class="nav-icon pull-right hidden-xs" href="javascript:void(0)"><span style="vertical-align: middle;" class="material-icons">account_circle</span></a>
   <a class="nav-icon pull-right hidden-xs" href="javascript:void(0)"><span style="vertical-align: middle;" class="material-icons">notifications</span></a>
