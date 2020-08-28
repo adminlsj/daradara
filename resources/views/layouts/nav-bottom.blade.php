c1be29ae72b0ff861e46b6eb05c14e01172ec851
laughseejapan <laughseejapan@gmail.com>
2020-08-28 18:10:26 +0800

    hanime1

--- resources/views/layouts/nav-bottom.blade.php
+++ resources/views/layouts/nav-bottom.blade.php
@@ -3,6 +3,10 @@
     <i style="font-size: 24px; margin-top: 7px" class="material-icons">home</i>
     <span style="padding-right: 1px;">主頁</span>
   </a>
+  <a href="{{ route('home.search') }}" class="{{ Request::is('*search*') ? 'active' : '' }}">
+    <i style="font-size: 27px; margin-top: 6px" class="material-icons">search</i>
+    <span>搜索</span>
+  </a>
   <a href="{{ Auth::check() ? route('home.list') : route('login') }}" class="{{ Request::is('*subscribes*') ? 'active' : ''}}">
     <i style="font-size: 23px; position: relative; margin-top: 7px" class="material-icons">subscriptions</i>
     <span style="padding-right: 1px;">訂閱項目</span>
@@ -17,9 +21,5 @@
   <a href="{{ route('home.search') }}" class="{{ Request::is('*newest*') ? 'active' : '' }}">
     <i style="padding-left: 2px; font-size: 25px; margin-top: 6px" class="material-icons">explore</i>
     <span>最新內容</span>
-  </a>
-  <a href="{{ Auth::check() ? route('home.list') : route('login') }}" class="{{ Request::is('*user*') ? 'active' : '' }}">
-    <i style="font-size: 23px; margin-top: 7px" class="material-icons">video_library</i>
-    <span>媒體庫</span>
   </a>
 </div>
