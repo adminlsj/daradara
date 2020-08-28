6cc598ffef258de2f841ac1a1f8f67d1bd915855
laughseejapan <laughseejapan@gmail.com>
2020-08-28 21:18:50 +0800

    hanime1

--- resources/views/layouts/nav-bottom.blade.php
+++ resources/views/layouts/nav-bottom.blade.php
@@ -9,10 +9,7 @@
   </a>
   <a href="{{ Auth::check() ? route('home.list') : route('login') }}" class="{{ Request::is('*subscribes*') ? 'active' : ''}}">
     <i style="font-size: 23px; position: relative; margin-top: 7px" class="material-icons">subscriptions</i>
-    <span style="padding-right: 1px;">訂閱項目</span>
-    @if (Auth::check() && strpos(auth()->user()->alert, 'subscribe') !== false)
-      <span style="position: absolute; top: -5px; right: calc(60% - 9px)" class="alert-circle"></span>
-    @endif
+    <span style="padding-right: 1px;">我的清單</span>
   </a>
   <a href="{{ route('home.search') }}?query=&sort=觀看次數" class="{{ Request::is('*rank*') ? 'active' : '' }}">
     <i style="padding-left: 0px; font-size: 24px;  margin-top: 7px" class="material-icons">whatshot</i>
