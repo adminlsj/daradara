596f1288759d8dc2bb71f84797e14432ad7ef437
laughseejapan <laughseejapan@gmail.com>
2020-08-28 21:13:13 +0800

    hanime1

--- resources/views/video/watch.blade.php
+++ resources/views/video/watch.blade.php
@@ -68,7 +68,7 @@
       <div style="width: 92%; margin-top: 18px;">
         <div data-toggle="modal" data-target="#playModal" style="cursor: pointer; font-size: 14px; border-radius: 3px; text-decoration: none; background-color: red; color: black; color: white; margin-left: 4%; padding: 5px 0" class="btn-block play-btn"><span style="vertical-align: middle; font-size: 2em; margin-top: -3px; padding-right: 3px; color: white;" class="material-icons">play_arrow</span>播放</div>
       </div>
-      <h4 style="overflow: hidden; text-overflow: ellipsis; display: -webkit-box; -webkit-line-clamp: 3; -webkit-box-orient: vertical; line-height: 16px; font-size: 12px; width: 92%; margin-left: 4%;">{{ $video->title }} • 中文字幕 • {{ $video->caption }}</h4>
+      <h4 style="overflow: hidden; text-overflow: ellipsis; display: -webkit-box; -webkit-line-clamp: 3; -webkit-box-orient: vertical; line-height: 16px; font-size: 12px; width: 92%; margin-left: 4%; text-align: left;">{{ $video->title }} • 中文字幕 • {{ $video->caption }}</h4>
       
       <div class="show-panel-icons" style="margin:0; margin-top: 15px;">
         <div id="video-like-form-wrapper" class="hover-opacity-all">
@@ -133,7 +133,7 @@
     </div>
   </div>
 
-  <img class="hidden-sm hidden-md hidden-lg" style="width: 100%; position: absolute; top: 0; left: 0; z-index: -1; filter: blur(10px) brightness(80%); -webkit-filter: blur(10px) brightness(80%); -webkit-mask-image:-webkit-gradient(linear, left top, left bottom, from(rgba(0,0,0,1)), to(rgba(0,0,0,0)));" src="{{ $video->cover }}">
+  <img class="hidden-sm hidden-md hidden-lg" style="width: 100%; position: absolute; top: 0; left: 0; z-index: -1; filter: blur(10px) brightness(80%); -webkit-filter: blur(10px) brightness(80%); -webkit-mask-image:-webkit-gradient(linear, left top, left bottom, from(rgba(0,0,0,1)), to(rgba(0,0,0,0))); padding-bottom: 20px" src="{{ $video->cover }}">
 
   <div id="home-rows-wrapper" class="intro-rows-wrapper" style="position: relative;">
     <h3>集數列表</h3>
