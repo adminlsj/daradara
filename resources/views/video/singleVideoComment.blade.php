d14d73bbdaeca6a728b121a83c888f759473b799
laughseejapan <laughseejapan@gmail.com>
2021-02-24 05:26:35 +0800

    hanime1

--- resources/views/video/singleVideoComment.blade.php
+++ resources/views/video/singleVideoComment.blade.php
@@ -31,7 +31,7 @@
 				  <img class="img-circle" style="width: 25px; height: auto; float:left;" src="{{ $reply->user->avatar == null ? $reply->user->avatarDefault() : $reply->user->avatar->filename }}">
 				</a>
 				<div class="comment-index-text" style="font-size: 0.9em; padding-left: 35px"><a style="text-decoration: none; color: darkgray;" href="{{ route('user.show', $reply->user) }}">{{ $reply->user->name }} • <span style="color: darkgray;">{{ Carbon\Carbon::parse($reply->created_at)->diffForHumans() }}</span></a></div>
-				<div class="comment-index-text" style="color: #222222; font-size: 1em; margin-top: 1px; padding-left: 35px">{{ $reply->text }}</div>
+				<div class="comment-index-text" style="color: white; font-size: 1em; margin-top: 1px; padding-left: 35px">{{ $reply->text }}</div>
 			</div>
 			<div style="padding-left: 35px; padding-top: 9px">
 				<div style="display: inline-block;" data-toggle="modal" data-target="#signUpModal">
