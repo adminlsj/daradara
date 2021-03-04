b18e1241bace1f254b6db68a9b7eee826406c419
laughseejapan <laughseejapan@gmail.com>
2021-03-05 04:52:45 +0800

    hanime1

--- resources/views/video/playlist-panel.blade.php
+++ resources/views/video/playlist-panel.blade.php
@@ -3,8 +3,8 @@
     <img style="object-fit: cover; width:58px; height:58px; border-radius: 50%; float: left; border: 2px solid gray" src="{{ $current->cover }}" alt="{{ $current->title }}">
     <h4 style="margin-top: 0px; line-height: 20px; margin-bottom: -5px; font-size: 14px; margin-left: 68px; color: white; overflow: hidden;text-overflow: ellipsis; display: -webkit-box; -webkit-line-clamp: 2; -webkit-box-orient: vertical; font-weight: bold; padding-right: 50px;">{{ $watch->title }}</h4>
     <h4 style="font-size: 12px; margin-left: 68px; color: darkgray; font-weight: bold;">{{ $watch->videos_count }} 部影片</h4>
-    <div id="hide-playlist-btn" style="position: absolute; right: 10px; top: 20px; cursor: pointer;" class="single-icon no-select hidden-md hidden-lg">
-      <i class="material-icons noselect" style="padding-top: 3px; padding-left: 2px; font-size: 30px; color: white">keyboard_arrow_down</i>
+    <div id="hide-playlist-btn" style="position: absolute; right: 10px; top: 21px; cursor: pointer;" class="single-icon no-select hidden-md hidden-lg">
+      <i class="material-icons noselect" style="padding-top: 3px; padding-left: 3px; font-size: 30px; color: white">keyboard_arrow_down</i>
     </div>
   </div>
   <div id="playlist-scroll" class="hover-video-playlist" style="text-align: left; max-height: 370px; overflow-y: scroll; position: relative">
