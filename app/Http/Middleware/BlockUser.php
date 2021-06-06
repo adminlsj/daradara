9efd6d589b5c145c180584e9c264464d9656b3c1
laughseejapan <laughseejapan@gmail.com>
2021-06-07 04:11:54 +0800

    hanime1

--- app/Http/Middleware/BlockUser.php
+++ app/Http/Middleware/BlockUser.php
@@ -15,7 +15,7 @@ class BlockUser
      */
     public function handle($request, Closure $next)
     {
-        if (/*!isset($_SERVER["HTTP_CF_IPCOUNTRY"]) || !isset($_SERVER["HTTP_CF_CONNECTING_IP"]) || */!isset($_SERVER["HTTP_USER_AGENT"])) {
+        if (!isset($_SERVER["HTTP_USER_AGENT"])) {
             abort(403);
         }
         // Log::info('USER INFO - COUNTRY CODE: '.$country_code.' / IP ADDRESS: '.$ip_address.' / USER AGENT: '.$user_agent);
