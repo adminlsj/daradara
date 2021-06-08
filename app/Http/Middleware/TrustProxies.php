db09e05baf8375b74b008a443c7111de43020a0a
laughseejapan <laughseejapan@gmail.com>
2021-06-09 02:15:02 +0800

    hanime1

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,9 @@ class TrustProxies extends Middleware
      *
      * @var array
      */
-    protected $proxies;
+    protected $proxies = [
+        '128.199.233.171',
+    ];
 
     /**
      * The headers that should be used to detect proxies.
