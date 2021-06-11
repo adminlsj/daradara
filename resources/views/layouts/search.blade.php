ad1b1ec7eed3d16c86b643ee759bdedf5a242eac
laughseejapan <laughseejapan@gmail.com>
2021-06-12 02:26:57 +0800

    hanime1

--- resources/views/layouts/search.blade.php
+++ resources/views/layouts/search.blade.php
@@ -217,7 +217,7 @@
 	    @else
 		    <div class="home-rows-videos-wrapper" style="white-space: normal; margin-left: -2px; margin-right: -2px;">
 			    @foreach ($videos as $video)
-			      	<a style="text-decoration: none;" href="{{ route('video.watch') }}?v={{ $video['id'] }}" target="_blank">
+			      	<a style="text-decoration: none;" href="{{ route('video.watch') }}?v={{ $video['id'] }}">
 				        <div class="home-rows-videos-div" style="position: relative; display: inline-block; margin-bottom:50px;">
 				          <img src="{{ $video['cover'] }}">
 				          <div class="home-rows-videos-title" style="position:absolute; bottom:0; left:0; white-space: initial; overflow: hidden;text-overflow: ellipsis; display: -webkit-box; -webkit-line-clamp: 2; -webkit-box-orient: vertical; color: white; width: 100%; padding: 3px 5px; background: linear-gradient(to bottom, transparent 0%, black 120%);">{{ $video['title'] }}</div>
