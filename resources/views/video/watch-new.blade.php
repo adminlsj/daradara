643628435e8bc3730b0a8b97c372467baa1007d8
laughseejapan <laughseejapan@gmail.com>
2021-06-11 21:59:24 +0800

    hanime1

--- resources/views/video/watch-new.blade.php
+++ resources/views/video/watch-new.blade.php
@@ -25,13 +25,13 @@
 
         @else
 
-          @include('video.player')
+          <!-- @include('video.player') -->
           
-          <!-- @if (strpos($video->sd, '.m3u8'))
+          @if (strpos($video->sd, '.m3u8'))
             @include('video.player-m3u8')
           @else
             @include('video.player-mp4')
-          @endif -->
+          @endif
 
         @endif
       @endif
