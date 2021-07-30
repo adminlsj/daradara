07800719f8711e06dc7b9f15eeaa040d7878f1c7
laughseejapan <laughseejapan@gmail.com>
2021-07-30 09:38:37 +0800

    hanime1

--- app/User.php
+++ app/User.php
@@ -38,81 +38,10 @@ class User extends Authenticatable
         }
     }
 
-    public function recommendTags()
-    {
-        $tags = [];
-        $subscribes = $this->subscribes();
-        foreach ($subscribes as $subscribe) {
-            if ($subscribe->type == 'watch' && $watch = $subscribe->watch()) {
-                $videoTags = $watch->videos()->first()->tags;
-                if (strpos($videoTags, '動漫') !== false && !in_array('動漫新番', $tags)) {
-                    array_unshift($tags, '動漫新番');
-                    if (!in_array('同人動畫', $tags)) {
-                        array_push($tags, '同人動畫');
-                    }
-                    if (!in_array('原創動畫', $tags)) {
-                        array_push($tags, '原創動畫');
-                    }
-                    if (!in_array('動漫講評', $tags)) {
-                        array_push($tags, '動漫講評');
-                    }
-                    if (!in_array('MAD·AMV', $tags)) {
-                        array_push($tags, 'MAD·AMV');
-                    }
-                }
-                if (strpos($videoTags, '日劇') !== false && !in_array('日劇', $tags)) {
-                    array_unshift($tags, '日劇');
-                    if (!in_array('明星', $tags)) {
-                        array_push($tags, '明星');
-                    }
-                    if (!in_array('日本人氣YouTuber', $tags)) {
-                        array_push($tags, '日本人氣YouTuber');
-                    }
-                    if (!in_array('日劇講評', $tags)) {
-                        array_push($tags, '日劇講評');
-                    }
-                }
-                if (strpos($videoTags, '綜藝') !== false && !in_array('綜藝', $tags)) {
-                    array_unshift($tags, '綜藝');
-                    if (!in_array('明星', $tags)) {
-                        array_push($tags, '明星');
-                    }
-                    if (!in_array('日本人氣YouTuber', $tags)) {
-                        array_push($tags, '日本人氣YouTuber');
-                    }
-                    if (!in_array('日本創意廣告', $tags)) {
-                        array_push($tags, '日本創意廣告');
-                    }
-                }
-            } elseif ($subscribe->type == 'video') {
-                if (!in_array($subscribe->tag, $tags)) {
-                    array_push($tags, $subscribe->tag);
-                }
-            }
-        }
-
-        if (!in_array('日劇', $tags)) {
-            array_push($tags, '日劇');
-        }
-        if (!in_array('綜藝', $tags)) {
-            array_push($tags, '綜藝');
-        }
-        if (!in_array('動漫新番', $tags)) {
-            array_push($tags, '動漫');
-        }
-
-        return $tags;
-    }
-
     public function watches()
     {
-        return Watch::where('user_id', $this->id)->orderBy('updated_at', 'desc')->get();
+        return $this->hasMany('App\Watch');
     }
-
-    /* public function videos()
-    {
-        return Video::where('user_id', $this->id)->orderBy('uploaded_at', 'desc')->get();
-    } */
 
     public function videos()
     {
