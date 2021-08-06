b684ddd822f696550083233633d9b8c859e2c197
laughseejapan <laughseejapan@gmail.com>
2021-08-06 13:41:53 +0800

    hanime1

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -19,9 +19,9 @@ class BotController extends Controller
         ini_set('max_execution_time', 0);
         ini_set('memory_limit', '-1');
 
-        $videos = Video::where('tags', 'ilike', '%WHITE BEAR%')->get();
+        $videos = Video::where('tags', 'ilike', '%CEO NEET%')->get();
         foreach ($videos as $video) {
-            $video->tags = str_replace('WHITE BEAR', 'WhiteBear', $video->tags);
+            $video->tags = str_replace('CEO NEET', 'CeoNeet', $video->tags);
             $video->save();
         }
 
