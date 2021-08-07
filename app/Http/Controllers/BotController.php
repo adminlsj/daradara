d144fd434425779f05d4dc54c79e67f2806441de
laughseejapan <laughseejapan@gmail.com>
2021-08-07 15:34:05 +0800

    hanime1

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -21,12 +21,12 @@ class BotController extends Controller
         ini_set('max_execution_time', 0);
         ini_set('memory_limit', '-1');
 
-        $videos = Video::where('tags', 'ilike', '% 睪丸 %')->get();
+        $videos = Video::where('tags', 'ilike', '% 張嘴 %')->get();
         foreach ($videos as $video) {
             $tags_array = $video->tags_array;
-            unset($tags_array['睪丸']);
+            unset($tags_array['張嘴']);
             $video->tags_array = $tags_array;
-            $video->tags = str_replace(' 睪丸 ', ' ', $video->tags);
+            $video->tags = str_replace(' 張嘴 ', ' ', $video->tags);
             $video->save();
         }
 
