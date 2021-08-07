2cd8796836e23bc0e3576043640eb31b09113fac
laughseejapan <laughseejapan@gmail.com>
2021-08-07 15:40:27 +0800

    hanime1

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -21,12 +21,12 @@ class BotController extends Controller
         ini_set('max_execution_time', 0);
         ini_set('memory_limit', '-1');
 
-        $videos = Video::where('tags', 'ilike', '% 性交 %')->get();
+        $videos = Video::where('tags', 'ilike', '% 肉棒 %')->get();
         foreach ($videos as $video) {
             $tags_array = $video->tags_array;
-            unset($tags_array['性交']);
+            unset($tags_array['肉棒']);
             $video->tags_array = $tags_array;
-            $video->tags = str_replace(' 性交 ', ' ', $video->tags);
+            $video->tags = str_replace(' 肉棒 ', ' ', $video->tags);
             $video->save();
         }
 
