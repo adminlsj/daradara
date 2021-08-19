9e8c23756e2ef76d2400ff827da8ca2c957e598f
laughseejapan <laughseejapan@gmail.com>
2021-08-19 18:05:10 +0800

    hanime1

--- app/Console/Commands/UpdateSpankbangBackup.php
+++ app/Console/Commands/UpdateSpankbangBackup.php
@@ -3,13 +3,7 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
-use App\Video;
-use Mail;
-use App\Mail\UserReport;
-use Spatie\Browsershot\Browsershot;
-use Illuminate\Support\Facades\Log;
-use Carbon\Carbon;
-use App\Helper;
+use App\Spankbang;
 
 class UpdateSpankbangBackup extends Command
 {
@@ -44,82 +38,6 @@ class UpdateSpankbangBackup extends Command
      */
     public function handle()
     {
-        Log::info('Spankbang backup update started...');
-
-        $videos = Video::where('foreign_sd', 'ilike', '%"spankbang"%')->select('id', 'title', 'sd', 'outsource', 'tags_array', 'foreign_sd', 'created_at')->get();
-
-        $base = Carbon::now()->addHours(3)->timestamp;
-
-        foreach ($videos as $video) {
-            $time = Helper::get_string_between($video->sd, ',', '&m=');
-            if ($time < $base) {
-                $pass = false;
-                $sd = '';
-                $source = '';
-                $qualities = [];
-
-                /* $requests = Browsershot::url($video->foreign_sd['spankbang'])
-                    ->useCookies(['username' => 'admin'])
-                    ->timeout(3600)
-                    ->userAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36')
-                    ->triggeredRequests();
-
-                foreach ($requests as $request) {
-                    if (strpos($request['url'], 'p.mp4') !== false) {
-                        $sd = $request['url'];
-                        $source = $request['url'];
-                        $pass = true;
-                    }
-                } */
-
-                $curl_connection = curl_init($video->foreign_sd['spankbang']);
-                curl_setopt($curl_connection, CURLOPT_CONNECTTIMEOUT, 30);
-                curl_setopt($curl_connection, CURLOPT_RETURNTRANSFER, true);
-                curl_setopt($curl_connection, CURLOPT_SSL_VERIFYPEER, false);
-                $html = curl_exec($curl_connection);
-                curl_close($curl_connection);
-
-                $sd = Helper::get_string_between($html, '"contentUrl": "', '"');
-                $source = Helper::get_string_between($html, '"contentUrl": "', '"');
-                if (strpos($sd, 'https://vdownload') !== false) {
-                    $pass = true;
-                }
-
-                if ($pass) {
-                    if (in_array('1080p', array_keys($video->tags_array))) {
-                        $sd = str_replace('-720p.mp4', '-1080p.mp4', $sd);
-                        $qualities['1080'] = $sd;
-                    }
-                    if (strpos($source, '720p') !== false) {
-                        $qualities['720'] = $source;
-                        $source = str_replace('-720p.mp4', '-480p.mp4', $source);
-                    }
-                    if (strpos($source, '480p') !== false) {
-                        $qualities['480'] = $source;
-                        $source = str_replace('-480p.mp4', '-240p.mp4', $source);
-                    }
-                    if (strpos($source, '240p') !== false) {
-                        $qualities['240'] = $source;
-                    }
-
-                    $video->sd = reset($qualities);
-                    $video->qualities = $qualities;
-                    $video->outsource = false;
-                    $video->save();
-
-                } else {
-                    Mail::to('vicky.avionteam@gmail.com')->send(new UserReport('master', 'Spankbang update failed', $video->id, $video->title, $video->sd, 'master', 'master'));
-                    $temp = $video->foreign_sd;
-                    $temp['error'] = $video->foreign_sd['spankbang'];
-                    unset($temp['spankbang']);
-                    $video->foreign_sd = $temp;
-                    $video->save();
-                }
-
-                sleep(10);
-            }
-        }
-
-        Log::info('Spankbang backup update ended...');
+        Spankbang::updateSpankbangBackup();
     }
 }
--- app/Spankbang.php
+++ app/Spankbang.php
@@ -14,7 +14,58 @@ class Spankbang
 {
     public static function updateSpankbang()
     {
+        Log::info('Spankbang update started...');
+
         $videos = Video::where('foreign_sd', 'ilike', '%"spankbang"%')->select('id', 'title', 'sd', 'outsource', 'tags_array', 'foreign_sd', 'created_at')->get();
+
+        foreach ($videos as $video) {
+
+            $curl_connection = curl_init($video->foreign_sd['spankbang']);
+            curl_setopt($curl_connection, CURLOPT_CONNECTTIMEOUT, 30);
+            curl_setopt($curl_connection, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($curl_connection, CURLOPT_SSL_VERIFYPEER, false);
+            $html = curl_exec($curl_connection);
+            curl_close($curl_connection);
+
+            $sd = Helper::get_string_between($html, '"contentUrl": "', '"');
+            $source = Helper::get_string_between($html, '"contentUrl": "', '"');
+            $qualities = [];
+
+            if (strpos($sd, 'https://vdownload') !== false) {
+                if (in_array('1080p', array_keys($video->tags_array))) {
+                    $sd = str_replace('-720p.mp4', '-1080p.mp4', $sd);
+                    $qualities['1080'] = $sd;
+                }
+                if (strpos($source, '720p') !== false) {
+                    $qualities['720'] = $source;
+                    $source = str_replace('-720p.mp4', '-480p.mp4', $source);
+                }
+                if (strpos($source, '480p') !== false) {
+                    $qualities['480'] = $source;
+                    $source = str_replace('-480p.mp4', '-240p.mp4', $source);
+                }
+                if (strpos($source, '240p') !== false) {
+                    $qualities['240'] = $source;
+                }
+
+                $video->sd = reset($qualities);
+                $video->qualities = $qualities;
+                $video->outsource = false;
+                $video->save();
+
+            } else {
+                Mail::to('vicky.avionteam@gmail.com')->send(new UserReport('master', 'Spankbang update failed', $video->id, $video->title, $video->sd, 'master', 'master'));
+                $temp = $video->foreign_sd;
+                $temp['error'] = $video->foreign_sd['spankbang'];
+                unset($temp['spankbang']);
+                $video->foreign_sd = $temp;
+                $video->save();
+            }
+
+            sleep(10);
+        }
+
+        /* $videos = Video::where('foreign_sd', 'ilike', '%"spankbang"%')->select('id', 'title', 'sd', 'outsource', 'tags_array', 'foreign_sd', 'created_at')->get();
 
         foreach ($videos as $video) {
 
@@ -68,8 +119,9 @@ class Spankbang
                 $video->foreign_sd = $temp;
                 $video->save();
             }
+        } */
 
-        }
+        Log::info('Spankbang update ended...');
     }
 
     public static function updateSpankbangBackup()
@@ -86,10 +138,9 @@ class Spankbang
                 $pass = false;
                 $sd = '';
                 $source = '';
-                $default = '';
                 $qualities = [];
 
-                $requests = Browsershot::url($video->foreign_sd['spankbang'])
+                /* $requests = Browsershot::url($video->foreign_sd['spankbang'])
                     ->useCookies(['username' => 'admin'])
                     ->timeout(3600)
                     ->userAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36')
@@ -101,6 +152,19 @@ class Spankbang
                         $source = $request['url'];
                         $pass = true;
                     }
+                } */
+
+                $curl_connection = curl_init($video->foreign_sd['spankbang']);
+                curl_setopt($curl_connection, CURLOPT_CONNECTTIMEOUT, 30);
+                curl_setopt($curl_connection, CURLOPT_RETURNTRANSFER, true);
+                curl_setopt($curl_connection, CURLOPT_SSL_VERIFYPEER, false);
+                $html = curl_exec($curl_connection);
+                curl_close($curl_connection);
+
+                $sd = Helper::get_string_between($html, '"contentUrl": "', '"');
+                $source = Helper::get_string_between($html, '"contentUrl": "', '"');
+                if (strpos($sd, 'https://vdownload') !== false) {
+                    $pass = true;
                 }
 
                 if ($pass) {
@@ -133,6 +197,8 @@ class Spankbang
                     $video->foreign_sd = $temp;
                     $video->save();
                 }
+
+                sleep(10);
             }
         }
 
