9bee6dd5a3476d423ded1e59d40921b2ea957b30
laughseejapan <laughseejapan@gmail.com>
2021-08-26 19:54:24 +0800

    hanime1

--- resources/views/video/watch-new.blade.php
+++ resources/views/video/watch-new.blade.php
@@ -36,6 +36,12 @@
         <ins class="adsbyexoclick" data-zoneid="4372406"></ins> 
       </div>
 
+      @if ($video->comic_id)
+        <a id="watch-comics-link-btn" href="{{ route('comic.showCover', ['comic' => $video->comic_id]) }}" target="_blank" class="no-select" style="background-color: crimson; border: none; border-radius: 5px; color: #d9d9d9; display: block; text-decoration: none; cursor: pointer;">
+          <span style="vertical-align: middle; font-size: 18px; margin-top: -3px; margin-right: 5px; cursor: pointer;" class="material-icons">visibility</span>漫畫鏈結
+        </a>
+      @endif
+
       <div class="video-show-panel-width">
         <div style="margin-bottom: -6px;">
           <p style="font-size: 12px; color: #bdbdbd; font-weight: 500">{{ Carbon\Carbon::parse($video->created_at)->format('Y-m-d') }} <span style="font-weight: normal;">&nbsp;|&nbsp;</span> {{ $video->views() }}次點閱</p>
