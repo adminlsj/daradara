21facd4bf6217252e0fc09020702c3a4f68a21e8
laughseejapan <laughseejapan@gmail.com>
2021-09-05 20:20:44 +0800

    hanime1

--- app/Cosplayjav.php
+++ app/Cosplayjav.php
@@ -9,10 +9,19 @@ use Carbon\Carbon;
 class Cosplayjav
 {
     public static $removed = [
-        "1080p", "60fps", "jav_full_hd", "tagme actress", "tagme_actress", "jav", "sex", "720p", "anime", "video", "wings", "cosplay", "idolmaster", "cosplay_sex", "split_roast", "koshimizu_sachiko", "Sano_Ai", "idolmaster_cinderella_girls", "dressing", "Kosutchi", "lightning", "pantsu", "final_fantasy", "doujin", "imageset", "Dangan_ronpa", "Enoshima_Junko", "bondage", "bukkake", "SAIT-024", "anal_toys", "nishita_karina", "Nanpa_Mokushiroku", "Violet Evergarden", "Violet_Evergarden", "NCY-104", "astolfo", "fate/apocrypha", "touhou", "kirisame_marisa", "kirito", "SDAB-183", "SOD_Create", "Hayami_Nana", "tagme_series", "tagme_character", "sword_art_online", "MIAA-448", "fukada_eimi", "NieR_Automata", "Yorha_No.2_Type_B", "MMUS-053", "grinding", "handjob_cum", "tohsaka_rin", "Momose_Asuka", "fate_stay/night", "love_live!_sunshine!", "tagme character", "fate_series", "shinon", "full_hd", "HOIZ-022", "Wakamiya Hono (若宮穂乃), Sakuraba Hikari (桜庭ひかり), Inaba Ruka (稲場るか)", "cum_on_belly", "Mashu_Kyrielight", "NCYF-008", "tagme series (私にタグ シリーズ)", "Kuroki_Ikumi", "shuten_douji"
+        "1080p", "60fps", "jav_full_hd", "tagme actress", "tagme_actress", "jav", "sex", "720p", "anime", "video", "wings", "cosplay", "idolmaster", "cosplay_sex", "split_roast", "koshimizu_sachiko", "Sano_Ai", "idolmaster_cinderella_girls", "dressing", "Kosutchi", "lightning", "pantsu", "final_fantasy", "doujin", "imageset", "Dangan_ronpa", "Enoshima_Junko", "bondage", "bukkake", "SAIT-024", "anal_toys", "nishita_karina", "Nanpa_Mokushiroku", "Violet Evergarden", "Violet_Evergarden", "NCY-104", "astolfo", "fate/apocrypha", "touhou", "kirisame_marisa", "kirito", "SDAB-183", "SOD_Create", "Hayami_Nana", "tagme_series", "tagme_character", "sword_art_online", "MIAA-448", "fukada_eimi", "NieR_Automata", "Yorha_No.2_Type_B", "MMUS-053", "grinding", "handjob_cum", "tohsaka_rin", "Momose_Asuka", "fate_stay/night", "love_live!_sunshine!", "tagme character", "fate_series", "shinon", "full_hd", "HOIZ-022", "Wakamiya Hono (若宮穂乃), Sakuraba Hikari (桜庭ひかり), Inaba Ruka (稲場るか)", "cum_on_belly", "Mashu_Kyrielight", "NCYF-008", "tagme series (私にタグ シリーズ)", "Kuroki_Ikumi", "shuten_douji", "DBVB-024", "crossdresser"
     ];
 
     public static $translations = [
+        "Felix Argail / Ferris (フェリックス・アーガイル / フェリス)" => '菲利克斯·阿吉爾',
+        "Tamamo no Mae (玉藻の前)" => '玉藻前',
+        "Re: Zero kara Hajimeru Isekai Seikatsu (Re：ゼロから始める異世界生活 )" => 'Re：從零開始的異世界生活',
+        "Satsuki (さつき)" => 'Satsuki',
+        "crossdress" => '異性裝扮',
+        "brown_hair" => '棕髮',
+        "fox_ears" => '狐狸耳',
+        "cat_ears" => '貓耳',
+        "gay" => '耽美',
         "Shuten Douji (酒呑童子)" => '酒吞童子',
         "Punimoe" => 'ぷにもえ！',
         "Kuroki Ikumi (黒木いくみ)" => '黑木郁美',
