585129169dd1dc77900a59facef28bb37af2f319
laughseejapan <laughseejapan@gmail.com>
2021-09-06 13:59:55 +0800

    hanime1

--- app/Cosplayjav.php
+++ app/Cosplayjav.php
@@ -9,10 +9,15 @@ use Carbon\Carbon;
 class Cosplayjav
 {
     public static $removed = [
-        "1080p", "60fps", "jav_full_hd", "tagme actress", "tagme_actress", "jav", "sex", "720p", "anime", "video", "wings", "cosplay", "idolmaster", "cosplay_sex", "split_roast", "koshimizu_sachiko", "Sano_Ai", "idolmaster_cinderella_girls", "dressing", "Kosutchi", "lightning", "pantsu", "final_fantasy", "doujin", "imageset", "Dangan_ronpa", "Enoshima_Junko", "bondage", "bukkake", "SAIT-024", "anal_toys", "nishita_karina", "Nanpa_Mokushiroku", "Violet Evergarden", "Violet_Evergarden", "NCY-104", "astolfo", "fate/apocrypha", "touhou", "kirisame_marisa", "kirito", "SDAB-183", "SOD_Create", "Hayami_Nana", "tagme_series", "tagme_character", "sword_art_online", "MIAA-448", "fukada_eimi", "NieR_Automata", "Yorha_No.2_Type_B", "MMUS-053", "grinding", "handjob_cum", "tohsaka_rin", "Momose_Asuka", "fate_stay/night", "love_live!_sunshine!", "tagme character", "fate_series", "shinon", "full_hd", "HOIZ-022", "Wakamiya Hono (若宮穂乃), Sakuraba Hikari (桜庭ひかり), Inaba Ruka (稲場るか)", "cum_on_belly", "Mashu_Kyrielight", "NCYF-008", "tagme series (私にタグ シリーズ)", "Kuroki_Ikumi", "shuten_douji", "DBVB-024", "crossdresser", "xidaidai", "phone_ver", "vaginal_sticker", "phone_resolution", "tagme series (私にタグ シリーズ)", "feet", "solo", "eromanga_sensei", "keqing", "Xingqiu"
+        "1080p", "60fps", "jav_full_hd", "tagme actress", "tagme_actress", "jav", "sex", "720p", "anime", "video", "wings", "cosplay", "idolmaster", "cosplay_sex", "split_roast", "koshimizu_sachiko", "Sano_Ai", "idolmaster_cinderella_girls", "dressing", "Kosutchi", "lightning", "pantsu", "final_fantasy", "doujin", "imageset", "Dangan_ronpa", "Enoshima_Junko", "bondage", "bukkake", "SAIT-024", "anal_toys", "nishita_karina", "Nanpa_Mokushiroku", "Violet Evergarden", "Violet_Evergarden", "NCY-104", "astolfo", "fate/apocrypha", "touhou", "kirisame_marisa", "kirito", "SDAB-183", "SOD_Create", "Hayami_Nana", "tagme_series", "tagme_character", "sword_art_online", "MIAA-448", "fukada_eimi", "NieR_Automata", "Yorha_No.2_Type_B", "MMUS-053", "grinding", "handjob_cum", "tohsaka_rin", "Momose_Asuka", "fate_stay/night", "love_live!_sunshine!", "tagme character", "fate_series", "shinon", "full_hd", "HOIZ-022", "Wakamiya Hono (若宮穂乃), Sakuraba Hikari (桜庭ひかり), Inaba Ruka (稲場るか)", "cum_on_belly", "Mashu_Kyrielight", "NCYF-008", "tagme series (私にタグ シリーズ)", "Kuroki_Ikumi", "shuten_douji", "DBVB-024", "crossdresser", "xidaidai", "phone_ver", "vaginal_sticker", "phone_resolution", "tagme series (私にタグ シリーズ)", "feet", "solo", "eromanga_sensei", "keqing", "Xingqiu", "CPDE-048"
     ];
 
     public static $translations = [
+        "Sakurai Chiharu (桜井千春)" => '櫻井千春',
+        "keqing (刻晴)" => '刻晴',
+        "priest_uniform" => '修女',
+        "prestige" => 'PRESTIGE',
+        "assjob" => '素股',
         "Keqing (Chinese: 刻晴 Kèqíng, &#8220;Sunny Moment&#8221; or &#8220;Delicate Carving&#8221;)" => '刻晴',
         "Xingqiu (Chinese: 行秋 Xíngqiū, &#8220;Progression of Autumn&#8221;)" => '行秋',
         "Izumi Sagiri (和泉 紗霧)" => '和泉紗霧',
