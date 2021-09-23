8f891078fca9955c93a760a4eb78935017e578c6
laughseejapan <laughseejapan@gmail.com>
2021-09-23 19:30:46 +0800

    hanime1

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -32,8 +32,53 @@ class BotController extends Controller
             curl_setopt($curl_connection, CURLOPT_CONNECTTIMEOUT, 30);
             curl_setopt($curl_connection, CURLOPT_RETURNTRANSFER, true);
             curl_setopt($curl_connection, CURLOPT_SSL_VERIFYPEER, false);
-            return $html = curl_exec($curl_connection);
+            $html = curl_exec($curl_connection);
             curl_close($curl_connection);
+
+            if (strpos($html, "html5player.setVideoHLS('") !== false) {
+                $m3u8 = Helper::get_string_between($html, "html5player.setVideoHLS('", "');");
+                $curl_connection = curl_init($m3u8);
+                curl_setopt($curl_connection, CURLOPT_CONNECTTIMEOUT, 30);
+                curl_setopt($curl_connection, CURLOPT_RETURNTRANSFER, true);
+                curl_setopt($curl_connection, CURLOPT_SSL_VERIFYPEER, false);
+                return $data = curl_exec($curl_connection);
+                curl_close($curl_connection);
+
+                $array = explode('#EXT-X-STREAM-INF', $data);
+                array_shift($array);
+
+                $qualities = [];
+                $m3u8_array = explode('/', $m3u8);
+                array_pop($m3u8_array);
+                $preset = implode('/', $m3u8_array).'/';
+                foreach ($array as $item) {
+                    $quality = Helper::get_string_between($item, 'NAME="', '"');
+                    $source = $preset.trim(explode('NAME="'.$quality.'"', $item)[1]);
+                    $qualities[str_replace('p', '', $quality)] = $source;
+                }
+                if (array_key_exists(1080, $qualities)) {
+                   $video->sd = $qualities[1080];
+                } elseif (array_key_exists(720, $qualities)) {
+                    $video->sd = $qualities[720];
+                } elseif (array_key_exists(480, $qualities)) {
+                    $video->sd = $qualities[480];
+                } elseif (array_key_exists(360, $qualities)) {
+                    $video->sd = $qualities[360];
+                } elseif (array_key_exists(250, $qualities)) {
+                    $video->sd = $qualities[250];
+                }
+
+                $video->outsource = false;
+                $video->save();
+
+            } else {
+                Mail::to('vicky.avionteam@gmail.com')->send(new UserReport('master', 'Xvideos update failed', $video->id, $video->title, $video->sd, 'master', 'master'));
+                $temp = $video->foreign_sd;
+                $temp['error'] = $video->foreign_sd['xvideos'];
+                unset($temp['xvideos']);
+                $video->foreign_sd = $temp;
+                $video->save();
+            }
         }
 
         /* $comics = Comic::orderBy('id', 'asc')->get();
