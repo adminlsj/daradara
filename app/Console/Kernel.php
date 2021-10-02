9c0cf05696577817be8c801107e0b948a8d1a066
laughseejapan <laughseejapan@gmail.com>
2021-10-02 20:08:49 +0800

    hanime1

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -37,20 +37,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('hanime1:update-data')->dailyAt('05:00');
-        $schedule->command('hanime1:reset-day-views')->dailyAt('05:00');
-        $schedule->command('hanime1:reset-week-views')->weeklyOn(1, '05:00');
-        $schedule->command('hanime1:check-motherless')->dailyAt('05:00');
-
-        $schedule->command('hanime1:update-xvideos')->hourly();
-        $schedule->command('hanime1:update-xvideoserrors')->hourly();
-        $schedule->command('hanime1:update-spankbang')->cron('0 */3 * * *')->between('2:00', '22:00');
-        $schedule->command('hanime1:update-spankbangbackup')->hourly();
-        $schedule->command('hanime1:update-spankbangerrors')->hourly();
-        $schedule->command('hanime1:update-youjizz')->cron('0 */4 * * *');
-
-        $schedule->command('hanime1:upload-nhentai')->hourly()->between('6:00', '21:00');
-        $schedule->command('hanime1:upload-rule34')->hourly()->between('6:00', '21:00');
+        $schedule->command('hanime1:update-xvideoserrors')->everyThirtyMinutes();
     }
 
     /**
