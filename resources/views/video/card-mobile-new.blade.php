c966c6110cb20c1feb5685dfffe2303412c50388
laughseejapan <laughseejapan@gmail.com>
2021-10-05 18:36:31 +0800

    hanime1

--- resources/views/video/card-mobile-new.blade.php
+++ resources/views/video/card-mobile-new.blade.php
@@ -19,11 +19,11 @@
 	    </div>
 	</a>
 
-	<div style="margin-top: 5px; padding: 0 7px;">
+	<div style="margin-top: 6px; padding: 0 8px;">
 		<div style="text-decoration: none; color: black;">
 			<div style="font-size: 0.85em; line-height: 17px; color: white; overflow: hidden;text-overflow: ellipsis; display: -webkit-box; -webkit-line-clamp: 2; -webkit-box-orient: vertical; font-weight: 400; height: 31px">{{ $video->title }}</div>
 
-			<div style="margin-top: 10px; padding-bottom: 7px; font-weight: 400">
+			<div style="margin-top: 9px; padding-bottom: 7px; font-weight: 400">
 				@if (array_key_exists('3D', $video->tags_array))
 					<span class="card-mobile-genre" style="color: rgba(245, 171, 53, 1); border-color: rgba(245, 171, 53, 0.30);">3D</span>
 				@elseif (array_key_exists('同人', $video->tags_array))
