a3699d1d9ea4ac143826bb177afc5a8947ab6875
laughseejapan <laughseejapan@gmail.com>
2021-10-07 19:58:25 +0800

    hanime1

--- resources/views/video/player-mp4.blade.php
+++ resources/views/video/player-mp4.blade.php
@@ -28,7 +28,24 @@
     },
     quality: {
       default: 720
-    }
+    },
+    i18n: {
+      rewind: 'Rewind 5s',
+      fastForward: 'Forward 5s',
+    },
+    controls: [
+    'play-large', // The large play button in the center
+    'rewind', // Rewind by the seek time (default 10 seconds)
+    'play', // Play/pause playback
+    'fast-forward', // Fast forward by the seek time (default 10 seconds)
+    'progress', // The progress bar and scrubber for playback and buffering
+    'current-time', // The current time of playback
+    'mute', // Toggle mute
+    'volume', // Volume control
+    'captions', // Toggle captions
+    'settings', // Settings menu
+    'fullscreen', // Toggle fullscreen
+    ]
   });
   window.player = player;
 
