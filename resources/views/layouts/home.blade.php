0ef705ad1619ad017b17b53f1842389b17907a02
laughseejapan <laughseejapan@gmail.com>
2021-10-20 02:47:54 +0800

    hanime1

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -7,15 +7,13 @@
 @section('content')
 <div class="hidden-xs" style="background-color: #222222; color: #A3A3A3; width: 100%; height: 57px; line-height: 57px; padding: 0 4%; font-weight: 400; font-size: 14px; text-align: center;">
 	<a href="https://theporndude.com/zh" style="color: #A3A3A3; text-decoration: underline; padding: 10px;" target="_blank">PornDude</a>
+	<a href="https://141jj.com/" style="color: #A3A3A3; text-decoration: underline; padding: 10px;" target="_blank">141JJ</a>
 	<a href="https://www.moefuns.net" style="color: #A3A3A3; text-decoration: underline; padding: 10px;" target="_blank">萌翻導航</a>
 	<a href="https://www.mmrkdh1.com" style="color: #A3A3A3; text-decoration: underline; padding: 10px;" target="_blank">秘密入口</a>
-	<a href="https://www.sshs.pw/" style="color: #A3A3A3; text-decoration: underline; padding: 10px;" target="_blank">紳士會所</a>
+	<a href="https://www.17dm.net/" style="color: #A3A3A3; text-decoration: underline; padding: 10px;" target="_blank">妖氣動漫導航</a>
+  <a href="https://share.acgnx.net/" style="color: #A3A3A3; text-decoration: underline; padding: 10px;" target="_blank">末日動漫資源庫</a>
   <a href="https://www.gal123.com/" style="color: #A3A3A3; text-decoration: underline; padding: 10px;" target="_blank">紳士導航♂</a>
-  <a href="https://www.17dm.net/" style="color: #A3A3A3; text-decoration: underline; padding: 10px;" target="_blank">妖氣動漫導航</a>
-
-  <!-- 
-  	<a href="https://xx-map.com/" style="color: #A3A3A3; text-decoration: underline; padding: 10px;" target="_blank">杏MAP</a>
-  -->
+	<a href="https://www.sshs.pw/" style="color: #A3A3A3; text-decoration: underline; padding: 10px;" target="_blank">紳士會所</a>
 </div>
 
 <div class="nav-bottom-padding">
--- resources/views/layouts/nav-bottom.blade.php
+++ resources/views/layouts/nav-bottom.blade.php
@@ -19,7 +19,7 @@
       <i style="font-size: 26px; margin-top: 11px;" class="material-icons-outlined">video_library</i>
     @endif
   </a>
-  <a data-toggle="modal" data-target="#links-modal">
+  <a style="cursor: pointer;" data-toggle="modal" data-target="#links-modal">
     <i style="padding-left: 2px; font-size: 29px; margin-top: 10px" class="material-icons">menu</i>
   </a>
 </div>
@@ -34,6 +34,34 @@
       <div class="modal-body" style="padding: 0;">
         <a style="color: white; text-decoration: none;" href="https://theporndude.com/zh" target="_blank">
           <div class="simple-dropdown-item genre-option">PornDude</div>
+        </a>
+        <hr style="margin: 0; border-color: #333333;">
+        <a style="color: white; text-decoration: none;" href="https://141jj.com/" target="_blank">
+          <div class="simple-dropdown-item genre-option">141JJ</div>
+        </a>
+        <hr style="margin: 0; border-color: #333333;">
+        <a style="color: white; text-decoration: none;" href="https://www.moefuns.net" target="_blank">
+          <div class="simple-dropdown-item genre-option">萌翻導航</div>
+        </a>
+        <hr style="margin: 0; border-color: #333333;">
+        <a style="color: white; text-decoration: none;" href="https://www.mmrkdh1.com" target="_blank">
+          <div class="simple-dropdown-item genre-option">秘密入口</div>
+        </a>
+        <hr style="margin: 0; border-color: #333333;">
+        <a style="color: white; text-decoration: none;" href="https://www.17dm.net/" target="_blank">
+          <div class="simple-dropdown-item genre-option">妖氣動漫導航</div>
+        </a>
+        <hr style="margin: 0; border-color: #333333;">
+        <a style="color: white; text-decoration: none;" href="https://share.acgnx.net/" target="_blank">
+          <div class="simple-dropdown-item genre-option">末日動漫資源庫</div>
+        </a>
+        <hr style="margin: 0; border-color: #333333;">
+        <a style="color: white; text-decoration: none;" href="https://www.gal123.com/" target="_blank">
+          <div class="simple-dropdown-item genre-option">紳士導航♂</div>
+        </a>
+        <hr style="margin: 0; border-color: #333333;">
+        <a style="color: white; text-decoration: none;" href="https://www.sshs.pw/" target="_blank">
+          <div class="simple-dropdown-item genre-option">紳士會所</div>
         </a>
         <hr style="margin: 0; border-color: #333333;">
       </div>
