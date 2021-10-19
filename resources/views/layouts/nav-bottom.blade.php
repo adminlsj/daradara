e159378e472bbbca5803f33b4bedc495d700d59e
laughseejapan <laughseejapan@gmail.com>
2021-10-19 19:24:07 +0800

    hanime1

--- resources/views/layouts/nav-bottom.blade.php
+++ resources/views/layouts/nav-bottom.blade.php
@@ -15,9 +15,9 @@
   </a>
   <a href="{{ Auth::check() ? route('home.list') : route('login') }}">
     @if (Request::is('*list*'))
-      <i style="font-size: 27px; margin-top: 9px; color: white" class="material-icons">subscriptions</i>
+      <i style="font-size: 27px; margin-top: 10px; color: white" class="material-icons">subscriptions</i>
     @else
-      <i style="font-size: 27px; margin-top: 9px;" class="material-icons-outlined">subscriptions</i>
+      <i style="font-size: 27px; margin-top: 10px;" class="material-icons-outlined">subscriptions</i>
     @endif
   </a>
   <a href="{{ route('home.search') }}?query=&sort=本日排行">
@@ -29,9 +29,9 @@
   </a>
   <a href="{{ route('home.search') }}">
     @if (Request::is('*newest*'))
-      <i style="padding-left: 2px; font-size: 31px; margin-top: 6px" class="material-icons{{ Request::is('*newest*') ? '' : '-outlined' }}">menu</i>
-    @else
       <i style="padding-left: 2px; font-size: 31px; margin-top: 7px" class="material-icons{{ Request::is('*newest*') ? '' : '-outlined' }}">menu</i>
+    @else
+      <i style="padding-left: 2px; font-size: 31px; margin-top: 8px" class="material-icons{{ Request::is('*newest*') ? '' : '-outlined' }}">menu</i>
     @endif
   </a>
 </div>
