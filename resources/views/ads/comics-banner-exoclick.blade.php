d769cbbe3fa67c10b7380475c63c3312ded0d87a
laughseejapan <laughseejapan@gmail.com>
2021-11-24 14:18:36 +0800

    hanime1

--- resources/views/ads/comics-banner-exoclick.blade.php
+++ resources/views/ads/comics-banner-exoclick.blade.php
@@ -1,8 +1,8 @@
 <div class="comics-banner-ads">
   <span class="hidden-xs">
-    <ins class="adsbyexoclick" data-zoneid="4399380"></ins> 
+    @include('layouts.exoclick', ['id' => '4399380', 'width' => '728', 'height' => '90'])
   </span>
   <span class="hidden-sm hidden-md hidden-lg">
-     <ins class="adsbyexoclick" data-zoneid="4399390"></ins> 
+    @include('layouts.exoclick', ['id' => '4399390', 'width' => '300', 'height' => '100'])
   </span>
 </div>
