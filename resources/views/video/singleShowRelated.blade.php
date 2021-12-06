60f84843cfc114a8ba77edf2d546954978d5b852
laughseejapan <laughseejapan@gmail.com>
2021-12-06 16:53:03 +0800

    hanime1

--- resources/views/video/singleShowRelated.blade.php
+++ resources/views/video/singleShowRelated.blade.php
@@ -2,10 +2,10 @@
   <div style="padding-right: 0px; width: 160px; max-width: 50%; position: relative;" class="col-xs-6 col-sm-6 col-md-6">
     <img style="width: 100%; height: 100%;" src="https://i.imgur.com/JMcgEkPt.jpg">
     @if ($video->id == $current->id)
-	    <img style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; filter: brightness(15%);" src="{{ $video->imgurM() }}" alt="{{ $video->title }}">
+	    <img style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; object-fit: cover; filter: brightness(15%);" src="{{ $video->imgurM() }}" alt="{{ $video->title }}">
 	    <div style="margin: 0; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); color: white; font-weight: bold">現正播放</div>
 	  @else
-		  <img style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;" src="{{ $video->imgurM() }}" alt="{{ $video->title }}">
+		  <img style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; object-fit: cover;" src="{{ $video->imgurM() }}" alt="{{ $video->title }}">
     @endif
   </div>
   <div style="width: calc(100% - 160px); min-width: 50%; max-height: 90px;" class="col-xs-6 col-sm-6 col-md-6 related-watch-title">
