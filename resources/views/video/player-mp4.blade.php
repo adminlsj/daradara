42a80e799f346f54c3c5950214bf6b0202467ba5
laughseejapan <laughseejapan@gmail.com>
2021-12-13 16:21:02 +0800

    hanime1

--- resources/views/video/player-mp4.blade.php
+++ resources/views/video/player-mp4.blade.php
@@ -10,9 +10,9 @@
     @endforeach
   @endif
 
-  @if ($video->id == 25248)
-    <track kind="captions" label="繁體中文" srclang="big5" src="https://cdn.jsdelivr.net/gh/guaishushukanlifan/Project-H@latest/data/{{ $video->id }}_zh_hant.vtt" default />
-    <track kind="captions" label="简体中文" srclang="gb" src="https://cdn.jsdelivr.net/gh/guaishushukanlifan/Project-H@latest/data/{{ $video->id }}_zh_hans.vtt" />
+  @if (array_key_exists('caption', $video->foreign_sd))
+    <track kind="captions" label="繁體中文" srclang="zh_hant" src="https://cdn.jsdelivr.net/gh/guaishushukanlifan/Project-H@latest/data/{{ $video->id }}_zh_hant.vtt" default />
+    <track kind="captions" label="简体中文" srclang="zh_hans" src="https://cdn.jsdelivr.net/gh/guaishushukanlifan/Project-H@latest/data/{{ $video->id }}_zh_hans.vtt" />
   @endif
 </video>
 <script>
@@ -58,13 +58,44 @@
     ],
     captions: {
       active: true, 
-      language: 'big5', 
+      language: 'zh_hant', 
       update: false
     }
   });
   window.player = player;
 
+  @if (array_key_exists('caption', $video->foreign_sd))
+    player.on('enterfullscreen', event => {
+      $('.plyr__captions').addClass('plyr__fullscreen_captions');
+      screen.orientation.lock('landscape');
+    });
 
+    player.on('exitfullscreen', event => {
+      $('.plyr__captions').removeClass('plyr__fullscreen_captions');
+      screen.orientation.lock('portrait');
+    });
+
+    var video = document.getElementById('player');
+    var trackList = document.querySelector('video').textTracks;
+    video.addEventListener("webkitbeginfullscreen", function(){
+      document.documentElement.style.setProperty('--webkit-text-track-display', 'block');
+      if (player.currentTrack == 0) {
+        trackList[0].mode = 'showing';
+      } else if (player.currentTrack == 1) {
+        trackList[1].mode = 'showing';
+      }
+    }, false);
+    video.addEventListener("webkitendfullscreen", function(){
+      document.documentElement.style.setProperty('--webkit-text-track-display', 'none');
+      if (trackList[0].mode == 'showing') {
+        player.currentTrack = 0;
+      } else if (trackList[1].mode == 'showing') {
+        player.currentTrack = 1;
+      } else {
+        player.currentTrack = -1;
+      }
+    }, false);
+  @endif
 
   @if ($video->duration == null)
     player.on('loadedmetadata', function () {
