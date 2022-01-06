76a3f6a1d1282dc1439ecdaa3494fa403d66ed09
laughseejapan <laughseejapan@gmail.com>
2022-01-06 09:07:45 +0800

    hanime1

---
+++ resources/views/layouts/juicyads.blade.php
@@ -0,0 +1,5 @@
+<!-- JuicyAds v3.1 -->
+<script type="text/javascript" data-cfasync="false" async src="https://poweredby.jads.co/js/jads.js"></script>
+<ins id="{{ $id }}" data-width="{{ $width }}" data-height="{{ $height }}"></ins>
+<script type="text/javascript" data-cfasync="false" async>(adsbyjuicy = window.adsbyjuicy || []).push({'adzone':{{ $id }}});</script>
+<!--JuicyAds END-->
--- resources/views/video/download.blade.php
+++ resources/views/video/download.blade.php
@@ -7,7 +7,6 @@
 
 @section('nav')
   @include('nav.main')
-  @include('layouts.devtools')
 @endsection
 
 @section('content')
