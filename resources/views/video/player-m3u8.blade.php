720f16c13b5eea48244f5c89c56e592932cef0fe
laughseejapan <laughseejapan@gmail.com>
2022-01-22 18:58:45 +0800

    hanime1

--- resources/views/video/player-m3u8.blade.php
+++ resources/views/video/player-m3u8.blade.php
@@ -57,12 +57,12 @@
   window.player = player;
 
   player.on('play', (event) => {
-    $('.player-lang-wrapper').removeClass('lang-visible');
-    $('.player-lang-wrapper').addClass('lang-hidden');
+    $('#player-lang-wrapper').removeClass('lang-visible');
+    $('#player-lang-wrapper').addClass('lang-hidden');
   });
 
   player.on('pause', (event) => {
-    $('.player-lang-wrapper').removeClass('lang-hidden');
-    $('.player-lang-wrapper').addClass('lang-visible');
+    $('#player-lang-wrapper').removeClass('lang-hidden');
+    $('#player-lang-wrapper').addClass('lang-visible');
   });
 </script>
--- resources/views/video/player-mp4.blade.php
+++ resources/views/video/player-mp4.blade.php
@@ -65,13 +65,13 @@
   window.player = player;
 
   player.on('play', (event) => {
-    $('.player-lang-wrapper').removeClass('lang-visible');
-    $('.player-lang-wrapper').addClass('lang-hidden');
+    $('#player-lang-wrapper').removeClass('lang-visible');
+    $('#player-lang-wrapper').addClass('lang-hidden');
   });
 
   player.on('pause', (event) => {
-    $('.player-lang-wrapper').removeClass('lang-hidden');
-    $('.player-lang-wrapper').addClass('lang-visible');
+    $('#player-lang-wrapper').removeClass('lang-hidden');
+    $('#player-lang-wrapper').addClass('lang-visible');
   });
 
   @if ($video->has_subtitles)
