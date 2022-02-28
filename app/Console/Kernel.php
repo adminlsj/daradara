e55d0f7dc2525eca76b5b1388b88a95545c9e5a9
laughseejapan <laughseejapan@gmail.com>
2022-02-28 12:52:41 +0800

    hanime1

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -55,6 +55,7 @@ class Kernel extends ConsoleKernel
 
         $schedule->command('hanime1:check-spankbang')->hourly();
         $schedule->command('hanime1:check-motherless')->cron('0 */3 * * *');
+        $schedule->command('hanime1:update-youjizz')->dailyAt('12:55');
 
         $schedule->command('hanime1:upload-nhentai')->hourly()->between('6:00', '21:00');
         $schedule->command('hanime1:upload-rule34')->hourly()->between('6:00', '21:00');
