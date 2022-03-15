4f3a575dcb0dca13621fa125901ad9e1ac519214
laughseejapan <laughseejapan@gmail.com>
2022-03-16 01:21:09 +0800

    hanime1

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,6 +26,7 @@ class Kernel extends ConsoleKernel
         'App\Console\Commands\UpdateSpankbang',
         'App\Console\Commands\UpdateSpankbangErrors',
         'App\Console\Commands\UpdateYoujizz',
+        'App\Console\Commands\UpdateYoujizzDownloads',
 
         'App\Console\Commands\UploadNhentai',
         'App\Console\Commands\UploadRule34',
@@ -48,7 +49,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('hanime1:update-xvideoserrors')->cron('*/10 * * * *');
         $schedule->command('hanime1:update-spankbangerrors')->hourly();
         $schedule->command('hanime1:update-youjizz')->cron('0 */6 * * *');
-        $schedule->command('hanime1:update-youjizz')->dailyAt('00:30');
+        $schedule->command('hanime1:update-youjizzdownloads')->cron('0 */6 * * *');
 
         $schedule->command('hanime1:check-spankbang')->hourly();
         $schedule->command('hanime1:check-motherless')->cron('0 */2 * * *');
