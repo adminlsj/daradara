59692c320dcc1795962b05655e97814694d973a6
laughseejapan <laughseejapan@gmail.com>
2022-03-28 06:32:04 +0800

    hanime1

--- resources/views/nav/home-mobile.blade.php
+++ resources/views/nav/home-mobile.blade.php
@@ -1,4 +1,4 @@
-<div id="main-nav-home" style="z-index: 10000; padding:0; padding-top: 5px; height: 54px; line-height: 47px; position: absolute;" class="hidden-sm hidden-md hidden-lg">
+<div id="main-nav-home" style="z-index: 10000; padding:0; padding-top: 5px; height: 53px; line-height: 47px; position: absolute;" class="hidden-sm hidden-md hidden-lg">
 
   <div style="padding: 0 15px; margin-bottom: -10px;">
     <a href="/" style="padding-right: 2.5%; color: white; font-size: 1.4em;">
@@ -18,7 +18,7 @@
     </a>
   </div>
 
-  <div class="hide-scrollbar nav-mobile-genres" style="overflow-x: scroll; width: calc(100%); display: inline-block; white-space: nowrap; margin-bottom: -10px; line-height: 31px; position: absolute; top: 54px; border-radius: 0px; padding-top: 5px; padding-bottom: 5px;">
+  <div class="hide-scrollbar nav-mobile-genres" style="overflow-x: scroll; width: calc(100%); display: inline-block; white-space: nowrap; margin-bottom: -10px; line-height: 31px; position: absolute; top: 53px; border-radius: 0px; padding-top: 5px; padding-bottom: 5px;">
     <a style="color: white; font-weight: normal; font-family: 'Trebuchet MS', sans-serif; margin-right: 18px; padding-left: 15px" href="{{ route('home.search') }}?genre=H動漫&tags%5B%5D=新番預告&sort=">預告</a>
     <a style="color: white; font-weight: normal; font-family: 'Trebuchet MS', sans-serif; margin-right: 18px; font-weight: bold;border-bottom: 2px solid crimson; color: crimson; padding-bottom: 10px; padding-left: 2px; padding-right: 2px;" href="/">主頁</a>
     <a style="color: white; font-weight: normal; font-family: 'Trebuchet MS', sans-serif; margin-right: 20px" href="{{ route('home.search') }}?genre=H動漫&duration=&sort=&query=&year=&month=">裏番</a>
@@ -32,10 +32,10 @@
   <script>
       var targetOffset = $(".nav-mobile-genres").offset().top;
       var $window = $(window).scroll(function(){
-          if ( $window.scrollTop() > 54 ) {   
+          if ( $window.scrollTop() > 53 ) {   
             $(".nav-mobile-genres").css({"position":"fixed", 'top':'0px', 'background-color': 'black'});
           } else {
-            $(".nav-mobile-genres").css({"position":"absolute", 'top':'54px', 'background-color': 'transparent'});
+            $(".nav-mobile-genres").css({"position":"absolute", 'top':'53px', 'background-color': 'transparent'});
           }
       });
     </script>
