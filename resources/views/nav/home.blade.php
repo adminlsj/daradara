139da142d5cf08bbb78660598ed4d49af1d20869
laughseejapan <laughseejapan@gmail.com>
2022-03-27 20:30:21 +0800

    hanime1

--- resources/views/nav/home.blade.php
+++ resources/views/nav/home.blade.php
@@ -2,9 +2,9 @@
   <a href="/" style="padding-right: 2.5%; color: white; font-size: 1.4em;">
     <span style="color: crimson">H</span>anime1<span style="color: crimson">.</span>me
   </a>
-  <a style="padding-right: 0px" class="nav-icon pull-right" href="{{ route('home.list') }}"><span style="vertical-align: middle;" class="material-icons">account_circle</span></a>
-  <a class="nav-icon pull-right" href="{{ route('home.search') }}"><span style="vertical-align: middle;" class="material-icons">search</span></a>
-  <a class="nav-icon pull-right" href="{{ Auth::check() ? route('user.userEditUpload', Auth::user()) : route('login') }}"><span style="vertical-align: middle;" class="material-icons">video_call</span></a>
+  <a style="padding-right: 0px" class="nav-icon pull-right" href="{{ route('home.list') }}"><span style="vertical-align: middle;" class="material-icons-outlined">account_circle</span></a>
+  <a class="nav-icon pull-right" href="{{ route('home.search') }}"><span style="vertical-align: middle;" class="material-icons-outlined">search</span></a>
+  <a class="nav-icon pull-right" href="{{ Auth::check() ? route('user.userEditUpload', Auth::user()) : route('login') }}"><span style="vertical-align: middle;" class="material-icons-outlined">video_call</span></a>
 
   @if (Request::is('/'))
     <div class="hidden-sm hidden-md hidden-lg" style="position: absolute; top: 40px; z-index: 10001; color: white; margin-left: auto; margin-right: auto; left: 0; right: 0; text-align: center;">
