4e479454ad01d6c266fa9e14ea5bc7a5376f7f07
laughseejapan <laughseejapan@gmail.com>
2022-03-28 22:12:34 +0800

    hanime1

--- resources/views/nav/home-mobile.blade.php
+++ resources/views/nav/home-mobile.blade.php
@@ -1,7 +1,7 @@
 <div id="main-nav-home" style="z-index: 10000; padding:0; padding-top: 3px; height: 47px; line-height: 40px; position: absolute; background-image: none; border-bottom: 1px solid #383838; margin-bottom: 0px; background-color: #212121;" class="hidden-sm hidden-md hidden-lg">
 
-  <div style="padding: 0 15px; margin-bottom: -10px;">
-    <a href="/" style="padding-right: 2.5%; color: white; font-size: 1.4em;">
+  <div style="padding: 0 10px; margin-bottom: -10px;">
+    <a href="/" style="padding-right: 2.5%; color: white; font-size: 1.4em; font-family: Cabin Condensed;">
       <span style="color: crimson">H</span>anime1<span style="color: crimson">.</span>me
     </a>
 
