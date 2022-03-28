2537d4ec5a8d2d061617de1dc9173f0f859235b7
laughseejapan <laughseejapan@gmail.com>
2022-03-29 01:25:25 +0800

    hanime1

--- resources/views/layouts/search-new.blade.php
+++ resources/views/layouts/search-new.blade.php
@@ -263,7 +263,7 @@
 		@endif
 
 		<div class="search-pagination hidden-xs">{!! $videos->appends(request()->query())->links() !!}</div>
-		<div style="margin-top: {{ $doujin ? '-26px' : '-34px' }};" class="search-pagination mobile-search-pagination hidden-sm hidden-md hidden-lg">{!! $videos->appends(request()->query())->onEachSide(1)->links() !!}</div>
+		<div style="margin-top: {{ $doujin ? '-26px' : '-29px' }};" class="search-pagination mobile-search-pagination hidden-sm hidden-md hidden-lg">{!! $videos->appends(request()->query())->onEachSide(1)->links() !!}</div>
 
 		@include('ads.search-banner-panel')
 
