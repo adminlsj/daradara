c8b0759489c7033774f620d8c11df6a18c03664e
laughseejapan <laughseejapan@gmail.com>
2022-03-29 16:49:10 +0800

    hanime1

--- resources/views/nav/main-new.blade.php
+++ resources/views/nav/main-new.blade.php
@@ -1,27 +1,16 @@
-<div id="main-nav" style="z-index: 10000 !important; background-color: gray" class="main-nav{{ Request::is('*watch*') || Request::is('*download*') ? '-video-show' : '' }}">
-  <a href="/" style="padding-right: 2.5%; color: white; font-size: 2em; font-stretch: condensed;">
-    <span style="color: crimson">H</span>ANIME1<span style="color: crimson">.</span>ME
+<div id="main-nav" style="z-index: 10000 !important; {{ Request::is('*search*') ? 'position: static !important' : '' }}" class="main-nav{{ Request::is('*watch*') || Request::is('*download*') ? '-video-show' : '' }}">
+  <a href="/" style="padding-right: 2.5%; color: white; font-size: 1.4em;">
+    <span style="color: crimson">H</span>anime1<span style="color: crimson">.</span>me
   </a>
-  <form id="main-search-form">
-      <div style="position: absolute; top: 0px; left: 245px;">
-        <input id="nav-query" name="query" style="box-shadow: none; border: 1px solid rgba(58,60,63,.85); background-color: #202020; font-size: 1.1em;border-radius: 2px; height: 40px; padding-left: 5px; color: darkgray; padding-bottom: 2px; font-weight: 500; transition: .3s cubic-bezier(0,0,.2,1);" type="text" value="{{ request('query') }}" placeholder="搜索">
-      </div>
-      <div style="position: absolute; top: 0px; left: 400px;">
-        <button style="height: 40px;" type="submit">
-          <i style="vertical-align: middle; margin-top: -35px" class="material-icons">search</i>
-        </button>
-      </div>
-  </form>
-  <form id="hentai-form" action="{{ route('home.search') }}" method="GET">
-    <input type="hidden" id="query" name="query" value="{{ Request::get('query') }}">
-  </form>
-  <!-- <a class="nav-item hidden-xs" href="/">主頁</a>
-  <a class="nav-item hidden-xs" href="{{ route('home.search') }}">H動漫</a>
-  <a href="https://www.laughseejapan.com/search?query=%E5%8B%95%E6%BC%AB" target="_blank" class="nav-item hidden-xs" href="javascript:void(0)">動畫卡通</a>
-  <a class="nav-item hidden-xs" href="{{ route('home.search') }}">最新內容</a>
-  <a class="nav-item hidden-xs" href="{{ Auth::check() ? route('home.list') : route('login') }}">我的清單</a>
+  <a class="nav-item hidden-xs" href="{{ route('home.search') }}?genre=裏番&duration=&sort=&query=&year=&month=">裏番</a>
+  <a class="nav-item hidden-xs hidden-sm" href="{{ route('home.search') }}?genre=裏番&tags%5B%5D=新番預告&sort=">預告</a>
+  <a class="nav-item hidden-xs hidden-sm" href="{{ route('home.search') }}?genre=泡麵番&duration=&sort=&query=&year=&month=">泡麵番</a>
+  <a class="nav-item hidden-xs" href="{{ route('home.search') }}?genre=3D動畫&duration=&sort=&query=&year=&month=">3D動畫</a>
+  <a class="nav-item hidden-xs" href="{{ route('home.search') }}?genre=同人作品&duration=&sort=&query=&year=&month=">同人作品</a>
+  <a class="nav-item hidden-xs" href="{{ route('home.search') }}?genre=Cosplay&duration=&sort=&query=&year=&month=">Cosplay</a>
+  <a class="nav-item hidden-xs" href="{{ route('comic.index') }}">H漫畫</a>
 
-  <a style="padding-right: 0px" class="nav-icon pull-right" href="{{ Auth::check() ? route('home.list') : route('login') }}"><span style="vertical-align: middle;" class="material-icons">account_circle</span></a>
-  <a class="nav-icon pull-right" href="{{ route('home.search') }}"><span style="vertical-align: middle;" class="material-icons">search</span></a>
-  <a class="nav-icon pull-right" href="{{ Auth::check() ? route('user.userEditUpload', Auth::user()) : route('login') }}"><span style="vertical-align: middle;" class="material-icons">video_call</span></a> -->
+  <a style="padding-right: 0px" class="nav-icon pull-right" href="{{ route('home.list') }}"><span style="vertical-align: middle;" class="material-icons-outlined">account_circle</span></a>
+  <a class="nav-icon pull-right" href="{{ route('home.search') }}"><span style="vertical-align: middle;" class="material-icons-outlined">search</span></a>
+  <a class="nav-icon pull-right" href="{{ Auth::check() ? route('user.userEditUpload', Auth::user()) : route('login') }}"><span style="vertical-align: middle;" class="material-icons-outlined">video_call</span></a>
 </div>
--- resources/views/nav/main.blade.php
+++ resources/views/nav/main.blade.php
@@ -1,3 +1,24 @@
-<div id="main-nav" style="z-index: 10000 !important; {{ Request::is('*search*') ? 'position: static !important' : '' }}" class="main-nav{{ Request::is('*watch*') || Request::is('*download*') ? '-video-show' : '' }}">
+<div id="main-nav" style="z-index: 10000 !important; {{ Request::is('*search*') ? 'position: static !important' : '' }}" class="main-nav{{ Request::is('*watch*') || Request::is('*download*') ? '-video-show' : '' }} hidden-xs">
   @include('nav.main-content')
+</div>
+
+<div id="main-nav-home" style="z-index: 10000; padding:0; padding-top: 3px; height: 48px; line-height: 40px; position: absolute; background-image: none; border-bottom: 1px solid #383838; margin-bottom: 0px; background-color: #212121;" class="hidden-sm hidden-md hidden-lg">
+
+  <div style="padding: 0 10px; margin-bottom: -10px;">
+    <a href="/" style="color: white; font-size: 1.4em; font-family: 'Encode Sans Condensed', sans-serif;">
+      <span style="color: crimson">H</span>anime1<span style="color: crimson">.</span>me
+    </a>
+
+    <a style="padding-right: 0px" class="nav-icon pull-right" href="{{ route('home.list') }}">
+      <span style="vertical-align: middle; margin-top: -2px;" class="material-icons">account_circle</span>
+    </a>
+
+    <a class="nav-icon pull-right" href="{{ route('home.search') }}">
+      <img style="margin-top: -3px; margin-right: 1px;" height="20" src="https://i.imgur.com/fblmkmT.png">
+    </a>
+
+    <a class="nav-icon pull-right" href="{{ Auth::check() ? route('user.userEditUpload', Auth::user()) : route('login') }}">
+      <img style="margin-top: -3px; margin-right: 5px;" height="20" src="https://i.imgur.com/ic0oQVj.png">
+    </a>
+  </div>
 </div>
