57e81fb8b624a3cbd35b2c987368c6c8f45240b0
laughseejapan <laughseejapan@gmail.com>
2022-03-30 00:34:22 +0800

    hanime1

--- resources/views/nav/home-mobile.blade.php
+++ resources/views/nav/home-mobile.blade.php
@@ -21,7 +21,7 @@
   <div class="hide-scrollbar nav-mobile-genres" style="overflow-x: scroll; width: calc(100%); display: inline-block; white-space: nowrap; margin-bottom: -10px; line-height: 31px; position: absolute; top: 48px; border-radius: 0px; padding-top: 9px; padding-bottom: 8px; border-bottom: 1px solid #383838; background-color: #141414;">
 
     <div style="border-right: 1px solid #383838; display: inline-block; margin-right: 7px;">
-      <a style="color: white; margin-right: 11px; margin-left: 10px; background-color: #373737; padding: 7px 13px 7px 8px; border-radius: 2px; font-weight: bold" href="{{ route('home.search') }}?query=&genre=全部&sort=正在觀看">
+      <a style="color: white; margin-right: 11px; margin-left: 10px; background-color: #2b2b2b; padding: 7px 13px 7px 8px; border-radius: 2px; font-weight: bold" href="{{ route('home.search') }}?query=&genre=全部&sort=正在觀看">
         <img style="margin-top: -3px; margin-right: 6px;" height="20" src="https://i.imgur.com/H7gRtRi.png">探索
       </a>
     </div>
