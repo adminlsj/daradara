31e6cd7fe7ea77b44165065f74ddf96489f289b3
laughseejapan <laughseejapan@gmail.com>
2022-03-29 22:15:59 +0800

    hanime1

--- resources/views/layouts/home-new.blade.php
+++ resources/views/layouts/home-new.blade.php
@@ -157,7 +157,7 @@
 
 		<div class="home-rows-margin-top">
 			<div class="content-padding-new">
-				<a class="home-rows-header" style="text-decoration: none;" href="/search?query=&genre=全部&sort=最新上傳">
+				<a class="home-rows-header" style="text-decoration: none;" href="/search?query=&genre=裏番&sort=正在觀看">
 					<h5 style="color: #8e9194;">當下</h3>
 					<h3 style="font-weight: 700; color: #edeeef; margin-bottom: 20px;">熱門影片</h3>
 					@include('layouts.home-row-arrow')
@@ -202,7 +202,7 @@
 		</script>
 
 		<div class="hidden-xs content-padding-new home-rows-margin-top">
-			<a class="home-rows-header" style="text-decoration: none;" href="/search?query=&genre=全部&sort=最新上傳">
+			<a class="home-rows-header" style="text-decoration: none;" href="/search?query=&genre=全部&sort=正在觀看">
 				<h5 style="color: #8e9194;">動態</h3>
 				<h3 style="font-weight: 700; color: #edeeef; margin-bottom: 20px;">正在觀看</h3>
 				@include('layouts.home-row-arrow')
