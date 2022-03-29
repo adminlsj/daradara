18c6248effe94addd28cdf898a33f789ae17e4c8
laughseejapan <laughseejapan@gmail.com>
2022-03-29 17:06:40 +0800

    hanime1

--- resources/views/video/watch-new.blade.php
+++ resources/views/video/watch-new.blade.php
@@ -166,7 +166,7 @@
                   <a style="text-decoration: none;" href="{{ route('video.watch') }}?v={{ $video->id }}">
                     <div class="home-rows-videos-div" style="position: relative; display: inline-block; margin-bottom:15px">
                       <img style="width: 100%" src="{{ $video->cover }}">
-                      <div class="home-rows-videos-title" style="position:absolute; bottom:0; left:0; white-space: initial; overflow: hidden;text-overflow: ellipsis; display: -webkit-box; -webkit-line-clamp: 2; -webkit-box-orient: vertical; color: white; width: 100%; padding: 3px 3px; background: linear-gradient(to bottom, transparent 0%, black 120%);">{{ $video->title }}</div>
+                      <div class="home-rows-videos-title" style="position:absolute; bottom:0; left:0; white-space: initial; overflow: hidden;text-overflow: ellipsis; display: -webkit-box; -webkit-line-clamp: 2; -webkit-box-orient: vertical; color: white; width: 100%; padding: 2px 3px; background: linear-gradient(to bottom, transparent 0%, black 120%);">{{ $video->title }}</div>
                       </div>
                   </a>
                 </div>
