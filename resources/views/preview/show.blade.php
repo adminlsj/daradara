2166e9be185f90e503f2ab9a182c42f29d964570
laughseejapan <laughseejapan@gmail.com>
2022-04-11 02:27:29 +0800

    hanime1

--- resources/views/preview/show.blade.php
+++ resources/views/preview/show.blade.php
@@ -2,6 +2,16 @@
 
 @section('head')
     @parent
+    <meta property="og:url" content="/previews/{{ $preview->uuid }}" />
+    <meta property="og:type" content="article" />
+    <meta property="og:title" content="{{ $year }}年{{ $month }}月裏番/里番新番列表 - Hanime1.me" />
+    <meta property="og:description" content="{{ $year }}年{{ $month }}月H動漫成人卡通新番表，開播日期、內容介紹、製作公司一口氣公開！本列表目前共收錄了{{ $videos->count() }}套新作工口H動畫。" />
+    <meta property="og:image" content="{{ $preview->cover }}" />
+
+    <title>{{ $year }}年{{ $month }}月裏番/里番新番列表&nbsp;-&nbsp;Hanime1.me</title>
+    <meta name="title" content="{{ $year }}年{{ $month }}月裏番/里番新番列表 - Hanime1.me">
+    <meta name="description" content="{{ $year }}年{{ $month }}月H動漫成人卡通新番表，開播日期、內容介紹、製作公司一口氣公開！本列表目前共收錄了{{ $videos->count() }}套新作工口H動畫。" />
+    <meta property="og:image" content="{{ $preview->cover }}">
 @endsection
 
 @section('nav')
