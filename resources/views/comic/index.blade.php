bf34cb43f8b97543f6698ff96c6c23e7ace19525
laughseejapan <laughseejapan@gmail.com>
2022-04-19 19:01:19 +0800

    hanime1

--- resources/views/comic/index.blade.php
+++ resources/views/comic/index.blade.php
@@ -10,7 +10,7 @@
   @include('ads.comics-banner-exoclick')
 
   @if (!isset($_GET['page']) || $_GET['page'] == 1)
-    <div class="">
+    <div class="comic-rows-wrapper">
         <h3 style="text-align: center; color: #d9d9d9;"><span style="vertical-align: middle; margin-top: -5px; margin-right: 5px; color: crimson;" class="material-icons">local_fire_department</span>發燒漫畫</h3>
         <div class="row no-gutter">
           @foreach ($trending as $comic)
