4c5329cf2ddb3f9df5d06eb7b77a8d5c9a8a5316
laughseejapan <laughseejapan@gmail.com>
2022-04-19 16:13:17 +0800

    hanime1

--- resources/views/video/search-nav.blade.php
+++ resources/views/video/search-nav.blade.php
@@ -3,7 +3,7 @@
 	<div id="genre-modal-trigger" class="search-nav no-select {{ Request::get('genre') ? 'active' : '' }}" data-toggle="modal" data-target="#genre-modal">
 		<span class="hidden-xs">{{ Request::get('genre') ? Request::get('genre') : '類型'}}</span>
 		<div class="hidden-sm hidden-md hidden-lg" style="border-right: 1px solid #2b2b2b; display: inline-block; margin-right: 7px; line-height: 31px;">
-	      <a style="color: white; margin-right: 11px; margin-left: 10px; background-color: #2b2b2b; padding: 7px 13px 7px 10px; border-radius: 2px; font-weight: bold; text-decoration: none;">
+	      <a style="color: white; margin-right: 11px; margin-left: 10px; background-color: #2b2b2b; padding: 8px 13px 8px 10px; border-radius: 2px; font-weight: bold; text-decoration: none;">
 	        <img style="margin-top: -3px; margin-right: 7px;" height="17" src="https://cdn.jsdelivr.net/gh/tatakanuta/tatakanuta@v1.0.0/asset/icon/categories.png">{{ Request::get('genre') ? Request::get('genre') : '類型'}}
 	      </a>
 	    </div>
@@ -11,27 +11,27 @@
 
 	<div class="search-nav no-select {{ Request::get('tags') ? 'active' : '' }}" data-toggle="modal" data-target="#tags">
 		<span class="hidden-xs">標籤</span>
-		<div class="nav-home-mobile-button hidden-sm hidden-md hidden-lg">標籤</div>
+		<div style="padding: 6px 10px;" class="nav-home-mobile-button hidden-sm hidden-md hidden-lg">標籤</div>
 	</div>
 
 	<div id="sort-modal-trigger" class="search-nav no-select {{ Request::get('sort') ? 'active' : '' }}" data-toggle="modal" data-target="#sort-modal">
 		<span class="hidden-xs">{{ Request::get('sort') ? Request::get('sort') : '排序方式'}}</span>
-		<div class="nav-home-mobile-button hidden-sm hidden-md hidden-lg">{{ Request::get('sort') ? Request::get('sort') : '排序方式'}}</div>
+		<div style="padding: 6px 10px;" class="nav-home-mobile-button hidden-sm hidden-md hidden-lg">{{ Request::get('sort') ? Request::get('sort') : '排序方式'}}</div>
 	</div>
 
 	<div class="search-nav no-select {{ Request::get('brands') ? 'active' : '' }}" data-toggle="modal" data-target="#brands">
 		<span class="hidden-xs">品牌</span>
-		<div class="nav-home-mobile-button hidden-sm hidden-md hidden-lg">品牌</div>
+		<div style="padding: 6px 10px;" class="nav-home-mobile-button hidden-sm hidden-md hidden-lg">品牌</div>
 	</div>
 
 	<div id="date-modal-trigger" class="search-nav no-select {{ Request::get('year') ? 'active' : '' }}" data-toggle="modal" data-target="#date-modal">
 		<span class="hidden-xs">{{ Request::get('year') ? Request::get('year').'年' : '發佈日期'}}</span>
-		<div class="nav-home-mobile-button hidden-sm hidden-md hidden-lg">{{ Request::get('year') ? Request::get('year').'年' : '發佈日期'}}</div>
+		<div style="padding: 6px 10px;" class="nav-home-mobile-button hidden-sm hidden-md hidden-lg">{{ Request::get('year') ? Request::get('year').'年' : '發佈日期'}}</div>
 	</div>
 
 	<div id="duration-modal-trigger" class="hidden-sm search-nav no-select {{ Request::get('duration') ? 'active' : '' }}" data-toggle="modal" data-target="#duration-modal">
 		<span class="hidden-xs">{{ Request::get('duration') ? Request::get('duration') : '片長'}}</span>
-		<div class="nav-home-mobile-button hidden-sm hidden-md hidden-lg" style="margin-right: 10px;">{{ Request::get('duration') ? Request::get('duration') : '片長'}}</div>
+		<div style="padding: 6px 10px;" class="nav-home-mobile-button hidden-sm hidden-md hidden-lg" style="margin-right: 10px;">{{ Request::get('duration') ? Request::get('duration') : '片長'}}</div>
 	</div>
 
 	<div class="no-select hidden-xs" style="display: inline-block; position: relative;">
