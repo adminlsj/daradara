a6adfd0822f726d0af349bf037e26e3b7543c514
laughseejapan <laughseejapan@gmail.com>
2022-04-19 19:49:57 +0800

    hanime1

--- resources/views/comic/card.blade.php
+++ resources/views/comic/card.blade.php
@@ -1,4 +1,4 @@
-<div class="comic-rows-videos-div" onclick="" style="cursor: pointer; position: relative; display: inline-block; margin-bottom: 0px; vertical-align: top;">
+<div class="comic-rows-videos-div" style="cursor: pointer; position: relative; display: inline-block; margin-bottom: 0px; vertical-align: top;" onclick="">
   <a style="text-decoration: none;" href="{{ route('comic.showCover', ['comic' => $comic->id]) }}">
     <img class="lazy" style="border-top-left-radius: 5px; border-top-right-radius: 5px;" src="https://i.imgur.com/0n3iJ9Ol.jpg" data-srcset="https://t.nhentai.net/galleries/{{ $comic->galleries_id }}/cover.{{ $comic->extension }}">
     <div style="position: relative; height: 51px;">
