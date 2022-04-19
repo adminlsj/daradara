491d1c376d064393adba7028bf4b7be54076c2de
laughseejapan <laughseejapan@gmail.com>
2022-04-19 19:03:48 +0800

    hanime1

--- resources/views/comic/card.blade.php
+++ resources/views/comic/card.blade.php
@@ -1,17 +1,17 @@
-<div class="comic-rows-videos-div" style="position: relative; display: inline-block; margin-bottom: 0px; vertical-align: top;">
+<!-- <div class="comic-rows-videos-div" style="position: relative; display: inline-block; margin-bottom: 0px; vertical-align: top;">
   <a style="text-decoration: none;" href="{{ route('comic.showCover', ['comic' => $comic->id]) }}">
   <img class="lazy" style="border-top-left-radius: 5px; border-top-right-radius: 5px;" src="https://i.imgur.com/0n3iJ9Ol.jpg" data-srcset="https://t.nhentai.net/galleries/{{ $comic->galleries_id }}/cover.{{ $comic->extension }}">
   <div style="position: relative; height: 51px;">
     <div class="comic-rows-videos-title">{{ $comic->title_n_before }} {{ $comic->title_n_pretty }} {{ $comic->title_n_after }}</div>
   </div>
   </a>
-</div>
+</div> -->
 
-<!-- <div class="col-xs-4 col-sm-4 col-md-2" style="padding: 0 2px;">
+<div class="col-xs-4 col-sm-4 col-md-2" style="padding: 0 2px;">
   <a style="text-decoration: none;" href="{{ route('comic.showCover', ['comic' => $comic->id]) }}">
   <img style="width: 100%;" class="lazy" style="border-top-left-radius: 5px; border-top-right-radius: 5px;" src="https://i.imgur.com/0n3iJ9Ol.jpg" data-srcset="https://t.nhentai.net/galleries/{{ $comic->galleries_id }}/cover.{{ $comic->extension }}">
   <div style="position: relative; height: 51px;">
     <div class="comic-rows-videos-title">{{ $comic->title_n_before }} {{ $comic->title_n_pretty }} {{ $comic->title_n_after }}</div>
   </div>
   </a>
-</div> -->
+</div>
