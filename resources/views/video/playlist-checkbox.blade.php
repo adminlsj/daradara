733839936477cf72f7545f171cbd4c7d835ecf28
laughseejapan <laughseejapan@gmail.com>
2022-04-28 23:45:57 +0800

    hanime1

--- resources/views/video/playlist-checkbox.blade.php
+++ resources/views/video/playlist-checkbox.blade.php
@@ -3,6 +3,6 @@
     <span style="font-size: 16px; font-weight: normal; overflow: hidden;text-overflow: ellipsis; display: -webkit-box; -webkit-line-clamp: 1; -webkit-box-orient: vertical;">{{ $title }}</span>
     <input id="{{ $id }}" class="playlist-checkbox" type="checkbox" {{ $checked ? 'checked' : '' }}>
     <span class="playlist-checkmark"></span>
-    <img style="position: absolute; height: 20px; top: 14px; {{ $private ? 'right: 20px;' : 'right: 18px;' }}" src="{{ $private ? 'https://i.imgur.com/JI7zhee.png' : 'https://i.imgur.com/I8xTV0b.png' }}">
+    <img style="position: absolute; height: 20px; top: 15px; {{ $private ? 'right: 20px;' : 'right: 17px;' }}" src="{{ $private ? 'https://i.imgur.com/JI7zhee.png' : 'https://i.imgur.com/I8xTV0b.png' }}">
   </label>
 </div>
