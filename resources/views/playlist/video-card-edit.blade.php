607d9fdd27ec44bf3400f6a70b386337be1fdf47
laughseejapan <laughseejapan@gmail.com>
2022-04-29 19:36:42 +0800

    hanime1

--- resources/views/playlist/video-card-edit.blade.php
+++ resources/views/playlist/video-card-edit.blade.php
@@ -22,9 +22,9 @@
         </div>
       </form>
     @endif
-  </div>
 
-  <a class="playlist-show-links" style="text-decoration: none;" href="{{ route('video.watch') }}?v={{ $video->id }}" target="_blank">
-    <div class="home-rows-videos-title" style="position:absolute; bottom:0; left:0; white-space: initial; overflow: hidden;text-overflow: ellipsis; display: -webkit-box; -webkit-line-clamp: 2; -webkit-box-orient: vertical; color: white; width: 100%; padding: 2px 5px; background: linear-gradient(to bottom, transparent 0%, black 120%);">{{ $video->title }}</div>
-  </a>
+    <a class="playlist-show-links" style="text-decoration: none;" href="{{ route('video.watch') }}?v={{ $video->id }}" target="_blank">
+      <div class="home-rows-videos-title" style="position:absolute; bottom:0; left:0; white-space: initial; overflow: hidden;text-overflow: ellipsis; display: -webkit-box; -webkit-line-clamp: 2; -webkit-box-orient: vertical; color: white; width: 100%; padding: 2px 5px; background: linear-gradient(to bottom, transparent 0%, black 120%);">{{ $video->title }}</div>
+    </a>
+  </div>
 </div>
