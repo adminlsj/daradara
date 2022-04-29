a998a484ca7fe175f1092b7b9e0854b5ada73a61
laughseejapan <laughseejapan@gmail.com>
2022-04-29 20:25:27 +0800

    hanime1

--- resources/views/playlist/video-card-edit.blade.php
+++ resources/views/playlist/video-card-edit.blade.php
@@ -7,7 +7,7 @@
         @if (strpos($video->cover, 'E6mSQA2') !== false)
           <img style="position: absolute; top: 0; left: 0; height: 100%; object-fit: cover;" src="{{ $video->thumbL() }}">
         @endif
-        <div class="home-rows-videos-title" style="position:absolute; bottom:0; left:0; white-space: initial; overflow: hidden;text-overflow: ellipsis; display: -webkit-box; -webkit-line-clamp: 2; -webkit-box-orient: vertical; color: white; width: 100%; padding: 2px 5px; background: linear-gradient(to bottom, transparent 0%, black 120%);">{{ $video->title }}</div>
+        <div class="home-rows-videos-title" style="position:absolute; bottom:0; left:0; white-space: initial; overflow: hidden;text-overflow: ellipsis; display: -webkit-box; -webkit-line-clamp: 2; -webkit-box-orient: vertical; color: white; width: 100%; padding: 2px 3px; background: linear-gradient(to bottom, transparent 0%, black 120%);">{{ $video->title }}</div>
       </div>
     </a>
 
