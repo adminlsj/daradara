970a17f12c78b01c2e8820b5fd1cb3e2145a9993
laughseejapan <laughseejapan@gmail.com>
2022-04-29 19:50:44 +0800

    hanime1

--- resources/views/playlist/video-card-edit.blade.php
+++ resources/views/playlist/video-card-edit.blade.php
@@ -1,6 +1,9 @@
 <a style="text-decoration: none;" href="{{ route('video.watch') }}?v={{ $video->id }}" class="hover-opacity" target="_blank">
     <div class="home-rows-videos-div" style="position: relative; display: inline-block; margin-bottom:50px;">
       <img src="{{ $video->cover }}">
+      @if (strpos($video->cover, 'E6mSQA2') !== false)
+        <img style="position: absolute; top: 0; left: 0; height: 100%; object-fit: cover" src="{{ $video->thumbL() }}">
+      @endif
       <div class="home-rows-videos-title" style="position:absolute; bottom:0; left:0; white-space: initial; overflow: hidden;text-overflow: ellipsis; display: -webkit-box; -webkit-line-clamp: 2; -webkit-box-orient: vertical; color: white; width: 100%; padding: 2px 5px; background: linear-gradient(to bottom, transparent 0%, black 120%);">{{ $video->title }}</div>
     </div>
 </a>
