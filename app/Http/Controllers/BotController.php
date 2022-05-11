d27cf24f1a5895ef03beb187c5916ce473001e9f
laughseejapan <laughseejapan@gmail.com>
2022-05-11 20:11:36 +0800

    hanime1

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -33,7 +33,7 @@ class BotController extends Controller
         ini_set('max_execution_time', 0);
         ini_set('memory_limit', '-1');
 
-        $artist = 'あんてきぬすっ';
+        $artist = 'メリー・ジェーン';
         $videos = Video::where('tags_array', 'like', '%"'.$artist.'"%')->get();
         foreach ($videos as $video) {
             $video->artist = $artist;
