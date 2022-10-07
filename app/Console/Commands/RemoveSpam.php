baefac0595bbaee5bbdee8232e7c7b63eee5b7ff
laughseejapan <laughseejapan@gmail.com>
2022-10-07 23:55:17 +0800

    hanime1

--- app/Console/Commands/RemoveSpam.php
+++ app/Console/Commands/RemoveSpam.php
@@ -61,7 +61,8 @@ class RemoveSpam extends Command
                                 '2001:67c:2628:647:f24d:a2ff:fe75:21dc',
                                 '77.111.245.13',
                                 '77.111.245.14',
-                                '2001:67c:2628:647:94af:c9ff:feb7:fd6f'
+                                '2001:67c:2628:647:94af:c9ff:feb7:fd6f',
+                                '2001:67c:198c:906:7a45:c4ff:fefb:b61a'
                             ])
                             ->whereDate('created_at', Carbon::today())
                             ->groupBy('user_id')
