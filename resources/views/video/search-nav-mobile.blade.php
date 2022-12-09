90270b4eea0ee018caa0624c6f742d5d8deb3475
laughseejapan <laughseejapan@gmail.com>
2022-12-09 19:16:32 +0800

    hanime1

--- resources/views/video/search-nav-mobile.blade.php
+++ resources/views/video/search-nav-mobile.blade.php
@@ -1,55 +1,27 @@
 <div id="search-nav-wrapper" style="background-color: black; padding: 14px 10px 15px 10px; margin-top: 0px; margin-bottom: 5px; border: none;" class="hidden-sm hidden-md hidden-lg hidden-xl">
 
-	<div style="color: white; display: inline-block; font-size: 17px; margin-right: 5px; font-weight: bold;">
-		全部類型
+	<div style="color: white; display: inline-block; font-size: 17px; margin-right: 5px; font-weight: bold; cursor: pointer;" data-toggle="modal" data-target="#genre-modal">
+		{{ !$genre || $genre == '全部' ? '全部類型' : Request::get('genre')}}
 		<i class="material-icons" style="vertical-align: middle; margin-top: -3px; margin-left: -5px;">arrow_drop_down</i>
 	</div>
 
-	<div style="color: #e5e5e5; display: inline-block; font-size: 14px; margin-right: 5px; font-weight: bold;">
+	<div style="color: #e5e5e5; display: inline-block; font-size: 14px; margin-right: 5px; font-weight: bold; cursor: pointer; {{ $type == 'artist' ? 'display:none' : '' }}" data-toggle="modal" data-target="#tags">
 		標籤
 		<i class="material-icons" style="vertical-align: middle; margin-top: -4px; margin-left: -5px;">arrow_drop_down</i>
 	</div>
 
-	<div style="color: #e5e5e5; display: inline-block; font-size: 14px; margin-right: 5px; font-weight: bold;">
-		排序方式
+	<div style="color: #e5e5e5; display: inline-block; font-size: 14px; margin-right: 5px; font-weight: bold; cursor: pointer;" data-toggle="modal" data-target="#sort-modal">
+		{{ $sort ? $sort : '排序方式'}}
 		<i class="material-icons" style="vertical-align: middle; margin-top: -4px; margin-left: -5px;">arrow_drop_down</i>
 	</div>
 
-	<div style="color: #e5e5e5; display: inline-block; font-size: 14px; margin-right: 5px; font-weight: bold;">
-		發布日期
+	<div style="color: #e5e5e5; display: inline-block; font-size: 14px; margin-right: 5px; font-weight: bold; cursor: pointer; {{ $type == 'artist' ? 'display:none' : '' }}" data-toggle="modal" data-target="#date-modal">
+		發佈日期
 		<i class="material-icons" style="vertical-align: middle; margin-top: -4px; margin-left: -5px;">arrow_drop_down</i>
 	</div>
 </div>
 
-<!-- <div id="search-nav-wrapper" style="background-color: blue; line-height: 50px; padding: 0px" class="hidden-sm hidden-md hidden-lg hidden-xl">
-
-	<div class="dropdown no-select" style="display: inline-block; padding: 0;">
-		<button style="color: white; font-size: 20px; display: inline-block; margin-right: 10px; background-color: transparent; outline: 0; border: 0;" class="btn btn-secondary dropdown-toggle search-nav-desktop" type="button" data-toggle="modal" data-target="#genre-modal">
-			{{ !$genre || $genre == '全部' ? '全部類型' : Request::get('genre')}}
-			<i class="material-icons" style="vertical-align: middle; margin-top: -3px; margin-left: 4px; margin-right: -7px">arrow_drop_down</i>
-		</button>
-	</div>
-
-	<div class="dropdown no-select" style="display: inline-block; padding: 0; {{ $type == 'artist' ? 'display:none' : '' }}">
-		<button style="color: white; font-size: 14px; display: inline-block; margin-right: 10px; background-color: transparent; outline: 0; border: 0;" class="btn btn-secondary dropdown-toggle search-nav-desktop" type="button" data-toggle="modal" data-target="#tags">
-			標籤
-			<i class="material-icons" style="vertical-align: middle; margin-top: -3px; margin-left: 4px; margin-right: -7px">arrow_drop_down</i>
-		</button>
-	</div>
-
-	<div class="dropdown no-select" style="display: inline-block; padding: 0;">
-		<button style="color: white; font-size: 14px; display: inline-block; margin-right: 10px; background-color: transparent; outline: 0; border: 0;" class="btn btn-secondary dropdown-toggle search-nav-desktop" type="button" data-toggle="modal" data-target="#sort-modal">
-			{{ $sort ? $sort : '排序方式'}}
-			<i class="material-icons" style="vertical-align: middle; margin-top: -3px; margin-left: 4px; margin-right: -7px">arrow_drop_down</i>
-		</button>
-	</div>
-
-	<div class="dropdown no-select" style="display: inline-block; padding: 0; {{ $type == 'artist' ? 'display:none' : '' }}">
-		<button style="outline:0; color:white;" class="btn btn-secondary dropdown-toggle search-nav-desktop" type="button" data-toggle="modal" data-target="#date-modal">
-			發佈日期
-			<i class="material-icons" style="vertical-align: middle; margin-top: -3px; margin-left: 16px; margin-right: -7px">arrow_drop_down</i>
-		</button>
-	</div>
+<!--
 
 	<input type="hidden" id="type" name="type" value="{{ $type }}">
 
