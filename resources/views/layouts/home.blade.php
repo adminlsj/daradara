d826805355d0c04814ff7738724b34ffd26f5b45
laughseejapan <laughseejapan@gmail.com>
2022-12-12 21:45:47 +0800

    hanime1

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -72,7 +72,7 @@
 				@foreach ($newestListing as $video)
 					<div class="multiple-link-wrapper search-doujin-videos home-doujin-videos" style="display: inline-block; padding-right: 3px;">
 						<a class="overlay" href="{{ route('video.watch') }}?v={{ $video->id }}"></a>
-						@include('layouts.card-doujin-desktop')
+						@include('video.card-doujin-desktop')
 					</div>
 				@endforeach
 			</div>
