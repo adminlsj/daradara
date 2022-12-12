edaeeeb0af1f8dd939b5f1bf0cd9be17c247544c
laughseejapan <laughseejapan@gmail.com>
2022-12-12 22:58:19 +0800

    hanime1

--- resources/views/layouts/card-doujin-desktop.blade.php
+++ resources/views/layouts/card-doujin-desktop.blade.php
@@ -3,7 +3,7 @@
 		<img style="width: 100%;" src="https://i.imgur.com/D1l0JoC.jpg">
 		<img style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; object-fit: cover; border-radius: 3px" src="{{ $video->thumbL() }}">
 
-		<div style="position: absolute; left: -2px; bottom: -3px;">
+		<div style="position: absolute; right: -2px; bottom: -3px;">
 			@if ($video->duration != null)
 			    <div class="card-mobile-duration" style="color: white; font-size: 10px; text-shadow: black 1px 0 10px;">
 			    	{{ $video->duration >= 3600 ? gmdate('H:i:s', $video->duration) : gmdate('i:s', $video->duration) }}
