0c113dc9ea28823dff299edead5dd4c7985a9625
laughseejapan <laughseejapan@gmail.com>
2022-12-12 23:24:59 +0800

    hanime1

--- resources/views/layouts/card-doujin-desktop.blade.php
+++ resources/views/layouts/card-doujin-desktop.blade.php
@@ -19,9 +19,9 @@
 
 	<div style="padding: 0 3px;">
 		<div style="text-decoration: none; color: black;">
-			<div class="card-mobile-title" style="margin-top: 6px; font-weight: normal; color: #e5e5e5; padding: 0px; font-size: 12px">{{ str_replace("[".$video->user->name."] ", "", $video->title) }}</div>
+			<div class="card-mobile-title" style="margin-top: 7px; font-weight: normal; color: #e5e5e5; padding: 0px; font-size: 12px">{{ str_replace("[".$video->user->name."] ", "", $video->title) }}</div>
 
-			<div class="card-mobile-genre-wrapper" style="margin-left: -2px; padding: 0px; margin-top: -2px;">
+			<div class="card-mobile-genre-wrapper" style="margin-left: -2px; padding: 0px; margin-top: -1px;">
 				<a href="{{ route('home.search') }}?query={{ $video->user->name }}" style="font-size: 12px; color: dimgray; margin-left: 2px; display: inline-block; font-weight: normal !important;" class="card-mobile-user">{{ $video->user->name }}</a>
 			</div>
 		</div>
