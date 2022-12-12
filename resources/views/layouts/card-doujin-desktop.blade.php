3995d833380bc5d4700783ed6c4238e3c287ebfd
laughseejapan <laughseejapan@gmail.com>
2022-12-13 00:24:23 +0800

    hanime1

--- resources/views/layouts/card-doujin-desktop.blade.php
+++ resources/views/layouts/card-doujin-desktop.blade.php
@@ -5,13 +5,12 @@
 
 		<div style="position: absolute; right: -2px; bottom: -3px;">
 			@if ($video->duration != null)
-			    <div class="card-mobile-duration" style="color: white; font-size: 10px; text-shadow: black 1px 0 10px;">
+			    <div class="card-mobile-duration" style="color: white; font-size: 10px; background-color: rgba(0, 0, 0, 0.6); padding: 0px 3px; line-height: 15px; margin-top: 6px; margin-right: 4px">
 			    	{{ $video->duration >= 3600 ? gmdate('H:i:s', $video->duration) : gmdate('i:s', $video->duration) }}
 			    </div>
 		    @endif
-		</div>
-		<div style="position: absolute; left: 3px; top: 2px;">
-			<div class="card-mobile-duration" style="color: white; font-size: 10px; background-color: rgba(0, 0, 0, 0.6); padding: 0px 3px; line-height: 15px">
+
+			<div class="card-mobile-duration" style="color: white; font-size: 10px; text-shadow: black 1px 0 10px;">
 		    	{{ $video->views() }}次
 		    </div>
 		</div>
