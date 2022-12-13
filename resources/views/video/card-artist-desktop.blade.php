dbda64a26b4c9c0513ef775c2b53a1682b8db55c
laughseejapan <laughseejapan@gmail.com>
2022-12-13 20:33:37 +0800

    hanime1

--- resources/views/video/card-artist-desktop.blade.php
+++ resources/views/video/card-artist-desktop.blade.php
@@ -4,9 +4,9 @@
 		<img style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; object-fit: cover; border-radius: 3px" src="{{ $artist->avatar_temp }}">
     </div>
 
-	<div style="margin-top: -1px; padding: 0 8px;">
+	<div class="card-mobile-details-wrapper" style="margin-top: -1px; padding: 0 8px;">
 		<div style="text-decoration: none; color: black;">
-			<div class="card-mobile-title search-artist-title" style="font-weight: normal;">{{ $artist->name }}</div>
+			<div class="card-mobile-title search-artist-title" style="font-weight: normal; color: #e5e5e5;">{{ $artist->name }}</div>
 
 			<div class="card-mobile-genre-wrapper" style="margin-top: 3px; margin-left: -2px">
 				<span style="text-decoration: none; font-size: 12px; color: dimgray; margin-left: 2px; display: inline-block;" class="card-mobile-user search-artist-count">{{ $artist->videos_count }} 部影片</span>
