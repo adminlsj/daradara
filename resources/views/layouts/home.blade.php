3340fbb6651ab4a4c161a76bacea42acd9d11e55
laughseejapan <laughseejapan@gmail.com>
2022-12-14 01:36:49 +0800

    hanime1

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -33,7 +33,10 @@
 	</div>
 
 	<div class="hidden-sm hidden-md hidden-lg" style="position: relative;">
-		<img style="width: 100%; -webkit-mask-image:-webkit-gradient(linear, left top, left bottom, from(rgba(0,0,0,1)), to(rgba(0,0,0,0)));" src="{{ $random->cover }}">
+		<div style="position: relative;">
+			<img style="width: 100%;" src="https://i.imgur.com/MZfDigy.jpg">
+			<img style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; object-fit: cover; -webkit-mask-image:-webkit-gradient(linear, left top, left bottom, from(rgba(0,0,0,1)), to(rgba(0,0,0,0)));" src="{{ $random->cover }}">
+	    </div>
 		<div style="position: absolute; left: 50%; -webkit-transform: translateX(-50%); transform: translateX(-50%); width: 96%; bottom: 14%; text-align: center; color: white">
 			<h3 style="font-weight: bold; font-size: 20px;"><span style="color: crimson">H</span>anime1<span style="color: crimson">.</span>me</h3>
 			<h1 style="font-size: 28px; font-weight: bold; margin: 0">{{ $random->title }}</h1>
