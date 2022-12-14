597b126470e8d9071d73e213b5c91f43a83f6b19
laughseejapan <laughseejapan@gmail.com>
2022-12-14 12:19:24 +0800

    hanime1

--- resources/views/ads/home-banner-juicyads.blade.php
+++ resources/views/ads/home-banner-juicyads.blade.php
@@ -6,7 +6,7 @@
 	<!--JuicyAds END-->
 </div>
 
-<div class="hidden-xs hidden-md hidden-lg" style="text-align: center; padding-top: 30px; padding-bottom: 0px; margin-bottom: -30px;">
+<div class="hidden-md hidden-lg" style="text-align: center; padding-top: 35px; padding-bottom: 0px;">
 	<span style="vertical-align: top; margin-top: 5px;">
 		<!-- JuicyAds v3.1 -->
 		<script type="text/javascript" data-cfasync="false" async src="https://poweredby.jads.co/js/jads.js"></script>
@@ -15,13 +15,11 @@
 		<!--JuicyAds END-->
 	</span>
 
-	@include('layouts.exoclick', ['id' => '4372316', 'width' => '300', 'height' => '250'])
+	<span class="hidden-xs">@include('layouts.exoclick', ['id' => '4372316', 'width' => '300', 'height' => '250'])</span>
 </div>
 
-<div class="hidden-sm hidden-md hidden-lg" style="text-align: center; padding-top: 20px; padding-bottom: 0px; margin-bottom: -10px;">
-	<!-- JuicyAds v3.1 -->
+<!--<div class="hidden-sm hidden-md hidden-lg" style="text-align: center; padding-top: 20px; padding-bottom: 0px; margin-bottom: -10px;">
 	<script type="text/javascript" data-cfasync="false" async src="https://poweredby.jads.co/js/jads.js"></script>
 	<ins id="906695" data-width="300" data-height="112"></ins>
 	<script type="text/javascript" data-cfasync="false" async>(adsbyjuicy = window.adsbyjuicy || []).push({'adzone':906695});</script>
-	<!--JuicyAds END-->
-</div>
+</div>-->
