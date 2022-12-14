82fcd36db1ca25f662a6e890996503a4644c3fd5
laughseejapan <laughseejapan@gmail.com>
2022-12-14 11:58:12 +0800

    hanime1

--- resources/views/ads/home-banner-exoclick.blade.php
+++ resources/views/ads/home-banner-exoclick.blade.php
@@ -2,10 +2,10 @@
 	@include('layouts.exoclick', ['id' => '4372306', 'width' => '900', 'height' => '250'])
 </div>
 
-<div class="hidden-xs hidden-md hidden-lg" style="text-align: center; padding-top: 30px; padding-bottom: 0px;">
+<div class="hidden-md hidden-lg" style="text-align: center; padding-top: 30px; padding-bottom: 0px;">
 	@include('layouts.exoclick', ['id' => '4372316', 'width' => '300', 'height' => '250'])
 
-	<span style="vertical-align: top; margin-top: 5px;">
+	<span class="hidden-xs" style="vertical-align: top; margin-top: 5px;">
 		<!-- JuicyAds v3.1 -->
 		<script type="text/javascript" data-cfasync="false" async src="https://poweredby.jads.co/js/jads.js"></script>
 		<ins id="940480" data-width="300" data-height="262"></ins>
@@ -14,6 +14,6 @@
 	</span>
 </div>
 
-<div class="hidden-sm hidden-md hidden-lg" style="text-align: center; padding-top: 16px; padding-bottom: 0px;">
+<!-- <div class="hidden-sm hidden-md hidden-lg" style="text-align: center; padding-top: 16px; padding-bottom: 0px;">
 	@include('layouts.exoclick', ['id' => '4372324', 'width' => '300', 'height' => '100'])
-</div>
+</div> -->
