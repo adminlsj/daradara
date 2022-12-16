9d96420e23d759e15820b349c135ab98404bf8ae
laughseejapan <laughseejapan@gmail.com>
2022-12-16 16:30:38 +0800

    hanime1

--- resources/views/nav/home.blade.php
+++ resources/views/nav/home.blade.php
@@ -16,7 +16,7 @@
   <a class="nav-icon pull-right" href="/previews/{{ Carbon\Carbon::now()->format('Ym') }}"><span style="vertical-align: middle; font-size: 24px" class="material-icons-outlined">cast</span></a>
 </div>
 
-<div class="hidden-sm hidden-md hidden-lg hidden-xl" style="position: absolute; top: 50px; width: 100%; text-align: center; z-index: 99; background: linear-gradient(to bottom, rgba(0,0,0,0.4), rgba(0,0,0,0)) !important; height: 40px; line-height: 40px; left: 50%; transform: translate(-50%, -50%);">
+<div class="hidden-sm hidden-md hidden-lg hidden-xl" style="position: absolute; top: 50px; width: 100%; text-align: center; z-index: 99; background: linear-gradient(to bottom, rgba(0,0,0,0.4), rgba(0,0,0,0)) !important; height: 40px; line-height: 40px; padding-left: 4%; padding-right: 4%;">
   <div style="display: inline-block; padding: 0 4%; cursor: pointer;"><a href="{{ route('home.search') }}?genre=裏番" style="color: white; text-decoration: none;">裏番</a></div>
   <div style="display: inline-block; padding: 0 4%; cursor: pointer;"><a href="{{ route('home.search') }}?genre=3D動畫" style="color: white; text-decoration: none;">3D動畫</a></div>
   <div style="display: inline-block; padding: 0 4%; cursor: pointer;"><a href="{{ route('home.search') }}?genre=同人作品" style="color: white; text-decoration: none;">同人作品</a></div>
