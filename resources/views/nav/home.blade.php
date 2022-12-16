d3979e316ccb64dc17281d52ed3bfa2150d5f88a
laughseejapan <laughseejapan@gmail.com>
2022-12-16 12:28:56 +0800

    hanime1

--- resources/views/nav/home.blade.php
+++ resources/views/nav/home.blade.php
@@ -3,17 +3,19 @@
     <span style="color: crimson">H</span>anime1<span style="color: crimson">.</span>me
   </a>
 
-  @if (Auth::check())
+  <!-- @if (Auth::check())
     <a id="user-modal-trigger" href="{{ route('home.list') }}" style="padding-right: 0px; cursor: pointer;" class="nav-icon pull-right no-select" >
         <img style="width: 25px; border-radius: 2px;" src="{{ Auth::user()->avatar_temp }}">
     </a>
   @else
       <a style="padding-right: 0px; padding-left: 10px" class="nav-icon pull-right" href="{{ route('login') }}"><span style="vertical-align: middle; font-size: 28px" class="material-icons-outlined">account_circle</span></a>
-  @endif
+  @endif -->
+
+  <a style="padding-right: 0px; padding-left: 10px" class="nav-icon pull-right" href="{{ route('login') }}"><span style="vertical-align: middle; font-size: 28px" class="material-icons-outlined">account_circle</span></a>
 
   <a class="nav-icon pull-right" href="{{ route('home.search') }}"><span style="vertical-align: middle;" class="material-icons-outlined">search</span></a>
 
-  <a class="nav-icon pull-right" href="{{ Auth::check() ? route('user.userEditUpload', Auth::user()) : route('login') }}"><span style="vertical-align: middle; font-size: 24px" class="material-icons-outlined">cast</span></a>
+  <a class="nav-icon pull-right" href="/previews/{{ Carbon\Carbon::now()->format('Ym') }}"><span style="vertical-align: middle; font-size: 24px" class="material-icons-outlined">cast</span></a>
 
   <div style="position: absolute; top: 43px; width: 100%; text-align: center;">
     <div style="display: inline-block; padding: 0 4%; cursor: pointer;"><a href="{{ route('home.search') }}?genre=裏番" style="color: white;">裏番</a></div>
