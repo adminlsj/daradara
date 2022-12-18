1e6239129fceafafbc2e11854914d1380ce1fa7c
laughseejapan <laughseejapan@gmail.com>
2022-12-18 14:58:18 +0800

    hanime1

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,25 +1,29 @@
 <div class="row footer-new" style="font-weight: normal; padding-top: 10px; padding-bottom: 10px; font-size: 12px; margin-top: -40px">
     <div class="col-xs-6 col-sm-3">
-        <div style="line-height: 30px;"><a style="color: grey;" href="/">關於 Hanime1</a></div>
-        <div style="line-height: 30px;"><a style="color: grey;" href="/contact">聯絡我們</a></div>
-        <div style="line-height: 30px;"><a style="color: grey;" href="/contact">廣告合作</a></div>
+        <div style="line-height: 30px;"><a style="color: grey;" href="https://discord.gg/WWYc9m9CUQ" target="_blank">Discord</a></div>
+        <div style="line-height: 30px;"><a style="color: grey;" href="/contact" target="_blank">廣告合作</a></div>
+        <div style="line-height: 30px;"><a style="color: grey;" href="/copyright" target="_blank">DMCA</a></div>
+        <div style="line-height: 30px;"><a style="color: grey;" href="/2257" target="_blank">2257</a></div>
     </div>
     <div class="col-xs-6 col-sm-3">
-        <div style="line-height: 30px;"><a style="color: grey;" href="/terms">服務條款</a></div>
-        <div style="line-height: 30px;"><a style="color: grey;" href="/policies">社群規範</a></div>
-        <div style="line-height: 30px;"><a style="color: grey;" href="/copyright">版權申訴</a></div>
+        <div style="line-height: 30px;"><a style="color: grey;" href="https://theporndude.com/zh" target="_blank">PornDude</a></div>
+        <div style="line-height: 30px;"><a style="color: grey;" href="https://qingse.one" target="_blank">情色網站大全</a></div>
+        <div style="line-height: 30px;"><a style="color: grey;" href="https://141jj.com/" target="_blank">141JJ 導航</a></div>
+        <div style="line-height: 30px;"><a style="color: grey;" href="http://www.pornbest.org/" target="_blank">PornBest 免費中文視頻</a></div>
     </div>
     <div class="col-xs-6 col-sm-3">
-        <div style="line-height: 30px;"><a style="color: grey;" href="/copyright">DMCA</a></div>
-        <div style="line-height: 30px;"><a style="color: grey;" href="/2257">2257</a></div>
-        <div style="line-height: 30px;"><a style="color: grey;" href="/">{{ gethostname() }}</a></div>
+        <div style="line-height: 30px;"><a style="color: grey;" href="https://www.17dm.net/" target="_blank">妖氣動漫導航</a></div>
+        <div style="line-height: 30px;"><a style="color: grey;" href="https://share.acgnx.net/" target="_blank">末日動漫資源庫</a></div>
+        <div style="line-height: 30px;"><a style="color: grey;" href="https://moeli-desu.com/" target="_blank">夢璃</a></div>
+        <div style="line-height: 30px;"><a style="color: grey;" href="https://www.sshs.pw/" target="_blank">紳士會所</a></div>
     </div>
     <div class="col-xs-6 col-sm-3">
-        <div style="line-height: 30px;"><a style="color: grey;" href="/">H動漫</a></div>
-        <div style="line-height: 30px;"><a style="color: grey;" href="/">成人色情</a></div>
-        <div style="line-height: 30px;"><a style="color: grey;" href="/">Hentai</a></div>
+        <div style="line-height: 30px;"><a style="color: grey;" href="#">H動漫</a></div>
+        <div style="line-height: 30px;"><a style="color: grey;" href="#">Hentai</a></div>
+        <div style="line-height: 30px;"><a style="color: grey;" href="#">成人色情</a></div>
+        <div style="line-height: 30px;"><a style="color: grey;" href="#">免費線上看</a></div>
     </div>
     <div class="col-sm-12">
-        <div style="line-height: 30px; color: grey;">© 2020-2022 Hanime1.me</div>
+        <div style="line-height: 30px; color: grey;">© 2020-2022 Hanime1.me ({{ gethostname() }})</div>
     </div>
 </div>
