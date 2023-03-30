8e3352319f34c7038b6500a763bcd41da95a593f
laughseejapan <laughseejapan@gmail.com>
2023-03-30 17:25:56 +0800

    hanime1

--- resources/views/video/watch-2022-12-19.blade.php
+++ resources/views/video/watch-2022-12-19.blade.php
@@ -54,107 +54,115 @@
         @include('layouts.exoclick', ['id' => '4372406', 'width' => '728', 'height' => '90'])
       </div>
 
-      <h3 id="shareBtn-title" style="line-height: 30px; font-weight: bold; font-size: 1.5em; margin-top: 10px; color: white;">{{ $video->translations['JP'] }}</h3>
+      <div class="video-details-wrapper">
+        <h3 id="shareBtn-title" style="line-height: 30px; font-weight: bold; font-size: 1.5em; margin-top: 10px; color: white;">{{ $video->translations['JP'] }}</h3>
 
-      <div style="display: inline-block;">
-        <a href="{{ route('home.search') }}?query={{ $artist->name }}&genre={{ $video->genre }}"><img style="width: 40px; border-radius: 50%;" src="{{ $artist->avatar_temp }}" alt="{{ $artist->name }}"></a>
-        <div style="display: inline-block; vertical-align: middle; margin-left: 8px">
-          <div style="font-size: 16px;"><a style="color: white; text-decoration: none;" href="{{ route('home.search') }}?query={{ $artist->name }}&genre={{ $video->genre }}">{{ $artist->name }}</a></div>
-          <div style="font-size: 12px; color: #aaa; font-weight: normal"><a style="color: #aaa;" href="{{ route('home.search') }}?genre={{ $video->genre }}">{{ $video->genre }}</a></div>
-        </div>
-      </div>
+        <div style="width: 100%">
+          <div style="display: inline-block;">
+            <a href="{{ route('home.search') }}?query={{ $artist->name }}&genre={{ $video->genre }}"><img style="width: 40px; border-radius: 50%;" src="{{ $artist->avatar_temp }}" alt="{{ $artist->name }}"></a>
+            <div style="display: inline-block; vertical-align: middle; margin-left: 8px">
+              <div style="font-size: 16px;"><a style="color: white; text-decoration: none;" href="{{ route('home.search') }}?query={{ $artist->name }}&genre={{ $video->genre }}">{{ $artist->name }}</a></div>
+              <div style="font-size: 12px; color: #aaa; font-weight: normal"><a style="color: #aaa;" href="{{ route('home.search') }}?genre={{ $video->genre }}">{{ $video->genre }}</a></div>
+            </div>
+          </div>
 
-      <div class="no-select video-subscribe-btn" style="display: inline-block; background-color: #F1F1F1; height: 36px; line-height: 36px; border-radius: 50px; padding: 0 16px; margin-left: 20px; vertical-align: middle;  cursor: pointer;" data-toggle="modal" data-target="#subscribeModal">
-        訂閱
-      </div>
-
-      <div id="subscribeModal" class="modal" role="dialog">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <span class="material-icons pull-left no-select modal-close-btn" data-dismiss="modal">close</span>
-              <h4 class="modal-title">訂閱作者</h4>
-            </div>
-            <div class="modal-body" style="padding-bottom: 20px; text-align: left;">
-              <h4>追蹤喜歡的作者</h4>
-              <p id="hentai-tags-text" style="color: darkgray;">訂閱功能即將全面開放！</p>
-            </div>
-            <hr style="border-color: #323434; margin: 0; margin-top: -10px;">
-            <div class="modal-footer">
-              <div data-dismiss="modal">返回</div>
-              <button data-dismiss="modal" class="pull-right btn btn-primary">我知道了</button>
-            </div>
+          <div class="no-select video-subscribe-btn hidden-md" style="display: inline-block; background-color: #F1F1F1; height: 36px; line-height: 36px; border-radius: 50px; padding: 0 16px; margin-left: 20px; vertical-align: middle; cursor: pointer; float: right;" data-toggle="modal" data-target="#subscribeModal">
+            訂閱
           </div>
         </div>
-      </div>
 
-      <div class="video-show-action-btn no-select" style="padding: 0 7px;" data-toggle="modal" data-target="#reportModal">
-        <i class="material-icons" style="vertical-align: middle; margin-top: -3px">more_horiz</i>
-      </div>
-
-      <div id="shareBtn" class="video-show-action-btn no-select" style="padding: 0 16px; margin-right: 8px;" data-toggle="modal" data-target="#shareModal">
-        <i class="material-icons" style="vertical-align: middle; margin-top: -3px; font-size: 20px; margin-left: -1px; margin-right: 9px;">share</i>分享
-      </div>
-
-      @if ($qualities != null || $downloads != null)
-        <a href="{{ route('video.download') }}?v={{ $video->id }}" target="_blank" id="downloadBtn" class="single-icon-wrapper">
-          <div class="video-show-action-btn no-select" style="padding: 0 16px; margin-right: 8px;">
-            <i class="material-icons" style="vertical-align: middle; margin-top: -2px; font-size: 22px; margin-left: -2px; margin-right: 8px;">download</i>下載
-          </div>
-        </a>
-      @else
-        <a class="single-icon-wrapper" title="無法下載" disabled="true">
-          <div class="video-show-action-btn no-select" style="padding: 0 16px; margin-right: 8px; background-color: #2E2E2E !important;">
-            <i class="material-icons" style="vertical-align: middle; margin-top: -2px; font-size: 22px; margin-left: -2px; margin-right: 8px;">download</i>下載
-          </div>
-        </a>
-      @endif
-
-      <div id="video-save-form-wrapper" class="video-show-action-btn no-select" style="margin-right: 8px;">
-        @if (!Auth::check())
-          <div data-toggle="modal" data-target="#signUpModal" style="text-decoration: none; color: inherit; text-align: center; cursor: pointer;" class="single-icon-wrapper">
-            <div class="single-icon no-select">
-              <i style="padding-top: 7px; font-size: 21px; color: white" class="material-icons">add_circle_outline</i>
+        <div id="subscribeModal" class="modal" role="dialog">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <span class="material-icons pull-left no-select modal-close-btn" data-dismiss="modal">close</span>
+                <h4 class="modal-title">訂閱作者</h4>
+              </div>
+              <div class="modal-body" style="padding-bottom: 20px; text-align: left;">
+                <h4>追蹤喜歡的作者</h4>
+                <p id="hentai-tags-text" style="color: darkgray;">訂閱功能即將全面開放！</p>
+              </div>
+              <hr style="border-color: #323434; margin: 0; margin-top: -10px;">
+              <div class="modal-footer">
+                <div data-dismiss="modal">返回</div>
+                <button data-dismiss="modal" class="pull-right btn btn-primary">我知道了</button>
+              </div>
             </div>
           </div>
-        @else
-          @include('video.saveBtn-new', ['save_icon' => $saved || $listed != '[]' ? 'playlist_add_check' : 'playlist_add', 'save_text' => $saved || $listed != '[]' ? '已儲存' : '儲存'])
-        @endif
-      </div>
+        </div>
 
-      <div id="video-like-form-wrapper" class="video-show-action-btn no-select" style="margin-right: 8px;">
-        @if (!Auth::check())
-          <div data-toggle="modal" data-target="#signUpModal" style="text-decoration: none; color: inherit; text-align: center; cursor: pointer;" class="single-icon-wrapper">
-            <div class="single-icon no-select">
-                <i style="color: white; padding-top: 8px; font-size: 20px" class="material-icons">favorite_border</i>
-            </div>
+        <div class="video-buttons-wrapper">
+          <div class="video-show-action-btn no-select" style="padding: 0 7px;" data-toggle="modal" data-target="#reportModal">
+            <i class="material-icons" style="vertical-align: middle; margin-top: -3px">more_horiz</i>
           </div>
-        @else
-          @include('video.likeBtn', ['user_id' => Auth::user()->id, 'video_id' => $video->id, 'likes_count' => $video->likes_count])
-        @endif
-      </div>
 
-      @if ($video->comic_id)
-        <a class="video-show-action-btn no-select" style="color: #e9e9e9; text-decoration: none; padding: 0 16px; margin-right: 8px;" href="{{ route('comic.showCover', ['comic' => $video->comic_id]) }}" target="_blank">
-            <i class="material-icons-outlined" style="vertical-align: middle; margin-top: -5px; font-size: 20px; margin-right: 10px;">import_contacts</i>漫畫原作
-        </a>
-      @endif
+          <div id="shareBtn" class="video-show-action-btn no-select hidden-md" style="padding: 0 16px; margin-right: 8px;" data-toggle="modal" data-target="#shareModal">
+            <i class="material-icons" style="vertical-align: middle; margin-top: -3px; font-size: 20px; margin-left: -1px; margin-right: 9px;">share</i>分享
+          </div>
 
-      <div class="video-description-panel video-description-panel-hover no-select" style="cursor: pointer; color: white; margin-top: 15px; padding: 10px 12px; border-radius: 15px; position: relative;">
-        <div>觀看次數：{{ $video->views() }}次&nbsp;&nbsp;{{ Carbon\Carbon::parse($video->created_at)->format('Y-m-d') }}</div>
-        <div style="margin-top: 5px">{{ $video->title }}</div>
-        <div class="video-caption-text caption-ellipsis" style="color: #b8babc; margin-top: 5px; font-weight: normal;">{{ $video->caption }}</div>
-      </div>
-
-      <div style="margin-top: 20px; margin-bottom: -20px">
-        @foreach ($tags as $tag)
-          @if ($tag != $video->artist)
-            <div class="single-video-tag" style="margin-bottom: 16px; font-weight: normal"><a style="border-radius: 15px;" href="/search?tags%5B%5D={{ $tag }}{{ $doujin ? '' : '&genre=裏番' }}">{{ $tag }}</a></div>
+          @if ($qualities != null || $downloads != null)
+            <a href="{{ route('video.download') }}?v={{ $video->id }}" target="_blank" id="downloadBtn" class="single-icon-wrapper">
+              <div class="video-show-action-btn no-select" style="padding: 0 16px; margin-right: 8px;">
+                <i class="material-icons" style="vertical-align: middle; margin-top: -2px; font-size: 22px; margin-left: -2px; margin-right: 8px;">download</i>下載
+              </div>
+            </a>
+          @else
+            <a class="single-icon-wrapper" title="無法下載" disabled="true">
+              <div class="video-show-action-btn no-select" style="padding: 0 16px; margin-right: 8px; background-color: #2E2E2E !important;">
+                <i class="material-icons" style="vertical-align: middle; margin-top: -2px; font-size: 22px; margin-left: -2px; margin-right: 8px;">download</i>下載
+              </div>
+            </a>
           @endif
-        @endforeach
 
-        <div class="single-video-tag" data-toggle="modal" data-target="{{ Auth::check() ? '#add-tags-modal' : '#signUpModal' }}" style="position: relative; cursor: pointer; margin-bottom: 16px; font-weight: normal"><a style="padding-left: 14px; padding-right: 14px; background-color: #2E2E2E; border-radius: 15px;"><span class="material-icons" style="position: absolute; margin-left: auto; margin-right: auto; left: -3px; right: 0; text-align: center; margin-top: -2px; font-size: 22px; vertical-align: middle">add</span></a></div>
-        <div class="single-video-tag" data-toggle="modal" data-target="{{ Auth::check() ? '#remove-tags-modal' : '#signUpModal' }}" style="position: relative; cursor: pointer; margin-bottom: 16px; font-weight: normal"><a style="padding-left: 14px; padding-right: 14px; background-color: #2E2E2E; border-radius: 15px;"><span class="material-icons" style="position: absolute; margin-left: auto; margin-right: auto; left: -2px; right: 0; text-align: center; margin-top: -2px; font-size: 22px; vertical-align: middle">remove</span></a></div>
+          <div id="video-save-form-wrapper" class="video-show-action-btn no-select" style="margin-right: 8px;">
+            @if (!Auth::check())
+              <div data-toggle="modal" data-target="#signUpModal" style="text-decoration: none; color: inherit; text-align: center; cursor: pointer;" class="single-icon-wrapper">
+                <div class="single-icon no-select">
+                  <i style="padding-top: 7px; font-size: 21px; color: white" class="material-icons">add_circle_outline</i>
+                </div>
+              </div>
+            @else
+              @include('video.saveBtn-new', ['save_icon' => $saved || $listed != '[]' ? 'playlist_add_check' : 'playlist_add', 'save_text' => $saved || $listed != '[]' ? '已儲存' : '儲存'])
+            @endif
+          </div>
+
+          <div id="video-like-form-wrapper" class="video-show-action-btn no-select" style="margin-right: 8px;">
+            @if (!Auth::check())
+              <div data-toggle="modal" data-target="#signUpModal" style="text-decoration: none; color: inherit; text-align: center; cursor: pointer;" class="single-icon-wrapper">
+                <div class="single-icon no-select">
+                    <i style="color: white; padding-top: 8px; font-size: 20px" class="material-icons">favorite_border</i>
+                </div>
+              </div>
+            @else
+              @include('video.likeBtn', ['user_id' => Auth::user()->id, 'video_id' => $video->id, 'likes_count' => $video->likes_count])
+            @endif
+          </div>
+
+          @if ($video->comic_id)
+            <a class="video-show-action-btn no-select" style="color: #e9e9e9; text-decoration: none; padding: 0 16px; margin-right: 8px;" href="{{ route('comic.showCover', ['comic' => $video->comic_id]) }}" target="_blank">
+                <i class="material-icons-outlined" style="vertical-align: middle; margin-top: -5px; font-size: 20px; margin-right: 10px;">import_contacts</i>漫畫原作
+            </a>
+          @endif
+        </div>
+
+        <br><br><br><br><br>
+
+        <div class="video-description-panel video-description-panel-hover no-select" style="cursor: pointer; color: white; margin-top: -35px; padding: 10px 12px; border-radius: 15px; position: relative;">
+          <div>觀看次數：{{ $video->views() }}次&nbsp;&nbsp;{{ Carbon\Carbon::parse($video->created_at)->format('Y-m-d') }}</div>
+          <div style="margin-top: 5px">{{ $video->title }}</div>
+          <div class="video-caption-text caption-ellipsis" style="color: #b8babc; margin-top: 5px; font-weight: normal;">{{ $video->caption }}</div>
+        </div>
+
+        <div style="margin-top: 20px; margin-bottom: -20px">
+          @foreach ($tags as $tag)
+            @if ($tag != $video->artist)
+              <div class="single-video-tag" style="margin-bottom: 16px; font-weight: normal"><a style="border-radius: 15px;" href="/search?tags%5B%5D={{ $tag }}{{ $doujin ? '' : '&genre=裏番' }}">{{ $tag }}</a></div>
+            @endif
+          @endforeach
+
+          <div class="single-video-tag" data-toggle="modal" data-target="{{ Auth::check() ? '#add-tags-modal' : '#signUpModal' }}" style="position: relative; cursor: pointer; margin-bottom: 16px; font-weight: normal"><a style="padding-left: 14px; padding-right: 14px; background-color: #2E2E2E; border-radius: 15px;"><span class="material-icons" style="position: absolute; margin-left: auto; margin-right: auto; left: -3px; right: 0; text-align: center; margin-top: -2px; font-size: 22px; vertical-align: middle">add</span></a></div>
+          <div class="single-video-tag" data-toggle="modal" data-target="{{ Auth::check() ? '#remove-tags-modal' : '#signUpModal' }}" style="position: relative; cursor: pointer; margin-bottom: 16px; font-weight: normal"><a style="padding-left: 14px; padding-right: 14px; background-color: #2E2E2E; border-radius: 15px;"><span class="material-icons" style="position: absolute; margin-left: auto; margin-right: auto; left: -2px; right: 0; text-align: center; margin-top: -2px; font-size: 22px; vertical-align: middle">remove</span></a></div>
+        </div>
       </div>
 
       <div class="hidden-md hidden-lg" style="margin-top: 25px">
