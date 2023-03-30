fa7e5e77bf48c5668f02a552367f4e4b4e764f34
laughseejapan <laughseejapan@gmail.com>
2023-03-30 17:34:02 +0800

    hanime1

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -38,7 +38,7 @@
 
 	<div class="hidden-sm hidden-md hidden-lg" style="position: relative;">
 		<div style="position: relative;">
-			<img style="width: 100%;" src="https://i.imgur.com/MZfDigy.jpg">
+			<img style="width: 100%;" src="https://i.imgur.com/Sk447OV.jpg">
 			<img style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; object-position: center 0px; object-fit: cover; -webkit-mask-image:-webkit-gradient(linear, left top, left bottom, from(rgba(0,0,0,1)), to(rgba(0,0,0,0)));" src="{{ $random->cover }}">
 	    </div>
 		<div style="position: absolute; left: 50%; -webkit-transform: translateX(-50%); transform: translateX(-50%); width: 96%; bottom: 14%; text-align: center; color: white">
