469d7241b797b31db19655e5e0f08e16f0e896f4
laughseejapan <laughseejapan@gmail.com>
2023-04-01 01:45:32 +0800

    hanime1

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -20,6 +20,8 @@ class ComicController extends Controller
         }
         $newest = Comic::orderBy('created_at', 'desc')->select('id', 'nhentai_id', 'galleries_id', 'title_n_before', 'title_n_pretty', 'title_n_after', 'extension', 'prefix', 'created_at')->paginate(30);
 
+        $newest->setPath('');
+
         return view('comic.index', compact('trending', 'newest'));
     }
 
