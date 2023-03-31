a6c65bc7452ffde574fd3b067b341e92461d5ef7
laughseejapan <laughseejapan@gmail.com>
2023-03-31 14:34:48 +0800

    hanime1

--- resources/views/video/likeBtn.blade.php
+++ resources/views/video/likeBtn.blade.php
@@ -6,8 +6,8 @@
   <input name="like-status" type="hidden" value="{{ $liked }}">
   <input name="likes-count" type="hidden" value="{{ $likes_count }}">
   <button id="video-like-btn" class="single-icon-wrapper no-button-style" method="POST">
-  	<div class="single-icon no-select" style="width: 100%; padding: 0 16px; border-radius: 50px;">
-	    <i style="vertical-align: middle; margin-top: -3px; font-size: 20px; margin-right: 10px;" class="material-icons{{ $liked ? '' : '-outlined'}}">thumb_up</i>{{ $likes_count }}
+  	<div class="single-icon no-select">
+	    <i class="material-icons{{ $liked ? '' : '-outlined'}}">thumb_up</i>{{ $likes_count }}
   	</div>
   </button>
 </form>
