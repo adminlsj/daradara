0cc1cf3b25ffdb6cfd3f11a9b584938dbfd72ce1
laughseejapan <laughseejapan@gmail.com>
2023-03-31 16:18:16 +0800

    hanime1

--- resources/views/video/playlist-panel.blade.php
+++ resources/views/video/playlist-panel.blade.php
@@ -1,8 +1,8 @@
 <div id="video-playlist-wrapper">
-  <div class="single-icon-wrapper video-playlist-top">
+  <div class="single-icon-wrapper video-playlist-top" style="position: relative;">
     <h4 style="margin-top: 0px; line-height: 20px; margin-bottom: -5px; font-size: 14px; color: white; overflow: hidden;text-overflow: ellipsis; display: -webkit-box; -webkit-line-clamp: 2; -webkit-box-orient: vertical; font-weight: bold;">{{ $current->watch->title }}</h4>
     <h4 style="font-size: 12px; color: darkgray; font-weight: bold; margin-bottom: 4px;">{{ $videos->count() }} 部影片</h4>
-    <div id="hide-playlist-btn" style="position: absolute; right: 10px; top: 21px; cursor: pointer;" class="single-icon no-select hidden-md hidden-lg">
+    <div id="hide-playlist-btn" style="position: absolute; right: 10px; top: 14px; cursor: pointer;" class="single-icon no-select hidden-md hidden-lg">
       <i class="material-icons noselect" style="padding-top: 3px; padding-left: 3px; font-size: 30px; color: white">keyboard_arrow_down</i>
     </div>
   </div>
