c81fdbf9ee3ba890f8f832aa2bbfc257de63addb
laughseejapan <laughseejapan@gmail.com>
2023-07-01 19:59:32 +0800

    hanime1

--- app/Console/Commands/UpdateYoujizzDownloadsSc.php
+++ app/Console/Commands/UpdateYoujizzDownloadsSc.php
@@ -5,7 +5,7 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use App\Youjizz;
 
-class UpdateYoujizzDownloads extends Command
+class UpdateYoujizzDownloadsSc extends Command
 {
     /**
      * The name and signature of the console command.
