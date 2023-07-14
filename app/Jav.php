842083741e19b94cbd21b054ae33309bc29452ca
laughseejapan <laughseejapan@gmail.com>
2023-07-14 19:16:30 +0800

    hanime1

--- app/Jav.php
+++ app/Jav.php
@@ -64,6 +64,7 @@ class Jav
         $videos = Video::where('id', '>=', 48445)
                     ->where('genre', '日本AV')
                     ->where('created_at', '2000-01-01 00:00:00')
+                    ->where('foreign_sd', 'not like', '%"missav"%')
                     ->orderBy('id', 'asc')
                     ->get()
                     ->split($total)[$number - 1]
@@ -80,78 +81,50 @@ class Jav
                 ->userAgent(Spankbang::$userAgents[array_rand(Spankbang::$userAgents)])
                 ->bodyHtml();
 
-            $downloads = 'https://rapidgator.net/file/'.Helper::get_string_between($missav_html, 'https://rapidgator.net/file/', '"');
-            $created_at = preg_replace('/\s+/', '', explode('>', Helper::get_string_between($missav_html, '發行日期:</span>', '</span>'))[1]).' '.Carbon::now()->toTimeString();
-            $created_at = Carbon::parse($created_at);
-            $code = preg_replace('/\s+/', '', explode('>', Helper::get_string_between($missav_html, '番號:</span>', '</span>'))[1]);
-            $title_jp = $code.' '.trim(explode('>', Helper::get_string_between($missav_html, '標題:</span>', '</span>'))[1]);
-            $characters = Helper::get_string_between($missav_html, '女優:</span>', '</div>');
-            $characters = explode(',', $characters);
-            foreach ($characters as &$character) {
-                $character = Helper::get_string_between($character, '>', '<');
+            if (strpos($missav_html, "找不到頁面") !== false) {
+                $temp = $video->foreign_sd;
+                $temp['missav'] = '404';
+                $video->foreign_sd = $temp;
+                $video->save();
+
+                Log::info('Missav update ID#'.$video->id.' failed...');
+
+            } else {
+                $downloads = 'https://rapidgator.net/file/'.Helper::get_string_between($missav_html, 'https://rapidgator.net/file/', '"');
+                $created_at = preg_replace('/\s+/', '', explode('>', Helper::get_string_between($missav_html, '發行日期:</span>', '</span>'))[1]).' '.Carbon::now()->toTimeString();
+                $created_at = Carbon::parse($created_at);
+                $code = preg_replace('/\s+/', '', explode('>', Helper::get_string_between($missav_html, '番號:</span>', '</span>'))[1]);
+                $title_jp = $code.' '.trim(explode('>', Helper::get_string_between($missav_html, '標題:</span>', '</span>'))[1]);
+                $characters = Helper::get_string_between($missav_html, '女優:</span>', '</div>');
+                $characters = explode(',', $characters);
+                foreach ($characters as &$character) {
+                    $character = Helper::get_string_between($character, '>', '<');
+                }
+                $brand = Helper::get_string_between($missav_html, '發行商:</span>', '</div>');
+                $brand = trim(Helper::get_string_between($brand, '>', '<'));
+                if (strpos($brand, "Moody's") !== false) {
+                    $brand = 'Moodyz';
+                }
+                if ($video->caption == '' || $video->caption == null) {
+                    $caption = trim(Helper::get_string_between($missav_html, 'line-clamp-2">', '</div>'));
+                    $video->caption = $caption;
+                }
+
+                $video->translations = ['JP' => $title_jp];
+                $video->downloads = ['720' => $downloads];
+                $video->artist = $brand;
+                $video->created_at = $created_at;
+                $video->uploaded_at = $created_at;
+
+                $temp = $video->foreign_sd;
+                $temp['characters'] = implode(',', $characters);
+                $temp['missav'] = $missav_link;
+                $temp['poster'] = trim(Helper::get_string_between($missav_html, 'property="og:image" content="', '"'));
+                $video->foreign_sd = $temp;
+                $video->save();
+
+                Log::info('Missav update ID#'.$video->id.' success...');
             }
-            $brand = Helper::get_string_between($missav_html, '發行商:</span>', '</div>');
-            $brand = trim(Helper::get_string_between($brand, '>', '<'));
-            if (strpos($brand, "Moody's") !== false) {
-                $brand = 'Moodyz';
-            }
-            if ($video->caption == '' || $video->caption == null) {
-                $caption = trim(Helper::get_string_between($missav_html, 'line-clamp-2">', '</div>'));
-                $video->caption = $caption;
-            }
-
-            /* $imgur = '';
-            $cover = '';
-            $imgur_url = trim(Helper::get_string_between($missav_html, 'property="og:image" content="', '"'));
-            $image = Image::make($imgur_url);
-            $image = $image->fit(2880, 1620, function ($constraint) {}, "top");
-            $image = $image->stream();
-            $pvars = array('image' => base64_encode($image));
-            $curl = curl_init();
-            curl_setopt($curl, CURLOPT_URL, 'https://api.imgur.com/3/image.json');
-            curl_setopt($curl, CURLOPT_TIMEOUT, 30);
-            curl_setopt($curl, CURLOPT_HTTPHEADER, array('Authorization: Client-ID ' . '5b63b1c883ddb72'));
-            curl_setopt($curl, CURLOPT_POST, 1);
-            curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-            curl_setopt($curl, CURLOPT_POSTFIELDS, $pvars);
-            $out = curl_exec($curl);
-            curl_close ($curl);
-            $pms = json_decode($out, true);
-            $imgur = $pms['data']['link'];
-
-            $image = Image::make($imgur_url);
-            $image = $image->fit(268, 394, function ($constraint) {}, "right");
-            $image = $image->stream();
-            $pvars = array('image' => base64_encode($image));
-            $curl = curl_init();
-            curl_setopt($curl, CURLOPT_URL, 'https://api.imgur.com/3/image.json');
-            curl_setopt($curl, CURLOPT_TIMEOUT, 30);
-            curl_setopt($curl, CURLOPT_HTTPHEADER, array('Authorization: Client-ID ' . '5b63b1c883ddb72'));
-            curl_setopt($curl, CURLOPT_POST, 1);
-            curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-            curl_setopt($curl, CURLOPT_POSTFIELDS, $pvars);
-            $out = curl_exec($curl);
-            curl_close ($curl);
-            $pms = json_decode($out, true);
-            $cover = $pms['data']['link']; */
-
-            $video->translations = ['JP' => $title_jp];
-            $video->downloads = ['720' => $downloads];
-            $video->artist = $brand;
-            $video->created_at = $created_at;
-            $video->uploaded_at = $created_at;
-            // $video->imgur = Helper::get_string_between($imgur, 'https://i.imgur.com/', '.');
-
-            $temp = $video->foreign_sd;
-            $temp['characters'] = implode(',', $characters);
-            $temp['missav'] = $missav_link;
-            $temp['poster'] = trim(Helper::get_string_between($missav_html, 'property="og:image" content="', '"'));
-            // $temp['cover'] = Helper::get_string_between($cover, 'https://i.imgur.com/', '.');
-            // $temp['thumbnail'] = Helper::get_string_between($imgur, 'https://i.imgur.com/', '.');
-            $video->foreign_sd = $temp;
-            $video->save();
-
-            Log::info('Missav update ID#'.$video->id.' success...');
 
             if ($videos->last() != $video) {
                 sleep(10);
