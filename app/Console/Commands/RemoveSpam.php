0015b428510bebfaf8b919463c9a68fdb14f17ed
laughseejapan <laughseejapan@gmail.com>
2023-07-17 00:58:29 +0800

    hanime1

--- app/Console/Commands/RemoveSpam.php
+++ app/Console/Commands/RemoveSpam.php
@@ -81,7 +81,8 @@ class RemoveSpam extends Command
                                 '103.149.249.228',
                                 '45.62.172.8',
                                 '45.62.172.3',
-                                '69.25.117.245'
+                                '69.25.117.245',
+                                '103.156.242.237'
                             ])
                             ->where('created_at', '>=', Carbon::now()->subDay())
                             ->groupBy('user_id')
