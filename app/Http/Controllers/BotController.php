62e37de6d6874fe5f91c9fc7e01e5343b1016bb1
laughseejapan <laughseejapan@gmail.com>
2023-07-19 18:43:24 +0800

    hanime1

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -39,9 +39,9 @@ class BotController extends Controller
 
         Log::info('Playlist update started...');
 
-        $code = "FSDSS-";
-        $user_id = 40;
-        $default_watch_id = 4283;
+        $code = "GVG-";
+        $user_id = 563947;
+        $default_watch_id = 5016;
         $videos = Video::where('user_id', 1)->where('title', 'like', "{$code}%")->where('foreign_sd', 'like', '%"missav"%')->orderBy('title', 'asc')->get();
         foreach ($videos as $video) {
             $video->user_id = $user_id;
