e3bbe0742bb0de16365fb8a965f79a496244ccce
laughseejapan <laughseejapan@gmail.com>
2023-07-25 21:24:38 +0800

    hanime1

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -39,14 +39,14 @@ class BotController extends Controller
 
         Log::info('Playlist update started...');
 
-        // $artist = 'ナンパTV';
-        $code = "DFE-";
-        $user_id = 569205;
-        $default_watch_id = 7162;
+        $artist = 'Glory Quest';
+        $code = "GG-";
+        $user_id = 563947;
+        $default_watch_id = 5016;
         $videos = Video::where('user_id', 1)->where('title', 'like', "{$code}%")->where('foreign_sd', 'like', '%"missav"%')->orderBy('title', 'asc')->get();
         foreach ($videos as $video) {
             $video->user_id = $user_id;
-            // $video->artist = $artist;
+            $video->artist = $artist;
             $missav_html = Browsershot::url($video->foreign_sd['missav'])
                 ->timeout(20)
                 ->setExtraHttpHeaders(['Referer' => 'https://missav.com/'])
