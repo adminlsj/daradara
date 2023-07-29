4593a8baa0c8a83a006c236c9cf2a5034bfe95c8
laughseejapan <laughseejapan@gmail.com>
2023-07-29 11:29:02 +0800

    hanime1

--- app/Console/Commands/RemoveSpam.php
+++ app/Console/Commands/RemoveSpam.php
@@ -60,7 +60,7 @@ class RemoveSpam extends Command
         }
 
         $ip_user_array = Comment::whereIn('ip_address', [
-                                '139.5.108.130', '139.5.108.194', '49.0.255.6', '84.17.45.179', '2001:b400:e737:a90d:b8a3:6b9b:6433:fabd'
+                                '139.5.108.130', '139.5.108.194', '49.0.255.6', '84.17.45.179', '2001:b400:e737:a90d:b8a3:6b9b:6433:fabd', '49.0.255.6'
                             ])
                             ->where('created_at', '>=', Carbon::now()->subDay())
                             ->groupBy('user_id')
