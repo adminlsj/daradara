725e6ef9efda80f552c579e2a72fba1fd5fe613d
laughseejapan <laughseejapan@gmail.com>
2023-08-01 01:56:41 +0800

    hanime1

--- app/Jav.php
+++ app/Jav.php
@@ -466,7 +466,7 @@ class Jav
                     $video = Video::create([
                         // HSCK user_id = 575858
                         'user_id' => 1,
-                        'playlist_id' => 8907,
+                        'playlist_id' => 8908,
                         'title' => strtoupper($title),
                         'translations' => ['JP' => strtoupper($title)],
                         'caption' => '',
