057924454d18e90a6bce2e7ad5aa388886292a34
laughseejapan <laughseejapan@gmail.com>
2023-08-01 03:03:12 +0800

    hanime1

--- app/Jav.php
+++ app/Jav.php
@@ -401,7 +401,7 @@ class Jav
         Log::info('Hscangku shirouto upload started...');
 
         $chinese = new Chinese();
-        for ($i = 3; $i <= $pages; $i++) { 
+        for ($i = 10; $i <= $pages; $i++) { 
             $base = Jav::$base;
             $page_url = "{$base}/vodtype/15-{$i}.html";
 
