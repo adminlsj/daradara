20888a738690405405017133bbb3b65eb3244f92
laughseejapan <laughseejapan@gmail.com>
2023-11-06 15:37:42 +0800

    hanime1

--- resources/views/nav/home.blade.php
+++ resources/views/nav/home.blade.php
@@ -1,7 +1,7 @@
 <div id="main-nav-home-mobile" style="z-index: 10000 !important; position: fixed !important; overflow-x: hidden; background: none; transition: height 0.3s, background-color 0.4s, backdrop-filter 0.4s, -webkit-backdrop-filter 0.4s, top 0.4s;" class="hidden-sm hidden-md hidden-lg">
 
   <div style="padding: 0 15px;">
-    <a href="/" style="padding-right: 2.5%; color: white; font-size: 1.45em; line-height: 53px; letter-spacing: 1px; margin-left: 13px;">
+    <a href="/" style="padding-right: 2.5%; color: white; font-size: 1.45em; line-height: 52px; letter-spacing: 1px; margin-left: 5px;">
       <img style="width: 18px;" src="https://i.imgur.com/9Yt93a3.png">
     </a>
 
