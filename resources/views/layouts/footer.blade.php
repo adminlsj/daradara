0bb14fd5c2fa949fbca396cd71997fb7adee1018
laughseejapan <laughseejapan@gmail.com>
2023-11-07 20:57:26 +0800

    hanime1

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -30,40 +30,40 @@
 
 <div class="row footer-new hidden-sm hidden-md hidden-lg" style="font-weight: normal; padding: 15px; font-size: 13px; margin-top: -50px !important; background-color: #161616;">
     <div class="col-xs-12">
-        <div style="line-height: 30px; color: #e5e5e5; font-weight: bold">用戶支援</div>
-        <div style="line-height: 30px;"><a style="color: grey;" href="{{ App\Helper::$discord }}" target="_blank">Discord</a></div>
-        <div style="line-height: 30px;"><a style="color: grey;" href="/contact" target="_blank">廣告合作</a></div>
-        <div style="line-height: 30px;"><a style="color: grey;" href="/copyright" target="_blank">DMCA</a></div>
-        <div style="line-height: 30px;"><a style="color: grey;" href="/2257" target="_blank">2257</a></div>
+        <div style="line-height: 30px; color: #e2e2e2; font-weight: bold">用戶支援</div>
+        <div style="line-height: 30px;"><a style="color: dimgray;" href="{{ App\Helper::$discord }}" target="_blank">Discord</a></div>
+        <div style="line-height: 30px;"><a style="color: dimgray;" href="/contact" target="_blank">廣告合作</a></div>
+        <div style="line-height: 30px;"><a style="color: dimgray;" href="/copyright" target="_blank">DMCA</a></div>
+        <div style="line-height: 30px;"><a style="color: dimgray;" href="/2257" target="_blank">2257</a></div>
         <hr style="border-color: #353535; margin: 15px 0px">
     </div>
     <div class="col-xs-12">
-        <div style="line-height: 30px; color: #e5e5e5; font-weight: bold">贊助商</div>
-        <div style="line-height: 30px;"><a style="color: grey;" href="https://creative.kbnmnl.com/widgets/v4/Universal?campaignId=300x100&modelPageOption=model&tag=girls%2Fchinese%2Cgirls%2Fjapanese&thumbsMargin=5&hideButton=1&hideTitle=1&userId=68266da2436a81581f441c04a73d1525467dff2da85808235979b437cff6f852&quality=240p&autoplay=all&autoplayForce=1" target="_blank">Stripchat</a></div>
-        <div style="line-height: 30px;"><a style="color: grey;" href="https://l.erodatalabs.com/s/0kHIbk" target="_blank">Erolabs</a></div>
-        <div style="line-height: 30px;"><a style="color: grey;" href="{{ route('jav.home') }}" target="_blank">日本AV 高清中字</a></div>
-        <div style="line-height: 30px;"><a style="color: grey;" href="https://theporndude.com/zh" target="_blank">PornDude</a></div>
+        <div style="line-height: 30px; color: #e2e2e2; font-weight: bold">贊助商</div>
+        <div style="line-height: 30px;"><a style="color: dimgray;" href="https://l.erodatalabs.com/s/0kHIbk" target="_blank">Erolabs</a></div>
+        <div style="line-height: 30px;"><a style="color: dimgray;" href="https://creative.kbnmnl.com/widgets/v4/Universal?campaignId=300x100&modelPageOption=model&tag=girls%2Fchinese%2Cgirls%2Fjapanese&thumbsMargin=5&hideButton=1&hideTitle=1&userId=68266da2436a81581f441c04a73d1525467dff2da85808235979b437cff6f852&quality=240p&autoplay=all&autoplayForce=1" target="_blank">Stripchat</a></div>
+        <div style="line-height: 30px;"><a style="color: dimgray;" href="{{ route('jav.home') }}" target="_blank">日本AV 高清中字</a></div>
+        <div style="line-height: 30px;"><a style="color: dimgray;" href="https://theporndude.com/zh" target="_blank">PornDude</a></div>
         <hr style="border-color: #353535; margin: 15px 0px">
     </div>
     <div class="col-xs-12">
-        <div style="line-height: 30px; color: #e5e5e5; font-weight: bold">友情鏈結</div>
-        <div style="line-height: 30px;"><a style="color: grey;" href="https://qingse.one" target="_blank">情色網站大全</a></div>
-        <div style="line-height: 30px;"><a style="color: grey;" href="https://141jj.com/" target="_blank">141JJ 導航</a></div>
-        <div style="line-height: 30px;"><a style="color: grey;" href="http://www.pornbest.org/" target="_blank">PornBest 免費中文視頻</a></div>
-        <div style="line-height: 30px;"><a style="color: grey;" href="https://share.acgnx.net/" target="_blank">末日動漫資源庫</a></div>
-        <div style="line-height: 30px;"><a style="color: grey;" href="https://moeli-desu.com/" target="_blank">夢璃</a></div>
-        <div style="line-height: 30px;"><a style="color: grey;" href="https://www.sshs.pw/" target="_blank">紳士會所</a></div>
+        <div style="line-height: 30px; color: #e2e2e2; font-weight: bold">友情鏈結</div>
+        <div style="line-height: 30px;"><a style="color: dimgray;" href="https://qingse.one" target="_blank">情色網站大全</a></div>
+        <div style="line-height: 30px;"><a style="color: dimgray;" href="https://141jj.com/" target="_blank">141JJ 導航</a></div>
+        <div style="line-height: 30px;"><a style="color: dimgray;" href="http://www.pornbest.org/" target="_blank">PornBest 免費中文視頻</a></div>
+        <div style="line-height: 30px;"><a style="color: dimgray;" href="https://share.acgnx.net/" target="_blank">末日動漫資源庫</a></div>
+        <div style="line-height: 30px;"><a style="color: dimgray;" href="https://moeli-desu.com/" target="_blank">夢璃</a></div>
+        <div style="line-height: 30px;"><a style="color: dimgray;" href="https://www.sshs.pw/" target="_blank">紳士會所</a></div>
         <hr style="border-color: #353535; margin: 15px 0px">
     </div>
     <div class="col-xs-12">
-        <div style="line-height: 30px; color: #e5e5e5; font-weight: bold">Hanime1.me</div>
-        <div style="line-height: 30px;"><a style="color: grey;" href="#">H動漫</a></div>
-        <div style="line-height: 30px;"><a style="color: grey;" href="#">Hentai</a></div>
-        <div style="line-height: 30px;"><a style="color: grey;" href="#">成人色情</a></div>
-        <div style="line-height: 30px;"><a style="color: grey;" href="#">免費線上看</a></div>
+        <div style="line-height: 30px; color: #e2e2e2; font-weight: bold">Hanime1.me</div>
+        <div style="line-height: 30px;"><a style="color: dimgray;" href="#">H動漫</a></div>
+        <div style="line-height: 30px;"><a style="color: dimgray;" href="#">Hentai</a></div>
+        <div style="line-height: 30px;"><a style="color: dimgray;" href="#">成人色情</a></div>
+        <div style="line-height: 30px;"><a style="color: dimgray;" href="#">免費線上看</a></div>
         <hr style="border-color: #353535; margin: 15px 0px">
     </div>
     <div class="col-xs-12">
-        <div style="line-height: 30px; color: grey;">© 2020-2023 Hanime1.me ({{ gethostname() }})</div>
+        <div style="line-height: 30px; color: dimgray;">© 2020-2023 Hanime1.me ({{ gethostname() }})</div>
     </div>
 </div>
