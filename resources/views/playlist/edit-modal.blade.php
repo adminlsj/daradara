f0ce802f12e4932d25b832c524c107152e69a2f6
laughseejapan <laughseejapan@gmail.com>
2023-11-12 14:45:48 +0800

    hanime1

--- resources/views/playlist/edit-modal.blade.php
+++ resources/views/playlist/edit-modal.blade.php
@@ -4,12 +4,12 @@
 
   <div id="playlistEditModal" class="modal" role="dialog">
     <div class="modal-dialog">
-      <div class="modal-content" style="border-radius: 3px; background-color: #222222; color: white">
-        <div class="modal-header" style="border-bottom: 1px solid #333333; position: relative; height: 65px;">
+      <div class="modal-content">
+        <div class="modal-header">
           <span class="material-icons pull-left no-select modal-close-btn" data-dismiss="modal">close</span>
-          <h4 class="modal-title" style="text-align: center; font-weight: bold; margin: 0; padding: 0; margin-top: 5px; font-size: 18px;">編輯播放清單</h4>
+          <h4 class="modal-title">編輯播放清單</h4>
         </div>
-        <div class="modal-body" style="padding-bottom: 20px;">
+        <div class="modal-body" style="padding: 15px 20px 20px 20px; text-align: left">
           <h4>編輯標題及說明</h4>
           <p id="hentai-tags-text" style="color: darkgray; padding-bottom: 10px">打造屬於你的播放清單，並向全世界分享你的收藏。</p>
           <div class="form-group" style="margin-top: 10px;">
@@ -30,8 +30,8 @@
         </div>
         <hr style="border-color: #333333; margin: 0; margin-top: 0px;">
         <div class="modal-footer" style="border-top: none; width: 100%; text-align: center; padding: 0;">
-          <div style="display: inline-block; width: 50%; float: left; line-height: 46px; color: darkgray; cursor: pointer;" data-dismiss="modal">返回</div>
-          <button style="border: none; color: white; background-color: #b08fff; border-radius: 0; height: 100%; width: 50%; font-weight: bold; line-height: 34px;" class="pull-right btn btn-primary" type="submit">確定</button>
+          <div data-dismiss="modal">返回</div>
+          <button class="pull-right btn btn-primary" type="submit">儲存播放清單</button>
         </div>
       </div>
     </div>
