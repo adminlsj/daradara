44a99bc6fb421b24d6ee0b840d66794c8a27a420
laughseejapan <laughseejapan@gmail.com>
2023-11-16 01:45:23 +0800

    hanime1

--- resources/views/ads/comics-banner-exoclick.blade.php
+++ resources/views/ads/comics-banner-exoclick.blade.php
@@ -4,6 +4,6 @@
   </span>
   <span class="hidden-sm hidden-md hidden-lg">
     <!-- include('layouts.exoclick', ['id' => '5058654', 'width' => '300', 'height' => '100']) -->
-    <a style="display: flex; justify-content: center; align-items: center; margin-top: 19px; margin-bottom: 20px;" href="https://l.erodatalabs.com/s/0kHIbk" target="_blank"><img style="width: 300px; height: 100px;" src="https://vdownload.hembed.com/image/icon/erolabs-300x100-tw.gif?secure=EILP6Tu8ABvHVcaBhpUFyA==,1731626854"></a>
+    <a style="display: flex; justify-content: center; align-items: center; margin-top: 19px; margin-bottom: 20px;" href="https://l.erodatalabs.com/s/2BhvoV" target="_blank"><img style="width: 300px; height: 100px;" src="https://vdownload.hembed.com/image/icon/erolabs-300x100-tw.gif?secure=EILP6Tu8ABvHVcaBhpUFyA==,1731626854"></a>
   </span>
 </div>
