149ef27dff0e3586848636779959c7b613cd9b2f
laughseejapan <laughseejapan@gmail.com>
2023-11-23 16:37:14 +0800

    hanime1

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -64,7 +64,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('hanime1:check-hetznerservers')->hourly();
         $schedule->command('hanime1:check-motherless')->cron('0 */2 * * *');
 
-        $schedule->command('hanime1:upload-hscangku')->dailyAt('16:20');
+        $schedule->command('hanime1:upload-hscangku')->dailyAt('16:38');
         // $schedule->command('hanime1:update-emptysd')->hourly();
         // $schedule->command('hanime1:update-withMissav')->dailyAt('19:50');
 
