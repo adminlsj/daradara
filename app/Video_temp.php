09137bf4e543b62cb3b80cb24412849ee4d03dc3
laughseejapan <laughseejapan@gmail.com>
2023-11-23 22:18:10 +0800

    hanime1

--- app/Video_temp.php
+++ app/Video_temp.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Video;
 
 class Video_temp extends Model
 {
@@ -48,7 +49,6 @@ class Video_temp extends Model
                         })->orderBy('created_at', 'desc')->select('id', 'title', 'translations', 'caption', 'cover', 'imgur', 'created_at', 'updated_at')->limit($hCount)->get();
 
         $loop = 0;
-        $new = [];
         foreach ($最新裏番_old as $old) {
             $new = $最新裏番_new[$loop];
             $old->id = $new->id;
@@ -65,6 +65,40 @@ class Video_temp extends Model
             $old->updated_at = $new->updated_at;
             $old->save();
             $loop++;
+        }
+
+        $最新上市 = Video::with('user:id,name')->whereIn('genre', Video::$genre)->orderBy('created_at', 'desc')->select('id', 'user_id', 'title', 'cover', 'imgur', 'views', 'duration', 'created_at', 'updated_at')->limit($dCountFirst)->get();
+        $最新上傳 = Video::with('user:id,name')->whereIn('genre', Video::$genre)->orderBy('uploaded_at', 'desc')->select('id', 'user_id', 'title', 'cover', 'imgur', 'views', 'duration', 'created_at', 'updated_at')->limit($dCountFirst)->get();
+        $中文字幕 = Video::with('user:id,name')->whereIn('genre', ['Motion Anime', '3D動畫', '同人作品', 'Cosplay'])->where('tags_array', 'like', '%中文字幕%')->orderBy('uploaded_at', 'desc')->select('id', 'user_id', 'title', 'cover', 'imgur', 'views', 'duration', 'created_at', 'updated_at')->limit($dCount)->get();
+        $他們在看 = Video::with('user:id,name')->whereIn('genre', Video::$genre)->orderBy('updated_at', 'desc')->select('id', 'user_id', 'title', 'cover', 'imgur', 'views', 'duration', 'created_at', 'updated_at')->limit($dCount)->get();
+        $Motion_Anime = Video::with('user:id,name')->where('genre', 'Motion Anime')->orderBy('uploaded_at', 'desc')->select('id', 'user_id', 'title', 'cover', 'imgur', 'views', 'duration', 'created_at', 'updated_at')->limit($dCount)->get();
+        $SD動畫 = Video::with('user:id,name')->where('genre', '3D動畫')->orderBy('uploaded_at', 'desc')->select('id', 'user_id', 'title', 'cover', 'imgur', 'views', 'duration', 'created_at', 'updated_at')->limit($dCount)->get();
+        $同人作品 = Video::with('user:id,name')->where('genre', '同人作品')->orderBy('uploaded_at', 'desc')->select('id', 'user_id', 'title', 'cover', 'imgur', 'views', 'duration', 'created_at', 'updated_at')->limit($dCount)->get();
+        $Cosplay = Video::with('user:id,name')->where('genre', 'Cosplay')->orderBy('uploaded_at', 'desc')->select('id', 'user_id', 'title', 'cover', 'imgur', 'views', 'duration', 'created_at', 'updated_at')->limit($dCount)->get();
+        $本日排行 = Video::with('user:id,name')->whereIn('genre', Video::$genre)->orderBy('current_views', 'desc')->select('id', 'user_id', 'title', 'cover', 'imgur', 'views', 'duration', 'created_at', 'updated_at')->limit($dCount)->get();
+        $本月排行 = Video::with('user:id,name')->whereIn('genre', Video::$genre)->orderBy('month_views', 'desc')->select('id', 'user_id', 'title', 'cover', 'imgur', 'views', 'duration', 'created_at', 'updated_at')->limit($dCount)->get();
+        $doujins = ['最新上市' => $最新上市, '最新上傳' => $最新上傳, '中文字幕' => $中文字幕, '他們在看' => $他們在看, 'Motion_Anime' => $Motion_Anime, 'SD動畫' => $SD動畫, '同人作品' => $同人作品, 'Cosplay' => $Cosplay, '本日排行' => $本日排行, '本月排行' => $本月排行];
+        foreach ($doujins as $name => $doujin_new) {
+            $doujin_old = Video_temp::where('name', $name)->get();
+            $loop = 0;
+            foreach ($doujin_old as $old) {
+                $new = $doujin_new[$loop];
+                $old->id = $new->id;
+                $old->route = '/';
+                $old->name = $name;
+                $old->user_id = $new->user_id;
+                $old->title = $new->title;
+                $old->cover = $new->cover;
+                $old->imgur = $new->imgur;
+                $old->thumbL = $new->thumbL();
+                $old->thumbH = $new->thumbH();
+                $old->views = $new->views();
+                $old->duration = $new->duration;
+                $old->created_at = $new->created_at;
+                $old->updated_at = $new->updated_at;
+                $old->save();
+                $loop++;
+            }
         }
     }
 }
