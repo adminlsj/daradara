cb5d0b411163ec3b134a56ba4c6e5214ede70b14
laughseejapan <laughseejapan@gmail.com>
2024-02-10 14:27:53 +0800

    hanime1

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -69,7 +69,7 @@ class BotController extends Controller
             $video->save();
         } */
 
-        $filename = 'NB7EeSy.jpg';
+        $filename = 'iLMCzoj.jpg';
         $url = 'vdownload.hembed.com';
         $expiration = time() + 2629743;
         $token = 'xVEO8rLVgGkUBEBg';
