9bdbc35dd003891151bbb6f704b3cc8250a16583
laughseejapan <laughseejapan@gmail.com>
2024-03-05 12:42:10 +0800

    hanime1

--- resources/views/video/subscribeBtn.blade.php
+++ resources/views/video/subscribeBtn.blade.php
@@ -1,4 +1,4 @@
-<form id="video-subscribe-form" action="{{ route('video.subscribe') }}">
+<form id="video-subscribe-form" action="{{ route('video.subscribe') }}" method="POST">
   {{ csrf_field() }}
   <input name="subscribe-user-id" type="hidden" value="{{ $user_id }}">
   <input name="subscribe-artist-id" type="hidden" value="{{ $artist_id }}">
