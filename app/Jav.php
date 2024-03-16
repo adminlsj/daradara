5eee57a8cb942b5f02f042b8c9afbea5bc22a7f8
laughseejapan <laughseejapan@gmail.com>
2024-03-16 15:05:47 +0800

    hanime1

--- app/Jav.php
+++ app/Jav.php
@@ -17,7 +17,7 @@ use App\Mail\UserReport;
 
 class Jav
 {
-    public static $base = "http://hsck650.cc/";
+    public static $base = "http://hsck664.cc/";
 
     public static function uploadHscangku($pages = 10)
     {
