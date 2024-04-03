694c1f559b19484fa6a17945aa00d5de6709a38c
laughseejapan <laughseejapan@gmail.com>
2024-04-03 13:56:52 +0800

    hanime1

--- app/Jav.php
+++ app/Jav.php
@@ -268,7 +268,7 @@ class Jav
         $videos = Video::where('id', '>=', 91798)
                     ->whereIn('genre', Video::$genre_jav)
                     ->where('foreign_sd', 'not like', '%"jable"%')
-                    ->orderBy('id', 'asc')
+                    ->orderBy('id', 'desc')
                     ->get();
 
         foreach ($videos as $video) {
