9d05bad337bec540b09a96156765d3b792e8e5dc
laughseejapan <laughseejapan@gmail.com>
2024-05-05 14:41:37 +0800

    hanime1

--- app/Video.php
+++ app/Video.php
@@ -29,10 +29,9 @@ class Video extends Model
     ];
 
     public static $vod_servers = [
-        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-        [11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
-        [22, 23, 24],
-        [31, 32, 33, 34]
+        [],
+        [],
+        [23, 25]
     ];
 
     public static $genre = [
