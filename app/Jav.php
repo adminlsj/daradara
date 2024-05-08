b840b3fd53e23b9e56fb5ab0b4d9860a5d7c8ab8
laughseejapan <laughseejapan@gmail.com>
2024-05-08 14:40:50 +0800

    hanime1

--- app/Jav.php
+++ app/Jav.php
@@ -143,7 +143,7 @@ class Jav
         Log::info('Empty sd update ended...');
     }
 
-    public static function updateWithMissav($order = 'asc')
+    public static function updateWithMissav(Request $request)
     {
         Log::info('Missav update started...');
 
@@ -151,7 +151,7 @@ class Jav
                     ->where('user_id', 1)
                     ->where('created_at', '2000-01-01 00:00:00')
                     ->where('foreign_sd', 'not like', '%"missav"%')
-                    ->orderBy('id', $order)
+                    ->orderBy('id', $request->order)
                     ->get();
 
         foreach ($videos as $video) {
