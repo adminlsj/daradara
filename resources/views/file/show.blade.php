36a006c528f59ddbb7f4a56956b9d0e42b1597b1
laughseejapan <laughseejapan@gmail.com>
2024-05-14 23:15:04 +0800

    swiftshare

--- resources/views/file/show.blade.php
+++ resources/views/file/show.blade.php
@@ -6,12 +6,12 @@
 
 @section('content')
 
-<div class="filebox-wrapper">
+<div class="filebox-wrapper" style="max-height: 100%; height: auto;">
 	<div style="color: #a3a3a3; font-weight: 300;">{{ App\Helper::getSize($file->size) }}&nbsp;&nbsp;&nbsp;{{ Carbon\Carbon::parse($file->created_at)->format('Y-m-d') }}</div>
-	<div style="color: #d6d6d6; font-size: 26px; font-weight: 400; margin-top: 5px;">{{ $file->title }}.{{ $file->extension }}</div>
+	<div style="color: #d6d6d6; font-size: 24px; font-weight: 400; margin-top: 5px;">{{ $file->title }}.{{ $file->extension }}</div>
 
 
-	<div class="filebox" style="height: calc(100% - 137px); background-color: #EDEDED;">
+	<div class="filebox" style="height: 292px; background-color: #EDEDED;">
 		<img class="no-select" style="height: 100px; margin: 0; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);" src="https://pbs.twimg.com/media/GNTXhV2WUAA9e0C?format=png&name=240x240">
 	</div>
 
