27bcbb35ddbfcf95b127fac273c558c633da2500
laughseejapan <laughseejapan@gmail.com>
2024-05-14 22:39:01 +0800

    swiftshare

--- resources/views/nav/md.blade.php
+++ resources/views/nav/md.blade.php
@@ -1,6 +1,6 @@
 <div class="nav">
 	<a href="/" style="font-size: 20px; font-weight: 300; color: white; text-decoration: none;">Swift<span style="font-weight: 600; margin-left: 3px;">Share</span></a>
-	<a href="{{ Auth::check() ? route('file.index', ['user' => Auth::user()]) : route('login') }}"><div style="font-weight: 300; color: white; font-size: 16px;" class="pull-right">Profile</div></a>
-	<a href="{{ Auth::check() ? route('file.index', ['user' => Auth::user()]) : route('login') }}"><div style="font-weight: 300; color: white; font-size: 16px; margin-right: 40px;" class="pull-right">Files</div></a>
-	<a href="/"><div style="font-weight: 300; color: white; font-size: 16px; margin-right: 40px;" class="pull-right">Upload</div></a>
+	<a href="{{ Auth::check() ? route('file.index', ['user' => Auth::user()]) : route('login') }}"><div class="pull-right">Profile</div></a>
+	<a href="{{ Auth::check() ? route('file.index', ['user' => Auth::user()]) : route('login') }}"><div style="margin-right: 20px;" class="pull-right">Files</div></a>
+	<a href="/"><div style="margin-right: 20px;" class="pull-right">Upload</div></a>
 </div>
