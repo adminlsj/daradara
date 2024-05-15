3b121a6ec7f4af652b36c306ca7802f63bd5180e
laughseejapan <laughseejapan@gmail.com>
2024-05-15 09:57:02 +0800

    swiftshare

--- resources/views/file/show.blade.php
+++ resources/views/file/show.blade.php
@@ -8,11 +8,11 @@
 
 <div class="filebox-wrapper" style="max-height: 100%; height: auto;">
 	<div style="color: #a3a3a3; font-weight: 300;">{{ App\Helper::getSize($file->size) }}&nbsp;&nbsp;&nbsp;{{ Carbon\Carbon::parse($file->created_at)->format('Y-m-d') }}</div>
-	<div style="color: #d6d6d6; font-size: 24px; font-weight: 400; margin-top: 5px;">{{ $file->title }}.{{ $file->extension }}</div>
+	<div style="color: #d6d6d6; font-size: 22px; font-weight: 400; margin-top: 5px;">{{ $file->title }}.{{ $file->extension }}</div>
 
 
-	<div class="filebox" style="height: 292px; background-color: #EDEDED; outline-style: none;">
-		<img class="no-select" style="height: 100px; margin: 0; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);" src="https://pbs.twimg.com/media/GNTXhV2WUAA9e0C?format=png&name=240x240">
+	<div class="filebox" style="height: 290px; background-color: #EDEDED; outline-style: none;">
+		<img class="no-select" style="height: 90px; margin: 0; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);" src="https://pbs.twimg.com/media/GNTXhV2WUAA9e0C?format=png&name=240x240">
 	</div>
 
 	<a class="popunder" href="{{ $download_url }}" target="_blank" style="text-decoration: none; cursor: pointer;">
