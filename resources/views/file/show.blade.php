eed46d9d85902fa953e3c4d68836e78de2e7847a
laughseejapan <laughseejapan@gmail.com>
2024-05-17 00:12:45 +0800

    swiftshare

--- resources/views/file/show.blade.php
+++ resources/views/file/show.blade.php
@@ -15,7 +15,7 @@
 		<img class="no-select" style="height: 80px; margin: 0; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);" src="https://pbs.twimg.com/media/GNTXhV2WUAA9e0C?format=png&name=240x240">
 	</div>
 
-	<a class="popunder" href="{{ $download_url }}" target="_blank" style="text-decoration: none; cursor: pointer;">
+	<a class="popunder" href="{{ $download_url }}" style="text-decoration: none; cursor: pointer;">
 		<div class="no-select" style="background-color: #BA9F33; line-height: 44px; height: 44px; border-radius: 8px; margin-top: 15px; font-size: 16px; color: white;">
 			Download
 		</div>
