0112f7e065ad62d23dad61a357cee63ceab6de7e
laughseejapan <laughseejapan@gmail.com>
2024-06-12 23:02:35 +0800

    starting themes tab

--- resources/views/layouts/show/tab.blade.php
+++ resources/views/layouts/show/tab.blade.php
@@ -2,8 +2,8 @@
     <button class="tablinks" onclick="activeTab(event, 'overview')">簡介</button>
     <button class="tablinks" onclick="activeTab(event, 'episodes')">集數列表</button>
     <button class="tablinks" onclick="activeTab(event, 'characters')">登場人物</button>
-    <button class="tablinks active" onclick="activeTab(event, 'staffs')">製作人員</button>
-    <button class="tablinks" onclick="activeTab(event, 'themes')">主題曲</button>
+    <button class="tablinks" onclick="activeTab(event, 'staffs')">製作人員</button>
+    <button class="tablinks active" onclick="activeTab(event, 'themes')">主題曲</button>
     <button class="tablinks" onclick="activeTab(event, 'comments')">討論版</button>
 </div>
 
---
+++ resources/views/layouts/show/themes.blade.php
@@ -0,0 +1,64 @@
+<div id="themes" class="tabcontent">
+    <div class="description">
+        <div class="ranking-seasonal">
+            <strong>⭐ 季度排名 #1</strong>
+        </div>
+        <div class="ranking-following">
+            <strong>❤️ 喜愛總數 #23</strong>
+        </div>
+        <div class="ranking-all">
+            <strong>🏆 動畫排名 #31</strong>
+        </div>
+        <div class="long-des">
+            <pre>
+<h3>資訊</h3>
+<strong>動畫類別</strong>
+<a href="">番劇</a>
+
+<strong>集數</strong>
+12
+
+<strong>集數總長</strong>
+24分鐘
+
+<strong>播放狀態</strong>
+<a href="">上映中</a>
+
+<strong>首播日期</strong>
+2024年4月8日
+
+<strong>完播日期</strong>
+2024年7月8日
+
+<strong>季番</strong>
+<a href="">春番</a>
+
+<strong>更新時間</strong>
+<a href="">週三</a> 23:30
+
+<strong>動畫社</strong>
+<a href="">Drive Inc.</a>
+
+<strong>類別</strong>
+<a href="">搞笑</a>
+<a href="">戀愛</a>
+<a href="">日常</a>
+<a href="">異世界</a>
+
+<h3>評分</h3>
+daradara    9/10
+MyAnimeList 8.8/10
+AniList     98/100
+bangumi     4.5/5
+
+<h3>副標題</h3>
+<strong>繁體:</strong> 為美好的世界獻上祝福！
+<strong>簡體:</strong> 為美好的世界獻上祝福！
+<strong>English:</strong> KonoSuba
+<strong>日文:</strong> この素晴らしい世界に祝福を!
+<strong>羅馬字:</strong> Kono Subarashii Sekai ni Shukufuku o!
+</pre>
+        </div>
+    </div>
+    
+</div>
