a33af755b21f8c56b9c894ee5bb8fc25e55101bd
laughseejapan <laughseejapan@gmail.com>
2024-06-18 22:54:09 +0800

    new changes

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -9,11 +9,15 @@
 		<h3>All animes</h1>
 		<div class="row">
 			@foreach ($animes as $anime)
-				<div class="col-md-2" style="height: 100%;">
+				<div class="col-md-2">
 					<a href="{{ route('anime.show', ['anime' => $anime->id, 'title' => $anime->title_ro]) }}">
-						<img style="width: 100%; margin-top: 0px" src="{{ $anime->photo_cover }}">
-						<div>{{ $anime->title_ro }}</div>
+						<div style="position: relative;">
+							<img style="width: 100%; margin-top: 0;" src="https://images2.imgbox.com/0c/85/A2O2FiGg_o.jpg">
+							<img style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; object-fit: cover; margin-top: 0;" src="{{ $anime->photo_cover }}">
+					    </div>
+						<div style="height: 40px">{{ $anime->title_ro }}</div>
 					</a>
+					<br>
 				</div>
 			@endforeach
 		</div>
