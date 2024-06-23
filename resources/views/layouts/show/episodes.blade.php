26e68bdc53a05804bf53c8715c7e5dc0796ea219
laughseejapan <laughseejapan@gmail.com>
2024-06-23 22:01:59 +0800

    added episodes table

--- resources/views/layouts/show/episodes.blade.php
+++ resources/views/layouts/show/episodes.blade.php
@@ -1,28 +1,12 @@
 <div id="episodes" class="tabcontent" style="display:none">
     <div class="episodes-wrap">
-        <div class="episodes-card">
+       @foreach ($episodes as $episode)
+       <div class="episodes-card">
             <a href="">
-                <img src="https://i.meee.com.tw/kOBlrQH.png" alt="">
-                <div class="title">第一集 - 為美好世界獻上爆焰！</div>
+                <img src="{{ $episode->episodes_thumbnail }}" alt="">
+                <div class="title">第{{ $episode->id }}集 - {{ $episode->title_zht }}</div>
             </a>
         </div>
-        <div class="episodes-card">
-            <a href="">
-                <img src="https://i.meee.com.tw/ysm6Shn.png" alt="">
-                <div class="title">第二集 - 為美好日常獻上祝福！</div>
-            </a>
-        </div>
-        <div class="episodes-card">
-            <a href="">
-                <img src="https://i.meee.com.tw/89EgVu2.png" alt="">
-                <div class="title">第三集 - 為美好公主獻上祝福！</div>
-            </a>
-        </div>
-        <div class="episodes-card">
-            <a href="">
-                <img src="https://i.meee.com.tw/7CCqiY3.png" alt="">
-                <div class="title">第四集 - 為美好夜襲獻上祝福！</div>
-            </a>
-        </div>
+       @endforeach
     </div>
 </div>
