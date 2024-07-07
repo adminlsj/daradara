d0a9b292ea6c9c4bc076793248a70e892e70a9bc
laughseejapan <laughseejapan@gmail.com>
2024-07-07 20:42:57 +0800

    added form on show page

--- resources/views/layouts/show/headings.blade.php
+++ resources/views/layouts/show/headings.blade.php
@@ -4,7 +4,8 @@
         <div class="button-groups">
             <div class="list btn-group">
                 <button class="add btn btn-secondary"
-                    onclick="document.getElementById('addToList').style.display='flex'">添加至列表</button>
+                    onclick="document.getElementById('addToList').style.display='flex'">添加至列表
+                </button>
 
                 <div id="addToList" class="modal">
                     <form class="modal-content animate" action="/action_page.php" method="post">
@@ -23,8 +24,88 @@
                                 </div>
                             </div>
 
-                            <label for="psw"><b>Password</b></label>
-                            <input type="password" placeholder="Enter Password" name="psw" required>
+                            <div class="add-to-list-content">
+                                <div class="filters">
+                                    <div class="filter watching-status">
+                                        <h3>觀看狀態</h3>
+                                        <div class="bar">
+                                            <input type="search" placeholder="觀看狀態..."
+                                                onclick="document.getElementById('option-watching-status').style.display='block'">
+                                            <div class="scroll-wrap" id="option-watching-status">
+                                                <div class="option-group">
+                                                    <option value="觀看中">觀看中</option>
+                                                    <option value="準備觀看">準備觀看</option>
+                                                    <option value="完成觀看">完成觀看</option>
+                                                    <option value="重看中">重看中</option>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="filter review-score">
+                                        <h3>評分</h3>
+                                        <div class="bar">
+                                            <input type="number" value="0" min="0" max="10">
+                                        </div>
+                                    </div>
+
+                                    <div class="filter episodes-progress">
+                                        <h3>觀看進度</h3>
+                                        <div class="bar">
+                                            <input type="number" value="1" min="1" max="{{ $anime->episodes }}">
+                                        </div>
+                                    </div>
+
+                                    <div class="filter watch-date">
+                                        <h3>觀看日期</h3>
+                                        <div class="bar">
+                                            <input type="date">
+                                        </div>
+                                    </div>
+
+                                    <div class="filter watch-enddate">
+                                        <h3>完成日期</h3>
+                                        <div class="bar">
+                                            <input type="date">
+                                        </div>
+                                    </div>
+
+                                    <div class="filter total-rewatches">
+                                        <h3>觀看次數</h3>
+                                        <div class="bar">
+                                            <input type="number">
+                                        </div>
+                                    </div>
+
+                                    <div class="filter notes">
+                                        <h3>備註</h3>
+                                        <div class="bar">
+                                            <textarea name="" id=""></textarea>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="add-to-list-options filter">
+                                    <h3>動漫清單</h3>
+                                    <div class="user-lists">
+                                        <input type="checkbox" name="雨宮天" value="雨宮天">
+                                        <label for="雨宮天">雨宮天</label>
+                                        <br>
+                                        <input type="checkbox" name="2023神番" value="2023神番">
+                                        <label for="2023神番">2023神番</label>
+                                        <br>
+                                    </div>
+                                    <div class="bar">
+                                            <input type="search" placeholder="公開設定..."
+                                                onclick="document.getElementById('option-list-privacy').style.display='block'">
+                                            <div class="scroll-wrap" id="option-list-privacy">
+                                                <div class="option-group">
+                                                    <option value="公開">公開</option>
+                                                    <option value="私人">私人</option>
+                                                </div>
+                                            </div>
+                                        </div>
+                                </div>
+                            </div>
                         </div>
                     </form>
                 </div>
@@ -92,4 +173,21 @@
         document.getElementById(tabName).style.display = "block";
         evt.currentTarget.className += " active";
     }
+</script>
+
+<script>
+    window.addEventListener('mouseup', function (event) {
+        var optionWatchingStatus = document.getElementById('option-watching-status');
+        var optionListPrivacy = document.getElementById('option-list-privacy');
+
+        if (event.target === optionWatchingStatus) {
+            optionWatchingStatus.style.display = 'block';
+        }
+        else if (event.target === optionListPrivacy) {
+            optionListPrivacy.style.display = 'block';
+        }
+
+        optionWatchingStatus.style.display = 'none';
+        optionListPrivacy.style.display = 'none';
+    });  
 </script>
