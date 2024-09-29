09335a7250de1c018ccc76c9d15b2a823f669a6b
laughseejapan <laughseejapan@gmail.com>
2024-09-29 12:39:37 +0800

    moved layouts show to anime show

--- resources/views/anime/show.blade.php
+++ resources/views/anime/show.blade.php
@@ -5,15 +5,15 @@
 @endsection
 
 @section('content') 
-    @include('layouts.show.banner')
-    @include('layouts.show.headings')
+    @include('anime.show.banner')
+    @include('anime.show.headings')
 
     <div class="contents" style="display: flex; flex-direction: row; padding: 25px 14%; background-color: #0b1622;">
-        @include('layouts.show.sidebar')
-        @include('layouts.show.overview')
-        @include('layouts.show.episodes')
-        @include('layouts.show.characters')
-        @include('layouts.show.staffs')
-        @include('layouts.show.comments')
+        @include('anime.show.sidebar')
+        @include('anime.show.overview')
+        @include('anime.show.episodes')
+        @include('anime.show.characters')
+        @include('anime.show.staffs')
+        @include('anime.show.comments')
     </div>
 @endsection
