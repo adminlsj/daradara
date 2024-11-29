6466dc5d6656eb7ff3c66d68184456f2a53bfc1a
laughseejapan <laughseejapan@gmail.com>
2024-11-29 11:14:15 +0800

    modified user page function

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,6 +16,7 @@ use Auth;
 use Redirect;
 use Carbon\Carbon;
 use App\Helper;
+use Doctrine\DBAL\Schema\View;
 use Illuminate\Support\Facades\Storage;
 
 class UserController extends Controller
@@ -28,8 +29,9 @@ class UserController extends Controller
 
     public function animelist(Request $request, User $user)
     {
-        $anime_lists = $user->anime_lists;
-        return view('user.show.animeList', compact('user', 'anime_lists'));
+        $anime_lists = $user->anime_lists->load('anime_saves.anime');
+        $anime_statuslists = $user->anime_statuslists->load('anime_saves.anime');
+        return view('user.show.animeList', compact('user', 'anime_lists', 'anime_statuslists'));
     }
 
     public function likes(Request $request, User $user)
