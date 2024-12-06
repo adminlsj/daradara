3fada40e4c35a8774b895e3cc72066ba8bf1d862
laughseejapan <laughseejapan@gmail.com>
2024-12-06 10:09:35 +0800

    new changes

--- app/Console/Commands/ScrapeMalSingleField.php
+++ app/Console/Commands/ScrapeMalSingleField.php
@@ -41,7 +41,7 @@ class ScrapeMalSingleField extends Command
     public function handle()
     {
         // Scrape is_adult from MAL
-        $animes = Anime::where('id', '>', 5907)->where('sources', 'ilike', '%"myanimelist"%')->where('genres', '[]')->where('rating_mal_count', null)->orderBy('id', 'asc')->limit(3)->get();
+        $animes = Anime::where('id', '>', 5936)->where('sources', 'ilike', '%"myanimelist"%')->where('genres', '[]')->where('rating_mal_count', null)->orderBy('id', 'asc')->limit(3)->get();
         foreach ($animes as $anime) {
             $curl_connection = curl_init($anime->sources['myanimelist']);
             curl_setopt($curl_connection, CURLOPT_CONNECTTIMEOUT, 30);
