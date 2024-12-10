980c6c86c9ddcbdd526070b909f099b9e8254c14
laughseejapan <laughseejapan@gmail.com>
2024-12-10 12:17:56 +0800

    modified user animelist

--- resources/views/user/show/navTabs.blade.php
+++ resources/views/user/show/navTabs.blade.php
@@ -1,4 +1,4 @@
-<div class="navtabs flex-row">
+<div class="navtabs flex-row" style="width:100%;">
     <a href="" class="tablinks">簡介</a>
     <a href="" class="tablinks">動畫列表</a>
     <a href="" class="tablinks">喜愛</a>
