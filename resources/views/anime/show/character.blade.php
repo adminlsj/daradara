5f4223172ed9457c3af4f4f2e17ecd7744dcff08
laughseejapan <laughseejapan@gmail.com>
2024-12-26 12:20:11 +0800

    new changes

--- resources/views/anime/show/character.blade.php
+++ resources/views/anime/show/character.blade.php
@@ -2,9 +2,9 @@
     <a href="{{ route('character.show', ['character' => $character->id, 'title' => $character->name_en]) }}"><img style="border-top-left-radius: 3px; border-bottom-left-radius: 3px;" src="{{ $character->photo_cover }}" alt=""></a>
     <div class="characters-description flex-column">
         <div class="characters-name characters-name-upper flex-row" style="color: rgb(92,114,138);">
-            <a href="{{ route('character.show', ['character' => $character->id, 'title' => $character->name_en]) }}">{{ $character->name_en }}</a>
+            <a href="{{ route('character.show', ['character' => $character->id, 'title' => $character->getName($chinese)]) }}">{{ $character->getName($chinese) }}</a>
             @if ($character->actors->first())
-                <a class="actors" href="{{ route('staff.show', ['staff' => $character->actors->first()->id, 'title' => $character->actors->first()->name_en]) }}">{{ $character->actors->first()->name_en }}</a>
+                <a class="actors" href="{{ route('staff.show', ['staff' => $character->actors->first()->id, 'title' => $character->actors->first()->getName($chinese)]) }}">{{ $character->actors->first()->getName($chinese) }}</a>
             @else
                 <a href="">Unknown</a>
             @endif
@@ -15,7 +15,7 @@
         </div>
     </div>
     @if ($character->actors->first())
-        <a href="{{ route('staff.show', ['staff' => $character->actors->first()->id, 'title' => $character->actors->first()->name_en]) }}"><img style="border-top-right-radius: 3px; border-bottom-right-radius: 3px;" src="{{ $character->actors->first()->photo_cover }}" alt="{{ $character->actors->first()->name_en }}"></a>
+        <a href="{{ route('staff.show', ['staff' => $character->actors->first()->id, 'title' => $character->actors->first()->getName($chinese)]) }}"><img style="border-top-right-radius: 3px; border-bottom-right-radius: 3px;" src="{{ $character->actors->first()->photo_cover }}" alt="{{ $character->actors->first()->getName($chinese) }}"></a>
     @else
         <a href="#"><img style="border-top-right-radius: 3px; border-bottom-right-radius: 3px;" src="https://images2.imgbox.com/8b/b1/PSDIPPwq_o.gif" alt=""></a>
     @endif
