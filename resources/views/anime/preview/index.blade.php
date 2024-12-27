b12b18dfa748f2ed9546401c7e78f0d0f7d64c97
laughseejapan <laughseejapan@gmail.com>
2024-12-27 23:23:31 +0800

    added preview page

--- resources/views/anime/preview/index.blade.php
+++ resources/views/anime/preview/index.blade.php
@@ -9,6 +9,20 @@
     method="GET">
     <div class="flex-center-wrapper home-wrapper">
         <div class="flex-center-content flex-column" style="margin-top: 100px;">
+            <div class="filter format flex-row" style="position: relative; justify-content:flex-end; gap:15px;">
+                <div onclick="extra_filter_toggle()" class="extra-filter-wrap no-select"
+                    style="background-color: transparent;" type="button" data-toggle="dropdown">
+                    <div class="flex-column" id="home-filter-more-btn">
+                        <i class="fa fa-calendar"></i>
+                    </div>
+                </div>
+                <div onclick="extra_filter_toggle()" class="extra-filter-wrap no-select"
+                    style="background-color: transparent;" type="button" data-toggle="dropdown">
+                    <div id="home-filter-more-btn">
+                        <i class="fa fa-bars"></i>
+                    </div>
+                </div>
+            </div>
             @include('anime.preview.search')
 
             @foreach (['TV', 'Movie', 'OVA / ONA / Special'] as $category)
