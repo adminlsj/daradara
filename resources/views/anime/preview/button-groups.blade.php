ddc7d1794e2f552c8a185608b2d4faa048e5df54
laughseejapan <laughseejapan@gmail.com>
2024-12-30 21:50:43 +0800

    modified preview page added box shadow

--- resources/views/anime/preview/button-groups.blade.php
+++ resources/views/anime/preview/button-groups.blade.php
@@ -1,15 +1,17 @@
-<div class="filter format flex-row" style="position: relative; justify-content:flex-end; gap:15px;">
+<div class="filter format flex-row preview-button-groups" style="position: relative; justify-content:flex-end; gap:15px;">
     <a href="{{ route('preview.airing') }}">
         <div class="extra-filter-wrap no-select" style="background-color: transparent;">
-            <div class="flex-column" id="home-filter-more-btn">
+            <div class="flex-column" id="preview-filter-more-btn">
                 <i class="fa fa-calendar"></i>
+                <h6>今週播放</h6>
             </div>
         </div>
     </a>
     <a href="{{ route('preview.index') }}">
         <div class="extra-filter-wrap no-select" style="background-color: transparent;">
-            <div id="home-filter-more-btn">
+            <div class="flex-column" id="preview-filter-more-btn">
                 <i class="fa fa-bars"></i>
+                <h6>新番表</h6>
             </div>
         </div>
     </a>
