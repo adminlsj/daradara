ee632176d3a73c56b20b14fa48bb9a882edcb60b
laughseejapan <laughseejapan@gmail.com>
2025-01-08 14:18:20 +0800

    new changes

--- resources/views/layouts/nav-bottom.blade.php
+++ resources/views/layouts/nav-bottom.blade.php
@@ -1,41 +1,51 @@
-<div style="z-index: 10000 !important; background-color: rgba(221, 230, 238,0.75); backdrop-filter: blur(40px); -webkit-backdrop-filter: blur(40px); {{ Request::is('*watch*') ? 'display:none;' : '' }}" class="bottom-nav hidden-lg hidden-md white-theme-nav-bottom">
-  <a href="/">
+<div style="z-index: 10000 !important; background-color: #edf1f5; border-top: 1px solid rgba(221, 230, 238, 0.6)" class="bottom-nav hidden-lg hidden-md white-theme-nav-bottom">
+  <a href="/" style="color: #5c728a;">
     @if (Request::is('/'))
-      <img style="height: 19px; margin-top: 8px;" src="https://images2.imgbox.com/0e/de/db2bisos_o.png">
-      <div style="font-size: 9px; color: white; margin-top: 5px;">主頁</div>
+      <img style="height: 21px; margin-top: 7px;" src="https://images2.imgbox.com/a1/39/dGzAzEb1_o.png">
+      <div style="font-size: 9px; margin-top: 4px;">主頁</div>
     @else
-      <img style="height: 19px; margin-top: 8px;" src="https://images2.imgbox.com/d7/86/WJavPL4c_o.png">
-      <div style="font-size: 9px; color: #838383; margin-top: 5px;">主頁</div>
+      <img style="height: 21px; margin-top: 7px;" src="https://images2.imgbox.com/a3/43/OwM5qFGZ_o.png">
+      <div style="font-size: 9px; margin-top: 4px;">主頁</div>
     @endif
   </a>
 
-  <a href="{{ route('anime.search') }}?sort=本日排行">
+  <a href="{{ route('preview.show', ['season' => 'Winter', 'year' => 2025]) }}" style="color: #5c728a;">
+    @if (Request::is('*preview*'))
+      <img style="height: 21px; margin-top: 7px;" src="https://images2.imgbox.com/ef/8b/IiDdEvLV_o.png">
+      <div style="font-size: 9px; margin-top: 4px;">新番預告</div>
+    @else
+      <img style="height: 21px; margin-top: 7px;" src="https://images2.imgbox.com/71/ca/71QRtoGb_o.png">
+      <div style="font-size: 9px; margin-top: 4px;">新番預告</div>
+    @endif
+  </a>
+
+  <a href="{{ route('anime.search') }}" style="color: #5c728a;">
+    @if (Request::is('*anime/search*'))
+      <img style="height: 24px; margin-top: 5px;" src="https://images2.imgbox.com/03/ae/xyQJpnO8_o.png">
+      <div style="font-size: 9px; margin-top: 3px;">搜尋</div>
+    @else
+      <img style="height: 24px; margin-top: 5px;" src="https://images2.imgbox.com/d0/ba/vOy6l41S_o.png">
+      <div style="font-size: 9px; margin-top: 3px;">搜尋</div>
+    @endif
+  </a>
+
+  <a href="{{ route('anime.search') }}?sort=本日排行" style="color: #5c728a;">
     @if (Request::getRequestUri() == '/search?sort=%E6%9C%AC%E6%97%A5%E6%8E%92%E8%A1%8C')
-      <img style="height: 21px; margin-top: 7px;" src="https://images2.imgbox.com/74/b8/zZMCNPR3_o.png">
-      <div style="font-size: 9px; color: white; margin-top: 4px;">本日排行</div>
+      <img style="height: 21px; margin-top: 7px;" src="https://images2.imgbox.com/2d/2f/omEUBRbL_o.png">
+      <div style="font-size: 9px; margin-top: 4px;">通知</div>
     @else
-      <img style="height: 21px; margin-top: 7px;" src="https://images2.imgbox.com/e7/90/6CJe0Mn7_o.png">
-      <div style="font-size: 9px; color: #838383; margin-top: 4px;">本日排行</div>
+      <img style="height: 21px; margin-top: 7px;" src="https://images2.imgbox.com/81/b4/mBL5Fanh_o.png">
+      <div style="font-size: 9px; margin-top: 4px;">通知</div>
     @endif
   </a>
 
-  <a href="{{ route('anime.search') }}">
-    @if (Request::is('*subscriptions*'))
-      <img style="height: 24px; margin-top: 5px; filter: brightness(200%);" src="https://images2.imgbox.com/84/9b/PfoATVQn_o.png">
-      <div style="font-size: 9px; color: white; margin-top: 3px;">訂閱內容</div>
-    @else
-      <img style="height: 24px; margin-top: 5px;" src="https://images2.imgbox.com/6d/64/xkjzVmdX_o.png">
-      <div style="font-size: 9px; color: #838383; margin-top: 3px;">訂閱內容</div>
-    @endif
-  </a>
-
-  <a href="{{ Auth::check() ? route('anime.search') : route('login') }}">
+  <a href="{{ Auth::check() ? route('user.animelist', ['user' => Auth::user(), 'name' => Auth::user()->name]) : route('login') }}" style="color: #5c728a;">
     @if (Request::is('*playlists*'))
       <img style="height: 22px; margin-top: 7px; border-radius: 2px;" src="{{ Auth::check() ? Auth::user()->avatar_temp : 'https://images2.imgbox.com/5f/2b/uPIwZSoy_o.jpg' }}">
-      <div style="font-size: 9px; color: white; margin-top: 3px;">我的 Hanime1</div>
+      <div style="font-size: 9px; margin-top: 3px;">我的清單</div>
     @else
       <img style="height: 22px; margin-top: 7px; border-radius: 2px;" src="{{ Auth::check() ? Auth::user()->avatar_temp : 'https://images2.imgbox.com/5f/2b/uPIwZSoy_o.jpg' }}">
-      <div style="font-size: 9px; color: #838383; margin-top: 3px;">我的 Hanime1</div>
+      <div style="font-size: 9px; margin-top: 3px;">我的清單</div>
     @endif
   </a>
 </div>
