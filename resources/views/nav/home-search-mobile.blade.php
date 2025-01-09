565f3c14992d2b23a951796ae8535426e8172a96
laughseejapan <laughseejapan@gmail.com>
2025-01-09 10:44:48 +0800

    new changes

--- resources/views/nav/home-search-mobile.blade.php
+++ resources/views/nav/home-search-mobile.blade.php
@@ -33,10 +33,10 @@
 	        <h4 class="modal-title">播出年份</h4>
 	      </div>
 	      <div class="modal-body" style="padding: 0; text-align: center">
-	        <input type="hidden" id="sort" name="sort" value="{{ $sort ? $sort : '' }}">
-
+	        <div class="simple-dropdown-item hentai-sort-options-wrapper home-option year-option">全部</div>
+	        <hr style="margin: 0; border-color: #E0E7EF;">
 	        @for ($i = Carbon\Carbon::now()->year + 1; $i >= 1917; $i--)
-		        <div class="simple-dropdown-item hentai-sort-options-wrapper {{ $sort ? $sort == $i ? 'active' : '' : '' }}"><div class="hentai-sort-options">{{ $i }}</div></div>
+		        <div class="simple-dropdown-item hentai-sort-options-wrapper home-option year-option {{ $year ? $year == $i ? 'active' : '' : '' }}" data-input="year">{{ $i }}</div>
 		        <hr class="{{ $i == 1917 ? 'hidden-sm hidden-md hidden-lg hidden-xl' : '' }}" style="margin: 0; border-color: #E0E7EF;">
 	        @endfor
 	      </div>
@@ -49,27 +49,159 @@
 	  </div>
 	</div>
 
-	<div class="home-genre-tabs-wrapper" data-toggle="modal" data-target="#date-modal">
+	<div class="home-genre-tabs-wrapper" data-toggle="modal" data-target="#season-modal">
 		<span class="home-genre-tabs">播放季度<img style="margin-top: -2px; margin-left: 7px; width: 12px;" src="https://images2.imgbox.com/36/8c/Npue5qJ0_o.png"></span>
 	</div>
 
-	<div class="home-genre-tabs-wrapper" data-toggle="modal" data-target="#date-modal">
+	<div id="season-modal" class="modal" role="dialog">
+	  <div class="modal-dialog">
+	    <div class="modal-content">
+	      <div class="modal-header">
+	        <span class="material-icons pull-left no-select modal-close-btn" data-dismiss="modal">close</span>
+	        <h4 class="modal-title">播放季度</h4>
+	      </div>
+	      <div class="modal-body" style="padding: 0; text-align: center">
+			@foreach (['全部', '1月冬番', '4月春番', '7月夏番', '10月秋番'] as $option)
+		        <div class="simple-dropdown-item hentai-sort-options-wrapper home-option season-option {{ $season ? $season == $option ? 'active' : '' : '' }}" data-input="season">{{ $option }}</div>
+		        <hr style="margin: 0; border-color: #E0E7EF;">
+	        @endforeach
+	      </div>
+	      <hr style="border-color: #E0E7EF; margin: 0">
+	      <div class="modal-footer">
+	      	<div data-dismiss="modal">取消</div>
+			<button class="pull-right btn btn-primary" type="submit">顯示搜索結果</button>
+	      </div>
+	    </div>
+	  </div>
+	</div>
+
+	<div class="home-genre-tabs-wrapper" data-toggle="modal" data-target="#category-modal">
 		<span class="home-genre-tabs">類型<img style="margin-top: -2px; margin-left: 7px; width: 12px;" src="https://images2.imgbox.com/36/8c/Npue5qJ0_o.png"></span>
 	</div>
 
-	<div class="home-genre-tabs-wrapper" data-toggle="modal" data-target="#date-modal">
+	<div id="category-modal" class="modal" role="dialog">
+	  <div class="modal-dialog">
+	    <div class="modal-content">
+	      <div class="modal-header">
+	        <span class="material-icons pull-left no-select modal-close-btn" data-dismiss="modal">close</span>
+	        <h4 class="modal-title">類型</h4>
+	      </div>
+	      <div class="modal-body" style="padding: 0; text-align: center">
+			@foreach (['全部', 'TV', 'Movie', 'TV Special', 'Special', 'OVA', 'ONA', 'Music', 'PV', 'CM', 'Unknown'] as $option)
+		        <div class="simple-dropdown-item hentai-sort-options-wrapper home-option category-option {{ $category ? $category == $option ? 'active' : '' : '' }}" data-input="category">{{ $option }}</div>
+		        <hr class="{{ $loop->last ? 'hidden-sm hidden-md hidden-lg hidden-xl' : '' }}" style="margin: 0; border-color: #E0E7EF;">
+	        @endforeach
+	      </div>
+	      <hr style="border-color: #E0E7EF; margin: 0">
+	      <div class="modal-footer">
+	      	<div data-dismiss="modal">取消</div>
+			<button class="pull-right btn btn-primary" type="submit">顯示搜索結果</button>
+	      </div>
+	    </div>
+	  </div>
+	</div>
+
+	<div class="home-genre-tabs-wrapper" data-toggle="modal" data-target="#airing-status-modal">
 		<span class="home-genre-tabs">播放狀態<img style="margin-top: -2px; margin-left: 7px; width: 12px;" src="https://images2.imgbox.com/36/8c/Npue5qJ0_o.png"></span>
 	</div>
 
-	<div class="home-genre-tabs-wrapper" data-toggle="modal" data-target="#date-modal">
+	<div id="airing-status-modal" class="modal" role="dialog">
+	  <div class="modal-dialog">
+	    <div class="modal-content">
+	      <div class="modal-header">
+	        <span class="material-icons pull-left no-select modal-close-btn" data-dismiss="modal">close</span>
+	        <h4 class="modal-title">播放狀態</h4>
+	      </div>
+	      <div class="modal-body" style="padding: 0; text-align: center">
+			@foreach (['全部', 'Currently Airing', 'Finished Airing', 'Not yet aired', 'Cancelled'] as $option)
+		        <div class="simple-dropdown-item hentai-sort-options-wrapper home-option airing-status-option {{ $airing_status ? $airing_status == $option ? 'active' : '' : '' }}" data-input="airing_status">{{ $option }}</div>
+		        <hr style="margin: 0; border-color: #E0E7EF;">
+	        @endforeach
+	      </div>
+	      <hr style="border-color: #E0E7EF; margin: 0">
+	      <div class="modal-footer">
+	      	<div data-dismiss="modal">取消</div>
+			<button class="pull-right btn btn-primary" type="submit">顯示搜索結果</button>
+	      </div>
+	    </div>
+	  </div>
+	</div>
+
+	<div class="home-genre-tabs-wrapper" data-toggle="modal" data-target="#streaming-on-modal">
 		<span class="home-genre-tabs">串流平台<img style="margin-top: -2px; margin-left: 7px; width: 12px;" src="https://images2.imgbox.com/36/8c/Npue5qJ0_o.png"></span>
 	</div>
 
-	<div class="home-genre-tabs-wrapper" data-toggle="modal" data-target="#date-modal">
+	<div id="streaming-on-modal" class="modal" role="dialog">
+	  <div class="modal-dialog">
+	    <div class="modal-content">
+	      <div class="modal-header">
+	        <span class="material-icons pull-left no-select modal-close-btn" data-dismiss="modal">close</span>
+	        <h4 class="modal-title">串流平台</h4>
+	      </div>
+	      <div class="modal-body" style="padding: 0; text-align: center">
+			@foreach (['全部'] as $option)
+		        <div class="simple-dropdown-item hentai-sort-options-wrapper home-option streaming-on-option {{ $streaming_on ? $streaming_on == $option ? 'active' : '' : '' }}" data-input="streaming_on">{{ $option }}</div>
+		        <hr style="margin: 0; border-color: #E0E7EF;">
+	        @endforeach
+	      </div>
+	      <hr style="border-color: #E0E7EF; margin: 0">
+	      <div class="modal-footer">
+	      	<div data-dismiss="modal">取消</div>
+			<button class="pull-right btn btn-primary" type="submit">顯示搜索結果</button>
+	      </div>
+	    </div>
+	  </div>
+	</div>
+
+	<div class="home-genre-tabs-wrapper" data-toggle="modal" data-target="#country-modal">
 		<span class="home-genre-tabs">原產國家<img style="margin-top: -2px; margin-left: 7px; width: 12px;" src="https://images2.imgbox.com/36/8c/Npue5qJ0_o.png"></span>
 	</div>
 
-	<div class="home-genre-tabs-wrapper" data-toggle="modal" data-target="#date-modal">
+	<div id="country-modal" class="modal" role="dialog">
+	  <div class="modal-dialog">
+	    <div class="modal-content">
+	      <div class="modal-header">
+	        <span class="material-icons pull-left no-select modal-close-btn" data-dismiss="modal">close</span>
+	        <h4 class="modal-title">原產國家</h4>
+	      </div>
+	      <div class="modal-body" style="padding: 0; text-align: center">
+			@foreach (['全部'] as $option)
+		        <div class="simple-dropdown-item hentai-sort-options-wrapper home-option country-option {{ $country ? $country == $option ? 'active' : '' : '' }}" data-input="country">{{ $option }}</div>
+		        <hr style="margin: 0; border-color: #E0E7EF;">
+	        @endforeach
+	      </div>
+	      <hr style="border-color: #E0E7EF; margin: 0">
+	      <div class="modal-footer">
+	      	<div data-dismiss="modal">取消</div>
+			<button class="pull-right btn btn-primary" type="submit">顯示搜索結果</button>
+	      </div>
+	    </div>
+	  </div>
+	</div>
+
+	<div class="home-genre-tabs-wrapper" data-toggle="modal" data-target="#source-modal">
 		<span class="home-genre-tabs">原作素材<img style="margin-top: -2px; margin-left: 7px; width: 12px;" src="https://images2.imgbox.com/36/8c/Npue5qJ0_o.png"></span>
+	</div>
+
+	<div id="source-modal" class="modal" role="dialog">
+	  <div class="modal-dialog">
+	    <div class="modal-content">
+	      <div class="modal-header">
+	        <span class="material-icons pull-left no-select modal-close-btn" data-dismiss="modal">close</span>
+	        <h4 class="modal-title">原作素材</h4>
+	      </div>
+	      <div class="modal-body" style="padding: 0; text-align: center">
+			@foreach (['全部'] as $option)
+		        <div class="simple-dropdown-item hentai-sort-options-wrapper home-option source-option {{ $source ? $source == $option ? 'active' : '' : '' }}" data-input="source">{{ $option }}</div>
+		        <hr style="margin: 0; border-color: #E0E7EF;">
+	        @endforeach
+	      </div>
+	      <hr style="border-color: #E0E7EF; margin: 0">
+	      <div class="modal-footer">
+	      	<div data-dismiss="modal">取消</div>
+			<button class="pull-right btn btn-primary" type="submit">顯示搜索結果</button>
+	      </div>
+	    </div>
+	  </div>
 	</div>
 </div>
--- resources/views/nav/home-search.blade.php
+++ resources/views/nav/home-search.blade.php
@@ -39,9 +39,9 @@
 			</button>
 			<div id="search-year-dropdown" class="dropdown-menu home-option-wrapper">
 				<input type="hidden" id="year" name="year" value="{{ $year }}">
-				<div class="home-option year-option">全部</div>
+				<div class="home-option year-option" data-input="year">全部</div>
 				@for ($i = Carbon\Carbon::now()->year + 1; $i >= 1917; $i--)
-			        <div class="home-option year-option">{{ $i }}</div>
+			        <div class="home-option year-option" data-input="year">{{ $i }}</div>
 			    @endfor
 			</div>
 		</div>
@@ -54,15 +54,13 @@
 			</button>
 			<div id="search-season-dropdown" class="dropdown-menu home-option-wrapper">
 				<input type="hidden" id="season" name="season" value="{{ $season }}">
-				<div class="home-option season-option">全部</div>
-				<div class="home-option season-option">1月冬番</div>
-				<div class="home-option season-option">4月春番</div>
-				<div class="home-option season-option">7月夏番</div>
-				<div class="home-option season-option">10月秋番</div>
+				@foreach (['全部', '1月冬番', '4月春番', '7月夏番', '10月秋番'] as $option)
+					<div class="home-option season-option" data-input="season">{{ $option }}</div>
+				@endforeach
 			</div>
 		</div>
 
-		<div class="filter format" style="position: relative;">
+		<div class="filter category" style="position: relative;">
 			<h3>類型</h3>
 			<button style="outline:0; color: rgb(173,192,210); padding: 0px 14px 5px 14px;" class="bar btn btn-secondary dropdown-toggle close-extra-filter-dropdown-menu" type="button" data-toggle="dropdown">
 				<div class="filter-value-text {{ $category ? 'active' : '' }}">{{ $category ? $category : '全部' }}</div>
@@ -70,9 +68,8 @@
 			</button>
 			<div id="search-category-dropdown" class="dropdown-menu home-option-wrapper">
 				<input type="hidden" id="category" name="category" value="{{ $category }}">
-				<div class="home-option category-option">全部</div>
-				@foreach (['TV', 'Movie', 'TV Special', 'Special', 'OVA', 'ONA', 'Music', 'PV', 'CM', 'Unknown'] as $category)
-					<div class="home-option category-option">{{ $category }}</div>
+				@foreach (['全部', 'TV', 'Movie', 'TV Special', 'Special', 'OVA', 'ONA', 'Music', 'PV', 'CM', 'Unknown'] as $category)
+					<div class="home-option category-option" data-input="category">{{ $category }}</div>
 				@endforeach
 			</div>
 		</div>
@@ -96,10 +93,9 @@
 					</button>
 					<div id="airing-status-dropdown" class="dropdown-menu home-option-wrapper" style="width: 180px">
 						<input type="hidden" id="airing_status" name="airing_status" value="{{ $airing_status }}">
-						<div class="home-option airing-status-option">全部</div>
-							@foreach (['Currently Airing', 'Finished Airing', 'Not yet aired', 'Cancelled'] as $airing_status)
-								<div class="home-option airing-status-option">{{ $airing_status }}</div>
-							@endforeach
+						@foreach (['全部', 'Currently Airing', 'Finished Airing', 'Not yet aired', 'Cancelled'] as $airing_status)
+							<div class="home-option airing-status-option" data-input="airing_status">{{ $airing_status }}</div>
+						@endforeach
 					</div>
 				</div>
 
